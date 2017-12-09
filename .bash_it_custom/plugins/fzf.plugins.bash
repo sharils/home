31dd29fa2742cee841d1e9efc2677d3fb5ca372b
sharils <sharils@users.noreply.github.com>
2017-12-09 12:19:10 +0800

    Show hidden file in fzf result

--- .bash_it_custom/plugins/fzf.plugins.bash
+++ .bash_it_custom/plugins/fzf.plugins.bash
@@ -4,7 +4,7 @@
 export FZF_DEFAULT_OPTS='--multi'
 
 # Setting rg as the default source for fzf
-export FZF_DEFAULT_COMMAND='rg --files'
+export FZF_DEFAULT_COMMAND='rg --files --hidden --glob=!.git'
 
 # To apply the command to CTRL-T as well
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
