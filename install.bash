efb62cfb153a594b82e450f62099a7261a9ae91b
Minnie Bavaro <minniebavaro@Mallory-Brosnan.lan>
2017-12-13 20:13:50 +0800

    Clean up git ignore

--- install.bash
+++ install.bash
@@ -21,11 +21,12 @@ git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it && \
 ln -fs \
     $PWD/.bash_it_custom \
     $PWD/.bashrc \
-    $PWD/.gitconfig \
     $PWD/.gitignore \
     $PWD/.vim \
     $PWD/.vimrc \
     ~
+
+cp .gitconfig ~
 
 git submodule update --init --recursive
 
