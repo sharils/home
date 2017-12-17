e8438c846445bf963eef9e4371c3de2e39212ed9
sharils <sharils@users.noreply.github.com>
2017-12-17 23:53:53 +0800

    Fix linter warning

--- install.bash
+++ install.bash
@@ -3,46 +3,45 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew install \
-    bash-completion \
-    fpp \
-    fzf \
-    git \
-    ripgrep \
-    thefuck \
-    tig \
-    vim \
-    yarn
+	bash-completion \
+	fpp \
+	fzf \
+	git \
+	ripgrep \
+	thefuck \
+	tig \
+	vim \
+	yarn
 
 vimdiff .bash_profile ~/.bash_profile
 
-git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it && \
-    ~/.bash_it/install.sh
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it &&
+	~/.bash_it/install.sh
 
 ln -fs \
-    $PWD/.bash_it_custom \
-    $PWD/.bashrc \
-    $PWD/.gitignore \
-    $PWD/.vim \
-    $PWD/.vimrc \
-    ~
+	"$PWD/.bash_it_custom" \
+	"$PWD/.bashrc" \
+	"$PWD/.gitignore" \
+	"$PWD/.vim" \
+	"$PWD/.vimrc" \
+	~
 
 cp .gitconfig ~
 
 git submodule update --init --recursive
 
 (
-    cd .vim/bundle-available/YouCompleteMe/
-    cd third_party/ycmd/third_party/racerd/
-    git checkout .
+	cd .vim/bundle-available/YouCompleteMe/third_party/ycmd/third_party/racerd/ || exit
+	git checkout .
 )
 
 (
-    cd .vim/bundle-available/tagbar-phpctags.vim
-    make
+	cd .vim/bundle-available/tagbar-phpctags.vim || exit
+	make
 )
 
 (
-    npm install -g git+https://github.com/ramitos/jsctags.git
-    cd .vim/bundle-available/tern_for_vim/
-    yarn --no-lockfile install
+	npm install -g git+https://github.com/ramitos/jsctags.git
+	cd .vim/bundle-available/tern_for_vim/ || exit
+	yarn --no-lockfile install
 )
