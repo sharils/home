0420889318fa2e67f4491b78f34ff6b88ce36139
sharils <sharils@users.noreply.github.com>
2018-05-07 21:35:46 +0800

    Add alias for docker-compose down --volumes

--- .bash_it_custom/aliases/docker-compose.aliases.bash
+++ .bash_it_custom/aliases/docker-compose.aliases.bash
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 alias dcod="docker-compose down"
-alias dcorr="docker-compose run --rm --volume $PWD:$PWD --workdir $PWD"
-alias dcoru="docker-compose run --rm --user $(id -u):$(id -g) --volume $PWD:$PWD --workdir $PWD"
+alias dcodv="docker-compose down --volumes"
+alias dcorr='docker-compose run --rm --volume $PWD:$PWD --workdir $PWD'
+alias dcoru='docker-compose run --rm --user $(id -u):$(id -g) --volume $PWD:$PWD --workdir $PWD'
 alias dcou="docker-compose up"
-alias dcoud="docker-compose up -d"
+alias dcoud="docker-compose up --detach"
