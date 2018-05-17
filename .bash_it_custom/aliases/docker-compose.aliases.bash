8d202b598a50a1a94a99e4fb3ed8b4b2dbefce34
sharils <sharils@users.noreply.github.com>
2018-05-17 21:49:45 +0800

    Add dcoe

--- .bash_it_custom/aliases/docker-compose.aliases.bash
+++ .bash_it_custom/aliases/docker-compose.aliases.bash
@@ -6,3 +6,4 @@ alias dcorr='docker-compose run --rm --volume $PWD:$PWD --workdir $PWD'
 alias dcoru='docker-compose run --rm --user $(id -u):$(id -g) --volume $PWD:$PWD --workdir $PWD'
 alias dcou="docker-compose up"
 alias dcoud="docker-compose up --detach"
+alias dcoue="docker-compose exec"
