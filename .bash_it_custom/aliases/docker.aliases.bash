dfa92cd0424f52ff10733269ea3bad5cfb7599ca
sharils <sharils@users.noreply.github.com>
2018-05-17 21:59:12 +0800

    Add dkbs

--- .bash_it_custom/aliases/docker.aliases.bash
+++ .bash_it_custom/aliases/docker.aliases.bash
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 alias dk="docker"
-alias dkb='docker build --tag $USER/$(basename $PWD) .'
-alias dkbr='docker build --tag $USER/$(basename $PWD) . && docker run --interactive --rm --tty $USER/$(basename $PWD)'
+alias dkb='docker build --tag ${USER}/$(basename $PWD) .'
+alias dkbr='docker build --tag ${USER}/$(basename $PWD) . && docker run --interactive --name ${USER}_$(basename $PWD) --rm --tty ${USER}/$(basename $PWD)'
+alias dkbs='docker stop ${USER}_$(basename $PWD)'
 alias dkrr='docker run --interactive --rm --tty --volume $PWD:$PWD --workdir $PWD'
 alias dkru='docker run --interactive --rm --tty --user $UID:$(id -g) --volume $PWD:$PWD --workdir $PWD'
 
