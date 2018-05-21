d9ae3a7adbfdafc494c3a61ee8c102ceffc966e7
sharils <sharils@users.noreply.github.com>
2018-05-21 21:38:23 +0800

    Fix dcoe

--- .bash_it_custom/aliases/docker-compose.aliases.bash
+++ .bash_it_custom/aliases/docker-compose.aliases.bash
@@ -2,8 +2,8 @@
 
 alias dcod="docker-compose down"
 alias dcodv="docker-compose down --volumes"
+alias dcoe="docker-compose exec"
 alias dcorr='docker-compose run --rm --volume $PWD:$PWD --workdir $PWD'
 alias dcoru='docker-compose run --rm --user $(id -u):$(id -g) --volume $PWD:$PWD --workdir $PWD'
 alias dcou="docker-compose up"
 alias dcoud="docker-compose up --detach"
-alias dcoue="docker-compose exec"
