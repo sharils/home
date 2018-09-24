a224c8b963b343e59f1c2e5c69898207c30ac9bc
sharils <sharils@users.noreply.github.com>
2018-09-24 22:40:00 +0800

    Update tig

--- .bash_it_custom/aliases/tig.aliases.bash
+++ .bash_it_custom/aliases/tig.aliases.bash
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 alias tl='tig'
+alias td='git diff | tig'
+alias tds='git diff --staged | tig'
 alias tll='tig --all'
-alias trf='tig refs'
+alias tb='tig refs'
+alias tbl='tig blame $(fzf)'
+alias ts='tig status'
 alias tst='tig stash'
