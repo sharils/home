9d08d3b0cb09c5a373c2d40438661e4bc671e169
sharils <sharils@users.noreply.github.com>
2018-10-14 10:41:33 +0800

    Clean up

--- .bash_it_custom/plugins/thefuck.plugins.bash
+++
@@ -1,5 +0,0 @@
-# Add the following to your .bash_profile, .bashrc or .zshrc:
-
-eval $(thefuck --alias dude)
-
-# For other shells, check https://github.com/nvbn/thefuck/wiki/Shell-aliases
--- .bash_it_custom/plugins/vim.plugins.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-vim-install() {
-    $(
-        cd ~/.vim/
-        git submodule add $* "bundle/$(basename $* .git)"
-    )
-}
