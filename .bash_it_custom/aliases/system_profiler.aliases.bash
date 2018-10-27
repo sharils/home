75c16b95f4c815ba4ebaff2ac7aa94265adde896
sharils <sharils@users.noreply.github.com>
2018-10-27 08:41:30 +0800

    Add spb

--- .bash_it_custom/aliases/system_profiler.aliases.bash
+++ .bash_it_custom/aliases/system_profiler.aliases.bash
@@ -1,2 +1,2 @@
 # shellcheck disable=SC2139
-alias bi=$'system_profiler SPPowerDataType | yq \'.Power["Battery Information"] | { "Charge Information": .["Charge Information"], "Health Information": .["Health Information"], "Battery Installed": .["Battery Installed"] }\''
+alias spb=$'system_profiler SPPowerDataType | yq \'.Power["Battery Information"] | { "Charge Information": .["Charge Information"], "Health Information": .["Health Information"], "Battery Installed": .["Battery Installed"] }\''
