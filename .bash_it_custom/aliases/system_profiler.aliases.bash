1528f7ddff3664895ffe9a98129541533bd07805
sharils <sharils@users.noreply.github.com>
2018-10-27 08:48:49 +0800

    Add sp alias

--- .bash_it_custom/aliases/system_profiler.aliases.bash
+++ .bash_it_custom/aliases/system_profiler.aliases.bash
@@ -1,2 +1,4 @@
+alias sp='system_profiler'
+alias spls='system_profiler -listDataTypes'
 # shellcheck disable=SC2139
 alias spb=$'system_profiler SPPowerDataType | yq \'.Power["Battery Information"] | { "Charge Information": .["Charge Information"], "Health Information": .["Health Information"], "Battery Installed": .["Battery Installed"] }\''
