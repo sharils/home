fc84eaa5a6cb7bd4fa36895f665d859e4c138221
sharils <sharils@users.noreply.github.com>
2018-10-29 09:09:00 +0800

    Update

--- .bash_it_custom/aliases/system_profiler.aliases.bash
+++ .bash_it_custom/aliases/system_profiler.aliases.bash
@@ -1,5 +1,6 @@
 alias sp='system_profiler'
 alias spls='system_profiler -listDataTypes'
-alias spp='system_profiler SPPowerDataType'
+# shellcheck disable=SC2139
+alias spp=$'system_profiler SPPowerDataType | yq \'.Power\''
 # shellcheck disable=SC2139
 alias sppb=$'system_profiler SPPowerDataType | yq \'.Power["Battery Information"]\''
