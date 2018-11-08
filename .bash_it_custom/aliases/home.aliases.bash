1ef2b65bceb3661ec787ba81209d7c5f6e280aca
sharils <sharils@users.noreply.github.com>
2018-11-08 09:16:06 +0800

    Make homeedit easier

--- .bash_it_custom/aliases/home.aliases.bash
+++ .bash_it_custom/aliases/home.aliases.bash
@@ -1,2 +1,2 @@
-alias homeedit='vim ~/git/github.com/sharils/home/install'
+alias homedit='vim ~/git/github.com/sharils/home/install'
 alias homepull='cd ~/git/github.com/sharils/home && git pull --prune && vim +PlugInstall && cd - && bash-it reload'
