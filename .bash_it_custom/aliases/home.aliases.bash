52c1c7a53e588b53a23822a8c751b0857f06f6ea
sharils <sharils@users.noreply.github.com>
2018-11-19 22:51:13 +0800

    Clean up before pulling home

--- .bash_it_custom/aliases/home.aliases.bash
+++ .bash_it_custom/aliases/home.aliases.bash
@@ -1,3 +1,3 @@
 alias homecd='cd  ~/git/github.com/sharils/home/'
 alias homedit='vim ~/git/github.com/sharils/home/install'
-alias homepull='cd ~/git/github.com/sharils/home && git pull --prune && vim +PlugInstall && cd - && bash-it reload'
+alias homepull='cd ~/git/github.com/sharils/home && git checkout . && git pull --prune && vim +PlugInstall && cd - && bash-it reload'
