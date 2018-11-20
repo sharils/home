5cda7a036931fc7c4baad95ee06ce7ac606612d6
sharils <sharils@users.noreply.github.com>
2018-11-20 21:04:20 +0800

    Add homepush

--- .bash_it_custom/aliases/home.aliases.bash
+++ .bash_it_custom/aliases/home.aliases.bash
@@ -1,3 +1,4 @@
 alias homecd='cd  ~/git/github.com/sharils/home/'
 alias homedit='vim ~/git/github.com/sharils/home/install'
 alias homepull='cd ~/git/github.com/sharils/home && git checkout . && git pull --prune && vim +PlugInstall && cd - && bash-it reload'
+alias homepush='cd ~/git/github.com/sharils/home && git push && cd -'
