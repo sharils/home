39635c665df5ec816573c6b54817149d9570a626
sharils <sharils@users.noreply.github.com>
2019-01-09 11:27:28 +0800

    Fix homepull

--- .bash_it_custom/aliases/home.aliases.bash
+++ .bash_it_custom/aliases/home.aliases.bash
@@ -1,4 +1,4 @@
 alias homecd='cd  ~/git/github.com/sharils/home/'
 alias homedit='vim ~/git/github.com/sharils/home/install'
-alias homepull='cd ~/git/github.com/sharils/home && git checkout . && git pull --prune && vim +PlugInstall && cd - && bash-it reload'
+alias homepull='cd ~/git/github.com/sharils/home && git checkout . && git pull --prune && vim -S ~/.vim/snapshot.vim && cd - && bash-it reload'
 alias homepush='cd ~/git/github.com/sharils/home && git push && cd -'
