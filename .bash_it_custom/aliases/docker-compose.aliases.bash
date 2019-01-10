4e8d4becb72d8c1712a6740ad375828316e6336f
sharils <sharils@users.noreply.github.com>
2019-01-10 20:32:37 +0800

    Add dcoub

--- .bash_it_custom/aliases/docker-compose.aliases.bash
+++ .bash_it_custom/aliases/docker-compose.aliases.bash
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-alias dcob="docker-compose build"
-alias dcod="docker-compose down"
-alias dcodv="docker-compose down --volumes"
-alias dcoe="docker-compose exec"
+alias dcob='docker-compose build'
+alias dcod='docker-compose down'
+alias dcodv='docker-compose down --volumes'
+alias dcoe='docker-compose exec'
 alias dcorr='docker-compose run --rm --volume $PWD:$PWD --workdir $PWD'
 alias dcoru='docker-compose run --rm --user $(id -u):$(id -g) --volume $PWD:$PWD --workdir $PWD'
-alias dcou="docker-compose up"
-alias dcoud="docker-compose up --detach"
+alias dcou='docker-compose up'
+alias dcoub='docker-compose up --build'
+alias dcoud='docker-compose up --detach'
