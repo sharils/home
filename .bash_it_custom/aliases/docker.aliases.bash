2873ce0d0859a7b89cec5dd2e559cd76c0a00a70
sharils <sharils@users.noreply.github.com>
2019-01-27 12:47:58 +0800

    Add speedtest-cli

--- .bash_it_custom/aliases/docker.aliases.bash
+++ .bash_it_custom/aliases/docker.aliases.bash
@@ -20,5 +20,6 @@ alias dkp='docker push'
 
 alias bfg='docker run --interactive --rm --tty --volume $PWD:/home/nikovirtala nikovirtala/bfg'
 alias dhttp='docker run --interactive --rm --tty alpine/httpie'
+alias speedtest-cli='docker run --rm moutten/speedtest-cli'
 alias toodles='docker run --interactive --rm --tty --publish 9001:9001 --volume $PWD:/repo aviaviavi/toodles'
 alias wraith='docker run --interactive --rm --tty bbcnews/wraith'
