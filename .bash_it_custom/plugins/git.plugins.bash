87a9c098afcdb2f21cf159bc12678f5992a3c205
sharils <sharils@users.noreply.github.com>
2019-02-11 20:06:53 +0800

    Add gclt1

--- .bash_it_custom/plugins/git.plugins.bash
+++ .bash_it_custom/plugins/git.plugins.bash
@@ -3,5 +3,11 @@
 gclt() {
     filename="$(basename "$1")"
     dir="/tmp/${filename%.*}"
+    git clone "$1" "$dir" && cd "$dir" || exit
+}
+
+gcl1t() {
+    filename="$(basename "$1")"
+    dir="/tmp/${filename%.*}"
     git clone --depth=1 "$1" "$dir" && cd "$dir" || exit
 }
