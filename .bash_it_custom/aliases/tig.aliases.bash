e3f58fe3fd805250affd7cb6ceb01d9f73ccc337
sharils <3063067+sharils@users.noreply.github.com>
2019-04-20 15:51:20 +0800

    Pump vim plugin verions

--- .bash_it_custom/aliases/tig.aliases.bash
+++ .bash_it_custom/aliases/tig.aliases.bash
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-alias tg='tig'
-alias tga='tig --all'
 alias tll='tig'
+alias tga='tig --all'
 alias tlla='tig --all'
 alias td='git diff | tig'
 alias tds='git diff --staged | tig'
-alias tb='tig refs'
-alias tt='tig refs'
+alias tba='tig refs'
 alias ts='tig status'
 alias tsh='tig show'
 alias tstl='tig stash'
--- .bash_it_custom/aliases/vim.aliases.bash
+++ .bash_it_custom/aliases/vim.aliases.bash
@@ -23,7 +23,8 @@ alias vnc='vim next.config.js'
 alias vpj='vim package.json'
 alias vrm='vim README.md'
 alias vsj='vim server.js'
-alias vtrc='vim .tigrc'
+alias vtgrc='vim ~/.tigrc'
+alias vtorc='vim ~/Library/Application\ Support/TorBrowser-Data/Tor/torrc'
 alias vvrc='vim ~/git/github.com/sharils/home/.vimrc'
 
 alias vt="vim /tmp/$USER-$RANDOM"
