2b916aafcd2697987b7d5cccd5e4763389cc9379
sharils <3063067+sharils@users.noreply.github.com>
2019-05-21 09:52:13 +0800

    Add mitmproxy

--- .bash_it_custom/aliases/docker.aliases.bash
+++ .bash_it_custom/aliases/docker.aliases.bash
@@ -21,6 +21,8 @@ alias dksd='docker system df'
 
 alias bfg='docker run --interactive --rm --tty --volume $PWD:/home/nikovirtala nikovirtala/bfg'
 alias http='docker run --interactive --rm --tty alpine/httpie'
+alias mitmproxy='docker run --interactive --rm --tty --publish 8080:8080 --publish 127.0.0.1:8081:8081 mitmweb --web-iface 0.0.0.0'
+alias mitmproxyv='docker run --interactive --rm --tty --publish 8080:8080 --publish 127.0.0.1:8081:8081 --volume ~/.mitmproxy:/home/mitmproxy/.mitmproxy mitmproxy/mitmproxy mitmweb --web-iface 0.0.0.0'
 alias speedtest-cli='docker run --rm moutten/speedtest-cli'
 alias swagger-codegen='docker run --interactive --rm --tty --volume ${PWD}:/${PWD} --workdir ${PWD} swaggerapi/swagger-codegen-cli generate'
 alias swagger-codegenes='docker run --interactive --rm --tty --volume ${PWD}:/${PWD} --workdir ${PWD} swaggerapi/swagger-codegen-cli generate --lang=javascript --additional-properties=usePromises=true,useES6=true'
