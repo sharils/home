bfa9988d1e8755867a6b123fedaf0ef77565a342
sharils <3063067+sharils@users.noreply.github.com>
2019-08-06 09:07:35 +0800

    Fix todoinit

--- .bash_it_custom/plugins/todo.plugins.bash
+++ .bash_it_custom/plugins/todo.plugins.bash
@@ -2,10 +2,12 @@
 export TODOTXT_DATE_ON_ADD=1
 
 todoinit() {
+    local SHARILS_HOME="$HOME/git/github.com/sharils/home"
     local TODO_CONFIG='.todo/config'
 
     # shellcheck disable=SC2016
-    cp -r "$HOME/.todo" . && \
+    mkdir -p "$(dirname $TODO_CONFIG)" && \
+    cp "$SHARILS_HOME/.todo/config" "$TODO_CONFIG" && \
     sed -i'.bak' $"/^export TODO_DIR/s@\$HOME@$PWD@; /^export TODO_DIR/s@$USER@\$USER@;" $TODO_CONFIG && \
     rm "$TODO_CONFIG.bak" && \
     echo 'export TODOTXT_CFG_FILE=$PWD/$TODO_CONFIG'
