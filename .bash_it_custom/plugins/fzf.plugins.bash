1b8ad5338bdd90feab4f1dd98696ba6ae1b1aad1
sharils <3063067+sharils@users.noreply.github.com>
2019-08-14 08:21:23 +0800

    Fix alt-c

--- .bash_it_custom/plugins/fzf.plugins.bash
+++ .bash_it_custom/plugins/fzf.plugins.bash
@@ -1,5 +1,6 @@
 export FZF_DEFAULT_COMMAND='fd --type f --exclude package-lock.json'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND='fd --type d'
 
 _fzf_compgen_path() {
    $FZF_DEFAULT_COMMAND "$1"
