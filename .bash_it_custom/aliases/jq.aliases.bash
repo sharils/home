a05e21419b48a70eedc0e960890673b405cb0bbb
sharils <3063067+sharils@users.noreply.github.com>
2019-11-03 21:30:07 +0800

    Add JQ_COLORS

--- .bash_it_custom/aliases/jq.aliases.bash
+++ .bash_it_custom/aliases/jq.aliases.bash
@@ -1,5 +1,9 @@
+export JQ_COLORS='1;33:0;35:0;35:0;36:0;38:0;37:0;37'
+
 alias jpR='jq --raw-input'
 alias jpS='jq --sort-keys'
 alias jpc='jq --compact-output'
-alias jpj='jq < package.json'
 alias jpr='jq --raw-output'
+
+alias jql='jq --color-output --sort-keys | less'
+alias jpj='jq < package.json'
