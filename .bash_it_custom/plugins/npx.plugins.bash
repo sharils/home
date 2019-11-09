0ffb1267e3c2e9a1cbd5c49627eaaebab7cf7958
sharils <3063067+sharils@users.noreply.github.com>
2019-11-09 17:06:03 +0800

    Add yo

--- .bash_it_custom/plugins/npx.plugins.bash
+++ .bash_it_custom/plugins/npx.plugins.bash
@@ -1,4 +1,4 @@
 # https://twitter.com/thatzackhawkins/status/893495424375341058?lang=en
 yo() {
-    npx --package yo --package "generator-${1%:*}" --call "yo $*"
+    pnpx --package yo --package "generator-${1%:*}" yo "$*"
 }
