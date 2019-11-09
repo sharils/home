7cc1f95231f0242c771ca55eb6f3bd47295fb6d9
sharils <3063067+sharils@users.noreply.github.com>
2019-11-09 10:29:31 +0800

    Add gcff and gcsf

--- .bash_it_custom/plugins/git.plugins.bash
+++ .bash_it_custom/plugins/git.plugins.bash
@@ -11,3 +11,16 @@ gcl1t() {
     dir="/tmp/${filename%.*}"
     git clone --depth=1 "$1" "$dir" && cd "$dir" || exit
 }
+
+_get_last_commit() {
+    filename="$(basename "$1")"
+    git log --pretty=format:'%H' -1 install
+}
+
+gcff() {
+    git commit --fixup "$(_get_last_commit "$1")"
+}
+
+gcsf() {
+    git commit --squash "$(_get_last_commit "$1")"
+}
