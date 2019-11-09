87c54627364486f432c8daba918ceb48ead12106
sharils <3063067+sharils@users.noreply.github.com>
2019-11-09 12:07:55 +0800

    Add random base64

--- .bash_it_custom/aliases/etc.aliases.bash
+++ .bash_it_custom/aliases/etc.aliases.bash
@@ -10,3 +10,7 @@ alias ex='elixir -e'
 alias js='node -e'
 alias phr='php -r'
 alias py='python -c'
+
+alias randombase36=$'base64 < /dev/random | tr -d \'+/[:upper:]\' | head -c 6'
+alias randombase62=$'base64 < /dev/random | tr -d \'+/\' | head -c 5'
+alias randombase64=$'base64 < /dev/random | tr \'+/\' \'-_\' | head -c 5'
