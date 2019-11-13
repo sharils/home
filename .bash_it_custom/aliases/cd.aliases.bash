fe3e8fc9ec1975aaea0dd5d3563fc72476074b2e
sharils <3063067+sharils@users.noreply.github.com>
2019-11-13 19:34:51 +0800

    Add random

--- .bash_it_custom/aliases/cd.aliases.bash
+++ .bash_it_custom/aliases/cd.aliases.bash
@@ -1,6 +1,7 @@
 # shellcheck disable=SC2139
+tmp="/tmp/$USER-$RANDOM"
 alias .ssh='cd ~/.ssh'
 alias dl='cd ~/Downloads'
 alias tmp='cd /tmp'
-alias mkcdtmp="mkdir /tmp/$USER-$RANDOM && cd !#:1"
+alias mkcdtmp="mkdir $tmp && cd $tmp"
 alias ~tmp='cd ~/tmp'
