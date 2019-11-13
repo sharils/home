a108354b5d9bdd751c5f8e7c0664996a9de0dccf
sharils <3063067+sharils@users.noreply.github.com>
2019-11-13 19:35:05 +0800

    Add mkcdtmp

--- .bash_it_custom/aliases/cd.aliases.bash
+++ .bash_it_custom/aliases/cd.aliases.bash
@@ -1,7 +1,7 @@
 # shellcheck disable=SC2139
-tmp="/tmp/$USER-$RANDOM"
 alias .ssh='cd ~/.ssh'
 alias dl='cd ~/Downloads'
 alias tmp='cd /tmp'
-alias mkcdtmp="mkdir $tmp && cd $tmp"
+tmp="/tmp/$USER-$RANDOM"
+alias mkcdtmp="mkdir -p $tmp && cd $tmp"
 alias ~tmp='cd ~/tmp'
