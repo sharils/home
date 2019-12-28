f2ab099e6960fe7f70a316f550131ef9bfbcc967
sharils <3063067+sharils@users.noreply.github.com>
2019-12-28 11:36:18 +0800

    Update npx

--- .bash_it_custom/aliases/npx.aliases.bash
+++ .bash_it_custom/aliases/npx.aliases.bash
@@ -1,6 +1,7 @@
 # Catch CSS curve balls
 alias backstopjs='pnpx backstopjs '
 
+# Live CSS Reload & Browser Syncing
 alias browser-sync='pnpx browser-sync '
 alias browser-syncw='pnpx browser-sync --no-open --watch'
 
@@ -10,6 +11,7 @@ alias bundle-phobia='pnpx bundle-phobia-cli '
 # Capacitor: Universal Web Applications
 alias cap='pnpx --package @capacitor/core --package @capacitor/cli cap '
 
+# The Canary in the Goldmine
 alias citgm='pnpx citgm'
 
 # Codecrumbs standalone: learn and share your code knowledge!
@@ -24,29 +26,34 @@ alias cost-of-modules='pnpx cost-of-modules '
 # `covgen` generates a code of conduct.
 alias covgen='pnpx covgen '
 
+# Create Next.js-powered React apps with one command
 alias create-next-app='pnpx create-next-app '
 
+# Create React apps with no build configuration.
 alias create-react-app='pnpx create-react-app '
 
+# Cypress.io end to end testing tool
 alias cypress='pnpx cypress '
 
+# An AST-based pattern checker for JavaScript.
 alias eslint='pnpx eslint '
 alias eslintf='pnpx eslint --fix .'
 
+# Automatically fetch gitignore files for any project type from github into your new project
 alias gitignore='pnpx gitignore '
 alias gitignorejs='pnpx gitignore node'
 
 # Utility to annotate console logging statements with timestamps and find slow processes
 alias gnomon='pnpx gnomon'
 
+# An opinionated code style agnostic linter – a friendly companion to Prettier.
 alias healthier='pnpx healthier '
 
 # 💡 A hinting engine for the web https://webhint.io/
 alias hint='pnpx hint'
 
+# HTTP server for debugging clients.
 alias httpbin='pnpx npx @buzuli/httpbin '
-
-alias insomniac='pnpx insomniac '
 
 # finds publicly known security vulnerabilities in a website's frontend JavaScript libraries
 alias is-website-vulnerable='is-website-vulnerable'
@@ -54,38 +61,55 @@ alias is-website-vulnerable='is-website-vulnerable'
 # 😎 Supercharged JavaScript REPL
 alias jay='pnpx jay-repl '
 
+# A toolkit for JavaScript codemods
 alias jscodeshift='pnpx jscodeshift '
 
+# Copy/paste detector for programming code, support JavaScript, CoffeeScript, PHP, Ruby, Python, Less, Go, Java, Yaml, C#, C++, C, Puppet, Twig languages
 alias jscpd='pnpx jscpd '
 
+# Detect structural similarities in your code
 alias jsinspect='pnpx jsinspect '
 
+# Kill processes occupying a port
 alias krampus='pnpx krampus '
 
+# A tool for managing JavaScript projects with multiple packages.
 alias lerna='pnpx lerna '
 
+# Check license info for a package
 alias license-checker='pnpx license-checker '
 
+# A tool to generate common FOSS LICENSE files
 alias license='pnpx license '
 
+# Lighthouse analyzes web apps and web pages, collecting modern performance metrics and insights on developer best practices.
 alias lighthouse='pnpx lighthouse '
 
 # Create graphs from your CommonJS, AMD or ES6 module dependencies
 alias madge='pnpx madge'
 
+# A CLI to help better understand the massive amount of information that surrounds Node.js versions.
 alias nanoprobe='pnpx nanoprobe'
 
+# Find newer versions of dependencies than what your package.json or bower.json allows
 alias ncu='pnpx npm-check-updates '
 
+# Easily find and remove old and heavy node_modules folders ✨
+alias npkill='pnpx npkill '
+
+# Create and build modern JavaScript applications with zero initial configuration
 alias neutrinojs-project='pnpx @neutrinojs/create-project '
 
+# Command-line companion utility for Postman
 alias newman='pnpx newman '
 
+# A command line tool for validating nginx files for style and common pitfalls.
 alias nginx-linter='pnpx nginx-linter '
 
 # Easily find and remove old and heavy node_modules folders ✨
 alias npkill='pnpx npkill '
 
+# Check for outdated, incorrect, and unused dependencies.
 alias npm-check='pnpx npm-check '
 
 # npm-merge-driver(1) -- git merge driver for automatic merging of lockfiles
@@ -94,31 +118,42 @@ alias npm-merge-driver='pnpx npm-merge-driver '
 # Run any Node.js version
 alias nve='pnpx nve '
 
+# A toolkit for React, Preact & Inferno apps, React libraries and other npm modules for the web, with no configuration (until you need it)
 alias nwb='pnpx nwb '
 
+# @pika/pack helps you build amazing packages without the hassle:
 alias pack='pnpx @pika/pack '
 
+# ⚖️ Find the cost of adding a new dependency to your project https://packagephobia.now.sh
 alias packagephobia-cli='pnpx packagephobia-cli '
 
+# Parser generator for JavaScript
 alias pegjs='pnpx pegjs'
 
+# Prettier formatter for package.json files
 alias prettier-package-json='pnpx prettier-package-json'
 alias prettier-package-jsonw='pnpx prettier-package-json --write'
 
+# Prettier is an opinionated code formatter
 alias prettier='pnpx prettier '
 alias prettierjs=$'pnpx prettier --write \'*.js\' \'**/*.js\' '
 
 # ⚡ Get Pretty Quick https://npm.im/pretty-quick
 alias pretty-quick='pnpx pretty-quick'
 
+# CLI that generates beautiful README.md files.
 alias readme-md-generator='pnpx readme-md-generator'
 
+# A framework for building native apps using React
 alias react-native='pnpx react-native '
 
+# The JSON Resume command line interface
 alias resume='pnpx resume-cli '
 
+# Run Selenium IDE projects in cli
 alias selenium-side-runner='pnpx selenium-side-runner '
 
+# Static file serving and directory listing
 alias serve='pnpx serve '
 
 # Hauls your node.js source files around
@@ -134,6 +169,8 @@ alias unsplash-so-fancy='pnpx unsplash-so-fancy '
 # NPM_CONFIG_REGISTRY=http://localhost:4873 npm i
 alias verdaccio='pnpx verdaccio'
 
+# 📦🐈 Fast, reliable, and secure dependency management.
 alias yarn='pnpx yarn'
 
+# Zero configuration web framework.
 alias zero='pnpx zero '
