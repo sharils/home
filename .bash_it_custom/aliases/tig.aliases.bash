8908d59edf058a0fa8b0e09a4fbc45be74e65d37
sharils <3063067+sharils@users.noreply.github.com>
2020-02-21 21:07:43 +0800

    Add tig

--- .bash_it_custom/aliases/tig.aliases.bash
+++ .bash_it_custom/aliases/tig.aliases.bash
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
 alias tll='tig'
+alias tlog='tig log'
+alias tsh='tig show'
+alias trl='tig reflog'
+alias tbl='tig blame'
+alias tgr='tig grep'
+alias tba='tig refs'
+alias tst='tig stash'
+alias ts='tig status'
+
 alias tlla='tig --all'
 alias td='git diff | tig'
 alias tds='git diff --staged | tig'
-alias tba='tig refs'
-alias ts='tig status'
-alias tsh='tig show'
-alias tstl='tig stash'
 
-alias tbl='tig blame $(fzf)'
