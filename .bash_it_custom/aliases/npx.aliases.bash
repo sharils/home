769a2dfb8debd80516f528a78b50c58fa87f6cfd
sharils <3063067+sharils@users.noreply.github.com>
2020-03-28 19:41:19 +0800

    Change pnpx to npx

--- .bash_it_custom/aliases/npx.aliases.bash
+++ .bash_it_custom/aliases/npx.aliases.bash
@@ -1,183 +1,183 @@
 # Catch CSS curve balls
-alias backstopjs='pnpx backstopjs '
+alias backstopjs='npx backstopjs '
 
 # Live CSS Reload & Browser Syncing
-alias browser-sync='pnpx browser-sync '
-alias browser-syncw='pnpx browser-sync --no-open --watch'
+alias browser-sync='npx browser-sync '
+alias browser-syncw='npx browser-sync --no-open --watch'
 
 # BundlePhobia ❘ cost of adding a npm package
-alias bundle-phobia='pnpx bundle-phobia-cli '
+alias bundle-phobia='npx bundle-phobia-cli '
 
 # ✨ Super Cool NPM Playground right on the Node REPL ✨🇱🇰🇱🇰
-alias baapan='pnpx baapan '
+alias baapan='npx baapan '
 
 # Capacitor: Universal Web Applications
-alias cap='pnpx --package @capacitor/core --package @capacitor/cli cap '
+alias cap='npx --package @capacitor/core --package @capacitor/cli cap '
 
 # The Canary in the Goldmine
-alias citgm='pnpx citgm'
+alias citgm='npx citgm'
 
 # Codecrumbs standalone: learn and share your code knowledge!
-alias codecrumbs='pnpx codecrumbs '
+alias codecrumbs='npx codecrumbs '
 
 # GraphQL code generator with flexible support for custom plugins and templates
 # https://graphql-code-generator.com
-alias graphql-codegen='pnpx -p graphql -p @graphql-codegen/cli -c graphql-codegen'
+alias graphql-codegen='npx -p graphql -p @graphql-codegen/cli -c graphql-codegen'
 
 # All importers & exporters for CodeSandbox
-alias codesandbox='pnpx codesandbox'
+alias codesandbox='npx codesandbox'
 
 # Find out which of your dependencies is slowing you down
-alias cost-of-modules='pnpx cost-of-modules '
+alias cost-of-modules='npx cost-of-modules '
 
 # `covgen` generates a code of conduct.
-alias covgen='pnpx covgen '
+alias covgen='npx covgen '
 
 # Create Next.js-powered React apps with one command
-alias create-next-app='pnpx create-next-app '
+alias create-next-app='npx create-next-app '
 
 # Create React apps with no build configuration.
-alias create-react-app='pnpx create-react-app '
+alias create-react-app='npx create-react-app '
 
 # Cypress.io end to end testing tool
-alias cypress='pnpx cypress '
+alias cypress='npx cypress '
 
 # An AST-based pattern checker for JavaScript.
-alias eslint='pnpx eslint '
-alias eslintf='pnpx eslint --fix .'
+alias eslint='npx eslint '
+alias eslintf='npx eslint --fix .'
 
 # Automatically fetch gitignore files for any project type from github into your new project
-alias gitignore='pnpx gitignore '
-alias gitignorejs='pnpx gitignore node'
+alias gitignore='npx gitignore '
+alias gitignorejs='npx gitignore node'
 
 # Utility to annotate console logging statements with timestamps and find slow processes
-alias gnomon='pnpx gnomon'
+alias gnomon='npx gnomon'
 
 # An opinionated code style agnostic linter – a friendly companion to Prettier.
-alias healthier='pnpx healthier '
+alias healthier='npx healthier '
 
 # 💡 A hinting engine for the web https://webhint.io/
-alias hint='pnpx hint'
+alias hint='npx hint'
 
 # HTTP server for debugging clients.
-alias httpbin='pnpx npx @buzuli/httpbin '
+alias httpbin='npx npx @buzuli/httpbin '
 
 # finds publicly known security vulnerabilities in a website's frontend JavaScript libraries
 alias is-website-vulnerable='is-website-vulnerable'
 
 # 😎 Supercharged JavaScript REPL
-alias jay='pnpx jay-repl '
+alias jay='npx jay-repl '
 
 # A toolkit for JavaScript codemods
-alias jscodeshift='pnpx jscodeshift '
+alias jscodeshift='npx jscodeshift '
 
 # Copy/paste detector for programming code, support JavaScript, CoffeeScript, PHP, Ruby, Python, Less, Go, Java, Yaml, C#, C++, C, Puppet, Twig languages
-alias jscpd='pnpx jscpd '
+alias jscpd='npx jscpd '
 
 # Detect structural similarities in your code
-alias jsinspect='pnpx jsinspect '
+alias jsinspect='npx jsinspect '
 
 # Kill processes occupying a port
-alias krampus='pnpx krampus '
+alias krampus='npx krampus '
 
 # A tool for managing JavaScript projects with multiple packages.
-alias lerna='pnpx lerna '
+alias lerna='npx lerna '
 
 # Check license info for a package
-alias license-checker='pnpx license-checker '
+alias license-checker='npx license-checker '
 
 # A tool to generate common FOSS LICENSE files
-alias license='pnpx license '
+alias license='npx license '
 
 # Lighthouse analyzes web apps and web pages, collecting modern performance metrics and insights on developer best practices.
-alias lighthouse='pnpx lighthouse '
+alias lighthouse='npx lighthouse '
 
 # Create graphs from your CommonJS, AMD or ES6 module dependencies
-alias madge='pnpx madge'
+alias madge='npx madge'
 
 # A CLI to help better understand the massive amount of information that surrounds Node.js versions.
-alias nanoprobe='pnpx nanoprobe'
+alias nanoprobe='npx nanoprobe'
 
 # Find newer versions of dependencies than what your package.json or bower.json allows
-alias ncu='pnpx npm-check-updates '
+alias ncu='npx npm-check-updates '
 
 # Easily find and remove old and heavy node_modules folders ✨
-alias npkill='pnpx npkill '
+alias npkill='npx npkill '
 
 # Create and build modern JavaScript applications with zero initial configuration
-alias neutrinojs-project='pnpx @neutrinojs/create-project '
+alias neutrinojs-project='npx @neutrinojs/create-project '
 
 # Command-line companion utility for Postman
-alias newman='pnpx newman '
+alias newman='npx newman '
 
 # A command line tool for validating nginx files for style and common pitfalls.
-alias nginx-linter='pnpx nginx-linter '
+alias nginx-linter='npx nginx-linter '
 
 # Easily find and remove old and heavy node_modules folders ✨
-alias npkill='pnpx npkill '
+alias npkill='npx npkill '
 
 # Check for outdated, incorrect, and unused dependencies.
-alias npm-check='pnpx npm-check '
+alias npm-check='npx npm-check '
 
 # npm-merge-driver(1) -- git merge driver for automatic merging of lockfiles
-alias npm-merge-driver='pnpx npm-merge-driver '
+alias npm-merge-driver='npx npm-merge-driver '
 
 # Run any Node.js version
-alias nve='pnpx nve '
+alias nve='npx nve '
 
 # A toolkit for React, Preact & Inferno apps, React libraries and other npm modules for the web, with no configuration (until you need it)
-alias nwb='pnpx nwb '
+alias nwb='npx nwb '
 
 # @pika/pack helps you build amazing packages without the hassle:
-alias pack='pnpx @pika/pack '
+alias pack='npx @pika/pack '
 
 # ⚖️ Find the cost of adding a new dependency to your project https://packagephobia.now.sh
-alias packagephobia-cli='pnpx packagephobia-cli '
+alias packagephobia-cli='npx packagephobia-cli '
 
 # Parser generator for JavaScript
-alias pegjs='pnpx pegjs'
+alias pegjs='npx pegjs'
 
 # Prettier formatter for package.json files
-alias prettier-package-json='pnpx prettier-package-json'
-alias prettier-package-jsonw='pnpx prettier-package-json --write'
+alias prettier-package-json='npx prettier-package-json'
+alias prettier-package-jsonw='npx prettier-package-json --write'
 
 # Prettier is an opinionated code formatter
-alias prettier='pnpx prettier '
-alias prettierjs=$'pnpx prettier --write \'*.js\' \'**/*.js\' '
+alias prettier='npx prettier '
+alias prettierjs=$'npx prettier --write \'*.js\' \'**/*.js\' '
 
 # ⚡ Get Pretty Quick https://npm.im/pretty-quick
-alias pretty-quick='pnpx pretty-quick'
+alias pretty-quick='npx pretty-quick'
 
 # CLI that generates beautiful README.md files.
-alias readme-md-generator='pnpx readme-md-generator'
+alias readme-md-generator='npx readme-md-generator'
 
 # A framework for building native apps using React
-alias react-native='pnpx react-native '
+alias react-native='npx react-native '
 
 # The JSON Resume command line interface
-alias resume='pnpx resume-cli '
+alias resume='npx resume-cli '
 
 # Run Selenium IDE projects in cli
-alias selenium-side-runner='pnpx selenium-side-runner '
+alias selenium-side-runner='npx selenium-side-runner '
 
 # Static file serving and directory listing
-alias serve='pnpx serve '
+alias serve='npx serve '
 
 # Hauls your node.js source files around
-alias trucker='pnpx trucker '
+alias trucker='npx trucker '
 
 # Quickly try out npm packages inside a container.
-alias try='pnpx try-pkg'
+alias try='npx try-pkg'
 
 # Automatically change your desktop image every 21600 seconds
-alias unsplash-so-fancy='pnpx unsplash-so-fancy '
+alias unsplash-so-fancy='npx unsplash-so-fancy '
 
 # 📦🔐A lightweight private npm proxy registry https://www.verdaccio.org/
 # NPM_CONFIG_REGISTRY=http://localhost:4873 npm i
-alias verdaccio='pnpx verdaccio'
+alias verdaccio='npx verdaccio'
 
 # 📦🐈 Fast, reliable, and secure dependency management.
-alias yarn='pnpx yarn'
+alias yarn='npx yarn'
 
 # Zero configuration web framework.
-alias zero='pnpx zero '
+alias zero='npx zero '
