70d555b2c82941970effce435ae185aec9a8afd4
sharils <3063067+sharils@users.noreply.github.com>
2020-03-30 20:47:37 +0800

    Add update

--- .bash_it_custom/plugins/etc.plugins.bash
+++ .bash_it_custom/plugins/etc.plugins.bash
@@ -44,6 +44,13 @@ ascii-x() {
     echo ''
 }
 
+fibonacci() {
+    echo ''
+    echo '0 1 2 3 4 5 6  7  8  9 10 11  12  13  14  15  16   17   18   19   20'
+    echo '0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765'
+    echo ''
+}
+
 rfc4648-4() {
     echo ''
     echo '                      Table 1: The Base 64 Alphabet'
