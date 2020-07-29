2e150c0d87c23f988946016f102ff32d4697b318
sharils <3063067+sharils@users.noreply.github.com>
2020-07-29 13:16:16 +0800

    Rebase on pull

--- .bash_it_custom/aliases/home.aliases.bash
+++ .bash_it_custom/aliases/home.aliases.bash
@@ -1,6 +1,6 @@
 alias homecd='cd  ~/git/github.com/sharils/home/'
 alias homedit='vim ~/git/github.com/sharils/home/install'
-alias homepull='cd ~/git/github.com/sharils/home && git stash push && git pull --prune && vim -S ~/.vim/snapshot.vim && cd - && bash-it reload'
+alias homepull='cd ~/git/github.com/sharils/home && git stash push && git pull --prune --rebase && vim -S ~/.vim/snapshot.vim && cd - && bash-it reload'
 alias homepush='cd ~/git/github.com/sharils/home && git push && cd -'
 alias homeupdate='bash-it update && hr ♾ && rustup update && hr ♾ && brew upgrade'
 alias homecupdate='brew cask upgrade --greedy'
