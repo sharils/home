43b108fb73962d206c70658ff0b4aac4f8e85920
sharils <3063067+sharils@users.noreply.github.com>
2020-09-21 22:11:53 +0800

    Make fzf ui consistent

--- .bash_it_custom/plugins/fzf.plugins.bash
+++ .bash_it_custom/plugins/fzf.plugins.bash
@@ -1,6 +1,7 @@
 export FZF_DEFAULT_COMMAND='fd --type f --exclude package-lock.json'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND='fd --type d'
+export FZF_DEFAULT_OPTS='--layout=reverse-list'
 
 _fzf_compgen_path() {
    $FZF_DEFAULT_COMMAND "$1"
