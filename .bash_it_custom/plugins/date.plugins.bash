8eef39b3b2b6a69938e699c1c168b5f567a59d8d
sharils <3063067+sharils@users.noreply.github.com>
2022-10-29 19:22:33 +0800

    Update d

--- .bash_it_custom/plugins/date.plugins.bash
+++ .bash_it_custom/plugins/date.plugins.bash
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
 
 d() {
+  if [ $# -eq 0 ]; then
+    date
+    return
+  fi
+
   cmd="$1"
   shift
 
@@ -8,17 +13,25 @@ d() {
   I)
     date +%Y%m%dT%H%M%S%z | sed "s/00$//"
     ;;
+
   i)
     date +%FT%T%z | sed "s/00$//"
     ;;
+
   U)
     date -u +%Y%m%dT%H%M%SZ
     ;;
+
   u)
     date -u +%FT%TZ
     ;;
+
   s)
     date -u +%s
     ;;
+
+  *)
+    date "$@"
+
   esac
 }
