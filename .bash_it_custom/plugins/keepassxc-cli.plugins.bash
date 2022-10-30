b1e1bb5cc890c344b3ebc75dafac7a32615c5a62
sharils <3063067+sharils@users.noreply.github.com>
2022-10-30 09:49:08 +0800

    Fix format

--- .bash_it_custom/plugins/keepassxc-cli.plugins.bash
+++ .bash_it_custom/plugins/keepassxc-cli.plugins.bash
@@ -5,12 +5,14 @@ kp() {
   shift
 
   case $cmd in
-  w)
-    keepassxc-cli diceware --words 12
-    ;;
+
   g)
     # alias kpxcg=$'keepassxc-cli generate --lower --numeric --special --upper --every-group --length 40 --exclude \$\'"\\" #$%&+,/:;<=>?@[\]^``{|}"\''
     keepassxc-cli generate --lower --numeric --special --upper --every-group --length 40 --exclude ' "#$%&+,/:;<=>?@[\]^`{|}'
     ;;
+
+  w)
+    keepassxc-cli diceware --words 12
+
   esac
 }
