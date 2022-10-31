95e0787d82c7481e8f4a87e91e7c4e93ba09b97d
sharils <3063067+sharils@users.noreply.github.com>
2022-10-31 16:59:07 +0800

    Change v to strong random

--- .bash_it_custom/plugins/v.plugins.bash
+++ .bash_it_custom/plugins/v.plugins.bash
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 
-random="$(awk 'BEGIN { srand(); print int(rand()*32768) }')"
-
+temp=''
 v() {
   if [ $# -eq 0 ]; then
     if [ -s Session.vim ]; then
@@ -19,8 +18,10 @@ v() {
 
   c | css | csv | eex | erl | ex | exs | gql | html | java | jq | js | json | \
     jsx | lua | md | mdx | mw | pegjs | php | py | rs | scss | sh | sql | \
-    story | ts | tsx | txt | wat | xhtml | yaml | yml | zep)
-    vim "/tmp/$cmd-$USER-$random.$cmd"
+    story | t | ts | tsx | txt | wat | xhtml | yaml | yml | zep)
+    [ ! -d "$temp" ] && temp="$(mktemp -d "/tmp/$USER-XXXXXX")"
+    [ "$cmd" != 't' ] && cmd="$cmd.$cmd"
+    vim "$temp/$cmd"
     ;;
 
   R) vim README.md ;;
@@ -30,7 +31,6 @@ v() {
   gi) vim .gitignore ;;
   pj) vim package.json ;;
   rc) vim ~/git/github.com/sharils/home/.vimrc ;;
-  t) vim "/tmp/$USER-$random" ;;
 
   *) vim "$cmd" "$@" ;;
 
