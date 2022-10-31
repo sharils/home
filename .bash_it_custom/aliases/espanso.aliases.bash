cfcd5109d61909a178ecfa09b3cb904660991ab4
sharils <3063067+sharils@users.noreply.github.com>
2022-10-31 12:30:25 +0800

    Remove espanso aliases

--- .bash_it_custom/aliases/espanso.aliases.bash
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-alias ee='espanso edit '
-# alias eee='espanso edit emoji '
-# alias er='espanso restart '
-# alias es='espanso status '
---
+++ .bash_it_custom/plugins/es.plugins.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+es() {
+  cmd="${1:-edit}"
+  shift
+
+  case "$cmd" in
+  r) cmd=restart ;;
+  s) cmd=status ;;
+  esac
+
+  espanso "$cmd" "$@"
+}
