092fca5565a160f8c1670e768daaf196eb405142
sharils <3063067+sharils@users.noreply.github.com>
2022-10-31 13:14:51 +0800

    Fix w format

--- .bash_it_custom/plugins/w.plugins.bash
+++ .bash_it_custom/plugins/w.plugins.bash
@@ -6,8 +6,10 @@ w() {
 
   case $cmd in
 
-  ex) watchman-make --pattern "**/*.ex" "**/*.exs" --run "${*:-mix test && mix format}" ;;
-  js) watchman-make --pattern "**/*.js" "**/*.jsx" "**/*.ts" "**/*.tsx" --run "${*:-npm test}" ;;
+  ex) watchman-make --pattern "**/*.ex" "**/*.exs" \
+    --run "${*:-mix test && mix format}" ;;
+  js) watchman-make --pattern "**/*.js" "**/*.jsx" "**/*.ts" "**/*.tsx" \
+    --run "${*:-npm test}" ;;
   py) watchman-make --pattern "**/*.py" --run "$*" ;;
   rs) watchman-make --pattern "**/*.rs" --run "${*:-cargo test}" ;;
   *) watchman-make "$cmd" "$@" ;;
