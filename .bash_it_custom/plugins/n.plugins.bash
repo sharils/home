4424b51c3691191cfd41389e5973f797326c0bf9
sharils <3063067+sharils@users.noreply.github.com>
2022-11-02 11:22:20 +0800

    Fix n init args

--- .bash_it_custom/plugins/n.plugins.bash
+++ .bash_it_custom/plugins/n.plugins.bash
@@ -8,7 +8,7 @@ n() {
 
   browser-sync | bundle-phobia | baapan | clear-npx-cache | codesandbox | composerize | csv2json | cypress | eslint | gitignore | jscodeshift | jscpd | license | lighthouse | npm-check-updates | newman | nginx-linter | npkill | npm-check | npm-merge-driver | packagephobia-cli | pegjs | prettier | prettier-package-json | qrcode-terminal | react-native | selenium-side-runner | serve | trucker | vercel | wait-on) npx --yes "$cmd" "$@" ;;
 
-  next-app | react-app) npm init "$cmd" "$@" ;;
+  next-app | react-app) npm init "$cmd" -- "$@" ;;
 
   v) n vercel --token="${VERCEL_TOKEN:?}" "$@" ;;
   *) npm "$cmd" "$*" ;;
