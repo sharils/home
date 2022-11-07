89a7bf4e8697147190aca4791d6be3a90e88b3ba
sharils <3063067+sharils@users.noreply.github.com>
2022-11-07 09:20:47 +0800

    Fix missing -y for n init

--- .bash_it_custom/plugins/n.plugins.bash
+++ .bash_it_custom/plugins/n.plugins.bash
@@ -9,7 +9,7 @@ n() {
   browser-sync | bundle-phobia | baapan | clear-npx-cache | codesandbox | composerize | csv2json | cypress | eslint | gitignore | jscodeshift | jscpd | license | lighthouse | npm-check-updates | newman | nginx-linter | npkill | npm-check | npm-merge-driver | packagephobia-cli | pegjs | prettier | prettier-package-json | qrcode-terminal | react-native | selenium-side-runner | serve | trucker | vercel | wait-on) npx --yes "$cmd" "$@" ;;
 
   # n nx-workspace firebase --name orgname --preset next --appName appName --style css --nxCloud
-  next-app | nx-workspace | react-app | react-native-app) npm init "$cmd" -- "$@" ;;
+  next-app | nx-workspace | react-app | react-native-app) npm init --yes "$cmd" -- "$@" ;;
 
   v) n vercel --token="${VERCEL_TOKEN:?}" "$@" ;;
   *) npm "$cmd" "$*" ;;
