fb793fd146a21694115149a827f582a84c74fdcf
sharils <3063067+sharils@users.noreply.github.com>
2022-11-13 21:58:45 +0800

    Remove dead code

--- .bash_it_custom/aliases/_alias.aliases.bash
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-# alias agr='alias | grep --extended-regexp --ignore-case'
-# alias az='alias | fzf'
--- .bash_it_custom/aliases/flyctl.aliases.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-alias f='flyctl '
-# alias fal='flyctl auth login '
-# alias fl='flyctl launch '
-# alias fli='flyctl launch --image '
-# alias fdp='flyctl deploy '
-# alias fs='flyctl status '
-# alias fo='flyctl open '
