1dc768093a7680d7385bb1016474d231b757d711
sharils <3063067+sharils@users.noreply.github.com>
2022-11-13 21:32:47 +0800

    Change u v to e

--- .bash_it_custom/plugins/u.plugins.bash
+++ .bash_it_custom/plugins/u.plugins.bash
@@ -6,9 +6,9 @@ u() {
 
   case "$cmd" in
 
-  rm) rm -fr "$HOME/Library/Application Support/qutebrowser/webengine" ;;
+  e) "$EDITOR" "$HOME/.qutebrowser/bookmarks/urls" "$HOME/.qutebrowser/quickmarks" "$HOME/Library/Preferences/qutebrowser/autoconfig.yml" "$HOME/.qutebrowser/greasemonkey" ;;
 
-  v) vim -p "$HOME/.qutebrowser/bookmarks/urls" "$HOME/.qutebrowser/quickmarks" "$HOME/Library/Preferences/qutebrowser/autoconfig.yml" "$HOME/.qutebrowser/greasemonkey" ;;
+  rm) rm -fr "$HOME/Library/Application Support/qutebrowser/webengine" ;;
 
   *) echo 'not implemented yet' ;;
 
