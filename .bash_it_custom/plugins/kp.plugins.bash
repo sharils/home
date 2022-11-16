95a7b87ae9802e71e05041ca67424f2ad038e40d
sharils <3063067+sharils@users.noreply.github.com>
2022-11-16 18:57:20 +0800

    Change kp w to d

--- .bash_it_custom/plugins/kp.plugins.bash
+++ .bash_it_custom/plugins/kp.plugins.bash
@@ -6,14 +6,14 @@ kp() {
 
   case $cmd in
 
+  d) keepassxc-cli diceware --words "${@:-12}" | tr -d \\n ;;
+
   g)
     # alias kpxcg=$'keepassxc-cli generate --lower --numeric --special --upper
     # --every-group --length 40 --exclude \$\'"\\" #$%&+,/:;<=>?@[\]^``{|}"\''
     keepassxc-cli generate --lower --numeric --special --upper --every-group \
       --length "${@:-40}" --exclude ' "#$%&+,/:;<=>?@[\]^`{|}' | tr -d \\n
     ;;
-
-  w) keepassxc-cli diceware --words "${@:-12}" | tr -d \\n ;;
 
   esac
 }
