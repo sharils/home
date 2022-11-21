901dd123c2a846bb1c4be1467a545f7131362a40
sharils <3063067+sharils@users.noreply.github.com>
2022-11-21 16:26:13 +0800

    Fix v md, etc

--- shell_plugins/v.sh
+++ shell_plugins/v.sh
@@ -17,6 +17,7 @@ v() {
 
   c | css | csv | eex | erl | ex | exs | gql | html | java | jq | js | json | jsx | lua | md | mdx | mw | pegjs | php | py | rs | scss | sh | sql | story | t | ts | tsx | txt | wat | xhtml | yaml | yml | zep)
     [ "$cmd" != 't' ] && cmd="$cmd.$cmd"
+    z t_init
     vim "$(z t && echo "$PWD")/$cmd"
     ;;
 
