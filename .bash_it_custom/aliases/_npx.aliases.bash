52609461b52339be18e668db86a0f7800c9d54ac
sharils <3063067+sharils@users.noreply.github.com>
2022-11-21 22:01:47 +0800

    Remove codemod-cli

--- .bash_it_custom/aliases/_npx.aliases.bash
+++ .bash_it_custom/aliases/_npx.aliases.bash
@@ -6,9 +6,6 @@
 # Codecrumbs standalone: learn and share your code knowledge!
 #alias codecrumbs='npx --yes codecrumbs '
 
-# codemod-cli is a command line tool for generating, testing, and publishing codemods.
-#alias codemod-cli='npx --yes codemod-cli'
-
 # Generate a report about your development environment for debugging and issue reporting
 #alias envinfo='npx --yes envinfo'
 
