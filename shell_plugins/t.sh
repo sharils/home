2d620e47c0a4e0f6059efc31aaca659ebc01f020
sharils <3063067+sharils@users.noreply.github.com>
2022-11-21 21:12:28 +0800

    Remove t gd

--- shell_plugins/t.sh
+++ shell_plugins/t.sh
@@ -10,14 +10,6 @@ t() {
 
   e) $EDITOR ~/Sync/todo/todo.txt ~/Sync/todo/QuickNote.md ~/Sync/todo/infanoj.md ;;
 
-  gd)
-    todo.sh -p ls | while IFS= read -r todo; do
-      task="$(echo "$todo" | cut -d" " -f2-)"
-      grep --text "$task" ~/Sync/todo/done.txt
-      echo "$todo"
-    done
-    ;;
-
   h) todo.sh -vv help ;;
 
   p)
