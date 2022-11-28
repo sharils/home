6bebe8c43923e5d485b62998ba5fec8eead9baa7
sharils <3063067+sharils@users.noreply.github.com>
2022-11-28 20:58:53 +0800

    Refactor npx packages taking longer time to the end

--- .bash_it_custom/aliases/_npx.aliases.bash
+++ .bash_it_custom/aliases/_npx.aliases.bash
@@ -6,15 +6,6 @@
 # Codecrumbs standalone: learn and share your code knowledge!
 #alias codecrumbs='npx --yes codecrumbs '
 
-# Generate a report about your development environment for debugging and issue reporting
-#alias envinfo='npx --yes envinfo'
-
-# 💡 A hinting engine for the web https://webhint.io/
-#alias hint='npx --yes hint'
-
-# finds publicly known security vulnerabilities in a website's frontend JavaScript libraries
-#alias is-website-vulnerable='is-website-vulnerable'
-
 # Create graphs from your CommonJS, AMD or ES6 module dependencies
 # alias madge='npx --yes madge'
 
@@ -45,3 +36,12 @@
 
 # Zero configuration web framework.
 #alias zero='npx --yes zero '
+
+# Generate a report about your development environment for debugging and issue reporting
+#alias envinfo='npx --yes envinfo'
+
+# 💡 A hinting engine for the web https://webhint.io/
+#alias hint='npx --yes hint'
+
+# finds publicly known security vulnerabilities in a website's frontend JavaScript libraries
+#alias is-website-vulnerable='is-website-vulnerable'
