441915d2fc1e46bd947eb1f6991411e7d4cd3dd5
sharils <3063067+sharils@users.noreply.github.com>
2022-11-28 16:50:06 +0800

    Add dk b r

--- shell_plugins/dk.sh
+++ shell_plugins/dk.sh
@@ -7,7 +7,12 @@ dk() {
   case "$cmd" in
   b)
     name="$(basename "$PWD" | x hostnameise)"
-    dk build --tag "$USER/$name" "${@:-.}"
+    cmd="${1:-b}"
+    shift
+    case "$cmd" in
+    r) dk run --interactive --rm --tty --name "${USER}_$name" "$USER/$name" "$@" ;;
+    b | *) dk build --tag "$USER/$name" "${@:-.}" ;;
+    esac
     ;;
   o) open -b com.docker.docker ;;
   q) osascript -e 'quit app "Docker"' ;;
