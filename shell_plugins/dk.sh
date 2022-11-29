fb1aacb3471c14f16986042aa7adbe9cc594ce76
sharils <3063067+sharils@users.noreply.github.com>
2022-11-29 23:30:56 +0800

    Add dk copies

--- shell_plugins/dk.sh
+++ shell_plugins/dk.sh
@@ -18,6 +18,14 @@ dk() {
     *) dk build --tag "$tag" "${@:-.}" ;;
     esac
     ;;
+  copies)
+    z t_init
+    (
+      g clt --quiet "$PWD"
+      find -- * -type d -depth 0 -exec echo COPY {} /workdir/{} \;
+      echo "COPY $(find -- * ! -name CHANGELOG.md ! -name Dockerfile ! -name README.md ! -name LICENSE ! -name docker-compose.yml -type f -depth 0 -exec echo {} \+) /workdir"
+    )
+    ;;
   i) dk image "${@:-ls}" ;;
   o) open -b com.docker.docker ;;
   q) osascript -e 'quit app "Docker"' ;;
