a86f25cbf10c2afc5aa0c368fc1799fe69bb28c6
sharils <3063067+sharils@users.noreply.github.com>
2022-11-29 23:35:41 +0800

    Refactor dk i

--- shell_plugins/dk.sh
+++ shell_plugins/dk.sh
@@ -26,7 +26,13 @@ dk() {
       echo "COPY $(find -- * ! -name CHANGELOG.md ! -name Dockerfile ! -name README.md ! -name LICENSE ! -name docker-compose.yml -type f -depth 0 -exec echo {} \+) /workdir"
     )
     ;;
-  i) dk image "${@:-ls}" ;;
+  i)
+    cmd="${1:-ls}"
+    shift
+    case "$cmd" in
+      *) dk image "$cmd" "$@" ;;
+    esac
+    ;;
   o) open -b com.docker.docker ;;
   q) osascript -e 'quit app "Docker"' ;;
   s) dk system "$@" ;;
