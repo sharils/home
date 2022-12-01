8a996c9b1d4bbbbd03d83514c8f4d5e257ed79e4
sharils <3063067+sharils@users.noreply.github.com>
2022-12-01 09:50:13 +0800

    u h

--- shell_plugins/u.sh
+++ shell_plugins/u.sh
@@ -8,6 +8,11 @@ u() {
 
   e) $EDITOR "$HOME/.qutebrowser/bookmarks/urls" "$HOME/.qutebrowser/quickmarks" "$HOME/Library/Preferences/qutebrowser/autoconfig.yml" "$HOME/.qutebrowser/greasemonkey" ;;
 
+  h)
+    # shellcheck disable=SC2016
+    yq --raw-output --yaml-output --arg contains "$*" '.["bindings.default"].default' /Applications/qutebrowser.app/Contents/Resources/config/configdata.yml | grep "$*"
+    ;;
+
   rm) rm -fr "$HOME/Library/Application Support/qutebrowser/webengine" ;;
 
   *) echo 'not implemented yet' ;;
