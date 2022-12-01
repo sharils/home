2591ff63772a7f32f2d5450feb01fcbc3623aa0f
sharils <3063067+sharils@users.noreply.github.com>
2022-12-01 10:36:22 +0800

    Add u o

--- shell_plugins/u.sh
+++ shell_plugins/u.sh
@@ -13,6 +13,8 @@ u() {
     yq --raw-output --yaml-output --arg contains "$*" '.["bindings.default"].default' /Applications/qutebrowser.app/Contents/Resources/config/configdata.yml | grep "$*"
     ;;
 
+  o) open -b org.qt-project.Qt.QtWebEngineCore ;;
+
   rm) rm -fr "$HOME/Library/Application Support/qutebrowser/webengine" ;;
 
   *) echo 'not implemented yet' ;;
