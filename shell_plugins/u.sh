8437bec3c787939b7c52000579ffed32e98e892c
sharils <3063067+sharils@users.noreply.github.com>
2022-12-03 18:02:00 +0800

    Refactor u h

--- shell_plugins/u.sh
+++ shell_plugins/u.sh
@@ -8,10 +8,7 @@ u() {
 
   e) $EDITOR "$HOME/.qutebrowser/bookmarks/urls" "$HOME/.qutebrowser/quickmarks" "$HOME/Library/Preferences/qutebrowser/autoconfig.yml" "$HOME/.qutebrowser/greasemonkey" ;;
 
-  h)
-    # shellcheck disable=SC2016
-    yq --raw-output --yaml-output --arg contains "$*" '.["bindings.default"].default' /Applications/qutebrowser.app/Contents/Resources/config/configdata.yml | grep "$*"
-    ;;
+  h) sed -n '3527,3842p' /Applications/qutebrowser.app/Contents/Resources/config/configdata.yml | grep "$*" ;;
 
   o) open -b org.qt-project.Qt.QtWebEngineCore ;;
 
