695aaa0cc339bc741dd79af74b424480143534dd
sharils <3063067+sharils@users.noreply.github.com>
2022-12-03 20:29:23 +0800

    Refactor qutebrowser g;

---
+++ .qutebrowser/greasemonkey/google.js
@@ -0,0 +1,5 @@
+globalThis.google = () => {
+  const q = new URLSearchParams(location.search).get("q"),
+    g = new URL("https://www.google.com.tw/search?q=%s&hl=en");
+  g.searchParams.set("q", q), (location = g);
+};
