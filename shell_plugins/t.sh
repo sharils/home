c2286ec3ab434acdb3de1ef5753a5589b30c5340
sharils <3063067+sharils@users.noreply.github.com>
2022-12-04 15:17:42 +0800

    Change t oot to use $TOOT_USING

--- shell_plugins/t.sh
+++ shell_plugins/t.sh
@@ -33,7 +33,13 @@ t() {
 
   n) t oot notifications "${@:---reverse}" ;;
 
-  oot) toot "$@" ;;
+  oot)
+    if [ -n "$TOOT_USING" ]; then
+      toot "$@" --using "$TOOT_USING"
+    else
+      toot "$@"
+    fi
+    ;;
 
   p)
     case "$cmd" in
