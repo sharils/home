5c042178b057a6fc911275533062ce685fc94779
sharils <3063067+sharils@users.noreply.github.com>
2022-12-04 14:05:55 +0800

    Fix qutebrowser g;

--- .qutebrowser/greasemonkey/google.js
+++ .qutebrowser/greasemonkey/google.js
@@ -1,5 +1,5 @@
 globalThis.google = () => {
   const q = new URLSearchParams(location.search).get("q"),
     g = new URL("https://www.google.com.tw/search?q=%s&hl=en");
-  g.searchParams.set("q", q), (location = g);
+  g.searchParams.set("q", q), (globalThis.location = g);
 };
