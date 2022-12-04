5e6b1d15176ea8acc231688e7ba771d1b1310a12
sharils <3063067+sharils@users.noreply.github.com>
2022-12-04 12:41:46 +0800

    Fix ssh-keygen to save to id_ed25519

--- shell_plugins/x.sh
+++ shell_plugins/x.sh
@@ -55,7 +55,7 @@ x() {
 
   ssh-copy-id) ssh-copy-id -o PreferredAuthentications=password -i "$HOME/.ssh/id_rsa.pub" "$@" ;;
 
-  ssh-keygen) ssh-keygen -f ~/.ssh/id_rsa -t ed25519 -C "${@:?}" ;;
+  ssh-keygen) ssh-keygen -f ~/.ssh/id_ed25519 -t ed25519 -C "${@:?}" ;;
 
   tel) open "tel:$*" ;;
 
