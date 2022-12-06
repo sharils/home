a6a4c247e624cb342e942e196ebbdecc7ffbca11
sharils <3063067+sharils@users.noreply.github.com>
2022-12-06 21:45:13 +0800

    Add b <language>

--- shell_plugins/b.sh
+++ shell_plugins/b.sh
@@ -14,6 +14,7 @@ b() {
   i) brew install "$@" ;;
   ri) brew reinstall "$@" ;;
   out) brew outdated "$@" ;;
+  c | css | csv | eex | erl | ex | exs | gql | html | java | jq | js | json | jsx | lua | md | mdx | mw | pegjs | php | py | rs | scss | sh | sql | story | t | ts | tsx | txt | wat | xhtml | yaml | yml | zep) bat --language "$cmd" "$@" ;;
   *) brew "$cmd" "$@"
   esac
 
