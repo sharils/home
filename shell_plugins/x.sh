f09338ce50833a59ac5babb4e7037e819ffd1a24
sharils <3063067+sharils@users.noreply.github.com>
2022-12-08 10:32:17 +0800

    Add x 5424

--- shell_plugins/x.sh
+++ shell_plugins/x.sh
@@ -6,6 +6,8 @@ x() {
 
   case "$cmd" in
 
+  5424) open 'https://www.rfc-editor.org/rfc/rfc5424.html#page-11' ;;
+
   args) xargs -I_ -n1 "$@" ;;
 
   bc) bc --mathlib --expression="$*" ;;
@@ -74,59 +76,3 @@ x() {
 
   esac
 }
-
-# rfc54241() {
-#   cat <<'TXT'
-#
-#           Numerical             Facility
-#              Code
-#
-#               0             kernel messages
-#               1             user-level messages
-#               2             mail system
-#               3             system daemons
-#               4             security/authorization messages
-#               5             messages generated internally by syslogd
-#               6             line printer subsystem
-#               7             network news subsystem
-#               8             UUCP subsystem
-#               9             clock daemon
-#              10             security/authorization messages
-#              11             FTP daemon
-#              12             NTP subsystem
-#              13             log audit
-#              14             log alert
-#              15             clock daemon (note 2)
-#              16             local use 0  (local0)
-#              17             local use 1  (local1)
-#              18             local use 2  (local2)
-#              19             local use 3  (local3)
-#              20             local use 4  (local4)
-#              21             local use 5  (local5)
-#              22             local use 6  (local6)
-#              23             local use 7  (local7)
-#
-#               Table 1.  Syslog Message Facilities
-#
-# TXT
-# }
-
-# rfc54242() {
-#   cat <<'TXT'
-#
-#            Numerical         Severity
-#              Code
-#
-#               0       Emergency: system is unusable
-#               1       Alert: action must be taken immediately
-#               2       Critical: critical conditions
-#               3       Error: error conditions
-#               4       Warning: warning conditions
-#               5       Notice: normal but significant condition
-#               6       Informational: informational messages
-#               7       Debug: debug-level messages
-#
-#               Table 2. Syslog Message Severities
-#
-# TXT
-# }
