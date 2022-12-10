cf7d9a30d00dfb6a39e95795b22f4b75c134f887
sharils <3063067+sharils@users.noreply.github.com>
2022-12-10 08:36:18 +0800

    Add x html t

--- shell_plugins/x.sh
+++ shell_plugins/x.sh
@@ -58,7 +58,17 @@ EOF
 
   hostnameise) tr '[:upper:]' '[:lower:]' | sed -E 's/[^[:alnum:]]+/-/g' ;;
 
-  html) echo '<!doctype html><html><head><meta charset="utf-8" /><meta http-equiv="Content-type" content="text/html; charset=utf-8" /><meta name="viewport" content="width=device-width, initial-scale=1" /><title>Hello HTML</title></head><body><h1>Hello HTML</h1></body></html>' | tee index.html ;;
+  html)
+    cmd="$1"
+    shift
+    case "$cmd" in
+    t)
+      echo >&2 '==> index.html <=='
+      x html | tee index.html
+      ;;
+    *) echo '<!doctype html><html><head><meta charset="utf-8" /><meta http-equiv="Content-type" content="text/html; charset=utf-8" /><meta name="viewport" content="width=device-width, initial-scale=1" /><title>Hello HTML</title></head><body><h1>Hello HTML</h1></body></html>' ;;
+    esac
+    ;;
 
   js) node --print "$*" ;;
 
