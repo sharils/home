8c34cdc13472fa9a9ece7db768c8db624185aff6
sharils <3063067+sharils@users.noreply.github.com>
2022-12-10 09:16:04 +0800

    Change k b

--- shell_plugins/k.sh
+++ shell_plugins/k.sh
@@ -9,9 +9,12 @@ k() {
   b)
     crockfords_base32="0123456789ABCDEFGHJKMNPQRSTVWXYZ"
     keepassxc-cli generate --custom "$crockfords_base32" --length "${@:-4}" | tr -d \\n | pbcopy
+    pbpaste
     ;;
 
-  d) keepassxc-cli diceware --words "${@:-12}" | tr -d \\n | pbcopy ;;
+  d)
+    keepassxc-cli diceware --words "${@:-12}" | tr -d \\n | pbcopy
+    ;;
 
   g)
     no_url_encode="!'()*-.0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz~"
