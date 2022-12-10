0beca663a64ed10e9805e7a902e3cc2e32e9cde1
sharils <3063067+sharils@users.noreply.github.com>
2022-12-10 08:14:40 +0800

    Change UltiSnips html h

--- shell_plugins/x.sh
+++ shell_plugins/x.sh
@@ -58,6 +58,8 @@ EOF
 
   hostnameise) tr '[:upper:]' '[:lower:]' | sed -E 's/[^[:alnum:]]+/-/g' ;;
 
+  html) echo '<!doctype html><html><head><meta charset="utf-8" /><meta http-equiv="Content-type" content="text/html; charset=utf-8" /><meta name="viewport" content="width=device-width, initial-scale=1" /><title>Hello HTML</title></head><body><h1>Hello HTML</h1></body></html>' | tee index.html ;;
+
   js) node --print "$*" ;;
 
   log) log show --predicate 'eventMessage CONTAINS "unlockUIBecomesActive"' --last "${@:-30m}" ;;
