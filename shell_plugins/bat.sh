7e5b1310d8c4fbe9e706253b45af32d390bf9347
sharils <3063067+sharils@users.noreply.github.com>
2022-12-12 13:14:15 +0800

    Add bat theme

--- shell_plugins/bat.sh
+++ shell_plugins/bat.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
 # export BAT_PAGER='auto never always'
-# export BAT_THEME='bat --list-themes'
+export BAT_THEME='Monokai Extended'
+# export BAT_THEME='Coldark-Dark'
+# export BAT_THEME='Nord'
+# export BAT_THEME='1337'
 # export BAT_STYLE='plain'
