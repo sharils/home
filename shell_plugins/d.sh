bd322385e9f3e0a5ed3eca9bb024d92289ea7488
sharils <3063067+sharils@users.noreply.github.com>
2022-12-17 17:22:12 +0800

    Refactor d i

--- shell_plugins/d.sh
+++ shell_plugins/d.sh
@@ -9,7 +9,7 @@ d() {
   a) direnv allow ;;
   e) direnv edit . && chmod 600 .envrc ;;
   I) date +%Y%m%dT%H%M%S%z | sed "s/00$//" ;;
-  i) date +%FT%T%z | sed "s/00$//" ;;
+  i) date -Iseconds "$@" | sed "s/:00$//" ;;
   U) date -u +%Y%m%dT%H%M%SZ ;;
   u) date -u +%FT%TZ ;;
   s) date -u +%s ;;
