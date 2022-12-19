ffc1bca4c574e0cc6ff386cdb44877b402c5872c
sharils <3063067+sharils@users.noreply.github.com>
2022-12-19 23:32:27 +0800

    Add x rm

--- shell_plugins/x.sh
+++ shell_plugins/x.sh
@@ -147,6 +147,23 @@ EOF
     killall Dock
     ;;
 
+  rm)
+    args=$(printf '%s\n' "$@")
+    printf '%s\n' "$args" | while read -r arg; do
+      osascript - "$(realpath "$arg")" <<APPLESCRIPT
+on run argv
+  set posix_files to {}
+
+  repeat with arg in argv
+    set end of posix_files to the POSIX file arg
+  end repeat
+
+  tell application "Finder" to move posix_files to trash
+end run
+APPLESCRIPT
+    done
+    ;;
+
   screen) screen -xRRe^Gg "$@" ;;
 
   sh)
