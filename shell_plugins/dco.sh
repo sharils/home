f1d06446d9887089bafec21a1ed2cd3863568749
sharils <3063067+sharils@users.noreply.github.com>
2022-12-20 12:51:18 +0800

    Add gco u b

--- shell_plugins/dco.sh
+++ shell_plugins/dco.sh
@@ -5,12 +5,20 @@ dco() {
   shift
 
   case "$cmd" in
-    b) dco build "$@" ;;
-    d) dco down "$@" ;;
-    e) dco exec "$@" ;;
-    l) dco logs "$@" ;;
-    r) dco run "$@" ;;
-    u) dco up "$@" ;;
-    *) docker compose "$cmd" "$@" ;;
+  b) dco build "$@" ;;
+  d) dco down "$@" ;;
+  e) dco exec "$@" ;;
+  l) dco logs "$@" ;;
+  r) dco run "$@" ;;
+  u)
+    cmd="$1"
+    shift
+    case "$cmd" in
+      b) set -- --build "$@" ;;
+      *) set -- "$cmd" "$@" ;;
+    esac
+    dco up "$@"
+    ;;
+  *) docker compose "$cmd" "$@" ;;
   esac
 }
