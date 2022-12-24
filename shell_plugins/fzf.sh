b6cba104e882ada1721e7ce84fc61adab1f18d19
sharils <3063067+sharils@users.noreply.github.com>
2022-12-24 09:19:29 +0800

    Change fzf ** to *

--- shell_plugins/fzf.sh
+++ shell_plugins/fzf.sh
@@ -3,6 +3,7 @@
 # shellcheck source=/dev/null
 [ -f ~/.fzf.bash ] && . ~/.fzf.bash
 
+export FZF_COMPLETION_TRIGGER='*'
 export FZF_DEFAULT_COMMAND="fd . --hidden --unrestricted --color always --ignore-file $HOME/.fdignore"
 export FZF_DEFAULT_OPTS='--ansi'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
