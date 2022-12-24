66b18d288b400fd9c3013b4a63b09700ace42a94
sharils <3063067+sharils@users.noreply.github.com>
2022-12-24 09:29:05 +0800

    Add fzf v **

--- shell_plugins/fzf.sh
+++ shell_plugins/fzf.sh
@@ -11,6 +11,9 @@ export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_OPTS='--preview "bat --color=always --line-range :500 {}"'
 export FZF_ALT_C_COMMAND="fd --hidden --unrestricted --color always --ignore-file $HOME/.fdignore --type d"
 
+# https://github.com/junegunn/fzf/blob/master/README.md#supported-commands
+_fzf_setup_completion path v
+
 _fzf_compgen_path() {
    $FZF_DEFAULT_COMMAND "$1"
 }
