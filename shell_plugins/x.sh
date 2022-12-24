5f2919fdc4da6a3f52e94d3c5bcff4c44d7be264
sharils <3063067+sharils@users.noreply.github.com>
2022-12-24 22:48:18 +0800

    Add x fx

--- shell_plugins/x.sh
+++ shell_plugins/x.sh
@@ -78,6 +78,16 @@ EOF
     [ "$now" -lt '1330' ] || [ '1930' -lt "$now" ]
     ;;
 
+  fx)
+    if [ $# -ne 0 ]; then
+      x fx | grep --ignore-case --color=never "$1"
+      return
+    fi
+    cmd="$HOME/tmp/sinopac/$(d F).json"
+    [ ! -f "$cmd" ] && curl 'https://mma.sinopac.com/ws/share/rate/ws_exchange.ashx' >"$cmd"
+    jq '([first.TitleInfo|splits("<.*?>")]|map(select(.!=""))[0][5:]|sub(" "; "T")) as $date|first.SubInfo|sort_by(.DataValue4)|map([$date,.DataValue4,.DataValue2,.DataValue3]|@tsv)[]' -r <"$cmd" | column -t
+    ;;
+
   gpg) echo | gpg --clearsign >/dev/null ;;
 
   gif)
