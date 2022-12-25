e509d5e64d42c661767995bb3ef2a08cb5b9c892
sharils <3063067+sharils@users.noreply.github.com>
2022-12-25 14:15:57 +0800

    Add x tz date

--- shell_plugins/x.sh
+++ shell_plugins/x.sh
@@ -231,9 +231,19 @@ EOF
   ) ;;
 
   tz)
-    tz="$(echo "$tz" | sort)"
-    echo "$tz" | grep /
-    echo "$tz" | grep -v /
+    cmd="$1"
+    shift
+    tz="$(cd /usr/share/zoneinfo && find ./* -type f -name '*[[:upper:]]*' ! -name +VERSION | sed 's#^./##')"
+    if [ "$cmd" = 'date' ]; then
+      # shellcheck disable=SC2016 # in sh -c
+      tz="$(echo "$tz" | xargs -I{} sh -c 'printf "%s {}\n" "$(TZ={} date +%z)"')"
+      echo "$tz" | grep '^-' | sort -r
+      echo "$tz" | grep '^+' | sort
+    else
+      tz="$(echo "$tz" | sort)"
+      echo "$tz" | grep /
+      echo "$tz" | grep -v /
+    fi
     ;;
 
   webp)
