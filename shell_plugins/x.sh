9b3cebc45ea3bd2f1dd67fc37c60d792632f3375
sharils <3063067+sharils@users.noreply.github.com>
2022-12-29 01:41:41 +0800

    Add x args o

--- shell_plugins/x.sh
+++ shell_plugins/x.sh
@@ -48,7 +48,15 @@ TEXT
 
   8601) open 'https://en.wikipedia.org/wiki/ISO_8601' ;;
 
-  args) xargs -o -I{} "$@" ;;
+  args)
+    cmd="$1"
+    shift
+    case "$cmd" in
+    o) set -- open "$@" ;;
+    *) set -- -I{} "$cmd" "$@" ;;
+    esac
+    xargs -o "$@"
+    ;;
 
   bc) bc --mathlib --expression="$*" ;;
 
