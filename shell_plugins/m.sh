9c130cebcb1c72cec65aeff93c100cc86a78347e
sharils <3063067+sharils@users.noreply.github.com>
2022-12-28 09:51:57 +0800

    Add m npm

--- shell_plugins/m.sh
+++ shell_plugins/m.sh
@@ -1,9 +1,13 @@
 m() {
   case "$1" in
-    erl) erl -man erl ;;
-    pj) m package-json ;;
-    re) m re_format ;;
-    sft) m strftime ;;
-    *) man "$(echo "$*" | tr ' ' '-')" ;;
+  erl) erl -man erl ;;
+  npm)
+    shift
+    npm help "$@"
+    ;;
+  pj) m package-json ;;
+  re) m re_format ;;
+  sft) m strftime ;;
+  *) man "$(echo "$*" | tr ' ' '-')" ;;
   esac
 }
