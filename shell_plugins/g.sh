ca71a3e4bd20388d8b1566bede64740fb0bd3f89
sharils <3063067+sharils@users.noreply.github.com>
2022-12-28 12:43:30 +0800

    Add g f for git flow

--- shell_plugins/g.sh
+++ shell_plugins/g.sh
@@ -47,6 +47,43 @@ g() {
 
   cs) git-crypt status "$@" ;;
 
+  f)
+    cmd="$1"
+    shift
+    case "$cmd" in
+    i) cmd='init' ;;
+    f) cmd='feature' ;;
+    b) cmd='bugfix' ;;
+    r) cmd='release' ;;
+    h) cmd='hotfix' ;;
+    s) cmd='support' ;;
+    l) cmd='log' ;;
+    *)
+      git f "$cmd" "$@"
+      return
+      ;;
+    esac
+
+    cmd1="$1"
+    shift
+    case "$cmd1" in
+    '') cmd1='' ;;
+    h) cmd1='help' ;;
+    l) cmd1='list' ;;
+    s) cmd1='start' ;;
+    f) cmd1='finish' ;;
+    p) cmd1='publish' ;;
+    t) cmd1='track' ;;
+    d) cmd1='diff' ;;
+    r) cmd1='rebase' ;;
+    co) cmd1='checkout' ;;
+    P) cmd1='pull' ;;
+    rm) cmd1='delete' ;;
+    esac
+
+    git flow "$cmd" "$cmd1" "$@"
+    ;;
+
   i)
     git init "$@"
     git commit --allow-empty --message "Initialize empty Git repository"
