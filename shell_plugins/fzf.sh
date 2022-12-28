e8424bca715da21c80ad5b9847d9add63fa42c57
sharils <3063067+sharils@users.noreply.github.com>
2022-12-29 01:48:43 +0800

    Change fzf FZF_DEFAULT_OPTS

--- shell_plugins/fzf.sh
+++ shell_plugins/fzf.sh
@@ -6,7 +6,7 @@
 
 export FZF_COMPLETION_TRIGGER='*'
 export FZF_DEFAULT_COMMAND="fd . --hidden --unrestricted --color always --ignore-file $HOME/.fdignore"
-export FZF_DEFAULT_OPTS='--ansi'
+export FZF_DEFAULT_OPTS='--ansi --cycle --multi'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_OPTS='--preview "bat --color=always --line-range :500 {}"'
 export FZF_ALT_C_COMMAND="fd --hidden --unrestricted --color always --ignore-file $HOME/.fdignore --type d"
