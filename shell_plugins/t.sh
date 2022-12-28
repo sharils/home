f7e93bc3b78779a125320894c91abc5405cd294d
sharils <3063067+sharils@users.noreply.github.com>
2022-12-28 23:16:10 +0800

    Add t * for tldr

--- shell_plugins/t.sh
+++ shell_plugins/t.sh
@@ -101,5 +101,7 @@ t() {
 
   add | a | addm | addto | append | app | archive | command | deduplicate | del | rm | depri | dp | done | do | help | list | ls | listall | lsa | listaddons | listcon | lsc | listfile | lf | listpri | lsp | listproj | lsprj | move | mv | prepend | prep | pri | replace | report | shorthelp) todo.sh "$cmd" "$@" ;;
 
+  *) tldr "$cmd" "$@" ;;
+
   esac
 }
