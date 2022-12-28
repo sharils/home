d76370fd3d1542e99ddaa9e19e7df925f48c0273
sharils <3063067+sharils@users.noreply.github.com>
2022-12-28 18:06:46 +0800

    Add export GIT_PAGER

--- shell_plugins/export.sh
+++ shell_plugins/export.sh
@@ -15,6 +15,8 @@ export ERL_AFLAGS="-kernel shell_history enabled"
 
 export GIT_PAGER="$PAGER --quit-if-one-screen"
 
+export GLAB_PAGER="$PAGER --quit-if-one-screen"
+
 export GREP_OPTIONS='--color=auto'
 
 export LANG='en_US.UTF-8'
