70da93fbf1deef72fdced846499d5e4d88f9acd8
sharils <3063067+sharils@users.noreply.github.com>
2022-12-29 23:05:15 +0800

    Change u h to h u

--- shell_plugins/u.sh
+++ shell_plugins/u.sh
@@ -8,8 +8,6 @@ u() {
 
   e) $EDITOR "$HOME/.qutebrowser/bookmarks/urls" "$HOME/.qutebrowser/quickmarks" "$HOME/Library/Preferences/qutebrowser/autoconfig.yml" "$HOME/.qutebrowser/greasemonkey" ;;
 
-  h) sed -n '3527,3842p' /Applications/qutebrowser.app/Contents/Resources/config/configdata.yml | grep "$*" ;;
-
   o) open -b org.qt-project.Qt.QtWebEngineCore ;;
 
   rm) rm -fr "$HOME/Library/Application Support/qutebrowser/webengine" ;;
