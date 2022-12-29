424e8408819317b6e38cd7bdcb3af0e2306456a9
sharils <3063067+sharils@users.noreply.github.com>
2022-12-29 14:40:35 +0800

    Add v nginx

--- shell_plugins/v.sh
+++ shell_plugins/v.sh
@@ -45,7 +45,7 @@ v() {
 
   d | p) vim -"$cmd" "$@" ;;
 
-  applescript | c | css | csv | eex | erl | ex | exs | gql | html | java | jq | js | json | jsx | lua | md | mdx | mw | pegjs | php | py | rs | scss | sh | sql | story | t | ts | tsx | txt | wat | xhtml | yaml | yml | zep)
+  applescript | c | css | csv | eex | erl | ex | exs | gql | html | java | jq | js | json | jsx | lua | md | mdx | mw | nginx | pegjs | php | py | rs | scss | sh | sql | story | t | ts | tsx | txt | wat | xhtml | yaml | yml | zep)
     if [ -f "$cmd" ]; then
       vim "$cmd"
       return
