5f0f7eb801eb7b18401a504c76b3f1d6ecfd75ce
sharils <3063067+sharils@users.noreply.github.com>
2022-12-31 18:13:29 +0800

    Remove w y

--- shell_plugins/w.sh
+++ shell_plugins/w.sh
@@ -20,7 +20,7 @@ w() {
 
   py | sh) w m --pattern "**/*.$cmd" --run "$*" ;;
 
-  b | l | o | y) "$@" | g url | x fzf | x a "$cmd" ;;
+  b | l | o) "$@" | g url | x fzf | x a "$cmd" ;;
 
   *) w o "$cmd" "$@" ;;
 
