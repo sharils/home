7a2ecca74c24cba833f01c39d5f9b2cdbd450c7a
sharils <3063067+sharils@users.noreply.github.com>
2022-12-31 20:10:42 +0800

    Change u rm to u clean

--- shell_plugins/u.sh
+++ shell_plugins/u.sh
@@ -6,11 +6,11 @@ u() {
 
   case "$cmd" in
 
+  clean) rm -fir "$HOME/Library/Application Support/qutebrowser/webengine" ;;
+
   e) $EDITOR "$HOME/.qutebrowser/bookmarks/urls" "$HOME/.qutebrowser/quickmarks" "$HOME/Library/Preferences/qutebrowser/autoconfig.yml" "$HOME/.qutebrowser/greasemonkey" ;;
 
   o) open -b org.qt-project.Qt.QtWebEngineCore ;;
-
-  rm) rm -fir "$HOME/Library/Application Support/qutebrowser/webengine" ;;
 
   *) echo 'not implemented yet' ;;
 
