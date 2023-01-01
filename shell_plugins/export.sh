a92c52d48d20f5d3e72e8c40a60bda0017ebd8b3
sharils <3063067+sharils@users.noreply.github.com>
2023-01-01 22:20:54 +0800

    Change export PAGER

--- shell_plugins/export.sh
+++ shell_plugins/export.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env sh
 
-# dependency stands out
-export PAGER='less'
-
 export ANDROID_SDK_ROOT="$HOME/Library/Android/sdk"
 export PATH="$PATH:$ANDROID_SDK_ROOT/emulator"
 export PATH="$PATH:$ANDROID_SDK_ROOT/platform-tools"
@@ -16,19 +13,15 @@ export EDITOR='vim'
 
 export ERL_AFLAGS="-kernel shell_history enabled"
 
-export GIT_PAGER="$PAGER --quit-if-one-screen"
-
-export GLAB_PAGER="$PAGER --quit-if-one-screen"
-
 export GREP_OPTIONS='--color=auto'
 
 export LANG='en_US.UTF-8'
 
 export LYNX_CFG="$SHARILS_HOME/.lynx/lynx.cfg"
 
-export MANPAGER="$PAGER --quit-if-one-screen"
-
 export LESS='--RAW-CONTROL-CHARS --ignore-case --no-init'
+
+export PAGER='less --quit-if-one-screen'
 
 export PATH="$HOME/.cargo/bin:$PATH"
 
