b610e208545222a4dd3e242b185af241b58f9aea
sharils <3063067+sharils@users.noreply.github.com>
2023-01-01 20:24:31 +0800

    Add t su TODO

--- shell_plugins/t.sh
+++ shell_plugins/t.sh
@@ -70,6 +70,10 @@ t() {
     cmd="$1"
     shift
     case "$cmd" in
+    add | a | addm | addto | append | app | archive | command | d | deduplicate | del | e | m | r | rm | depri | dp | done | do | help | list | ls | listall | lsa | listaddons | listcon | lsc | listfile | lf | listpri | lsp | listproj | lsprj | move | mv | prepend | prep | pri | replace | report | shorthelp)
+      TODO_FILE="$TODO_DIR/su.txt" t "$cmd" "$@"
+      return
+      ;;
     *[!0-9]*)
       if [ ${#cmd} -le 1 ]; then
         t su 1 "$cmd" "$@"
