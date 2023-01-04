3f7f2c6174dc085175b0d8e44b1ed3bc612285b6
sharils <3063067+sharils@users.noreply.github.com>
2023-01-04 21:47:21 +0800

    Change u clean

--- shell_plugins/u.sh
+++ shell_plugins/u.sh
@@ -6,7 +6,7 @@ u() {
 
   case "$cmd" in
 
-  clean) rm -fir "$HOME/Library/Application Support/qutebrowser/webengine" ;;
+  clean) rm "${@:--fir}" "$HOME/Library/Application Support/qutebrowser/webengine" ;;
 
   e) $EDITOR "$HOME/.qutebrowser/bookmarks/urls" "$HOME/.qutebrowser/quickmarks" "$HOME/Library/Preferences/qutebrowser/autoconfig.yml" "$HOME/.qutebrowser/greasemonkey" ;;
 
