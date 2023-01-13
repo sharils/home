7f8bea1178b1b1c9cb1e444cbb67dff1f62040a1
sharils <3063067+sharils@users.noreply.github.com>
2023-01-13 09:01:16 +0800

    Change qutebrowser g;

--- .qutebrowser/greasemonkey/google.js
+++ .qutebrowser/greasemonkey/google.js
@@ -1,5 +1,5 @@
 globalThis.google = () => {
   const q = new URLSearchParams(location.search).get("q"),
-    g = new URL("https://www.google.com.tw/search?q=%s&hl=en");
+    g = new URL("https://www.google.com.tw/search?client=firefox-b-d&gbv=1&q=%s&hl=en");
   g.searchParams.set("q", q), (globalThis.location = g);
 };
