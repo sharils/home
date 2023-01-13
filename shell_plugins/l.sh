55642910fdb76ffe086aa0f07107be72928306e3
sharils <3063067+sharils@users.noreply.github.com>
2023-01-13 08:33:03 +0800

    Add l x g

--- shell_plugins/l.sh
+++ shell_plugins/l.sh
@@ -32,6 +32,7 @@ l() {
       '') l x "$@" ;;
       b) l x "https://search.brave.com/search?q=$*" ;;
       dg) l x "https://ddg.gg/lite/?q=$*" ;;
+      g) l x "https://www.google.com/search?client=firefox-b-d&gbv=1&q=$*" ;;
       wp) l x "https://en.wikipedia.org/wiki/Special:Search?go=Go&ns0=1&search=$*" ;;
       *) lynx "$cmd" "$@" ;;
     esac
