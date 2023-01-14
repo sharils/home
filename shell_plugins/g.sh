57dd41eddad5c9fb818a44c309bd8119f3cef089
sharils <3063067+sharils@users.noreply.github.com>
2023-01-15 01:16:19 +0800

    Add g url .geek

--- shell_plugins/g.sh
+++ shell_plugins/g.sh
@@ -6,7 +6,7 @@ g() {
     cmd="$1"
     shift
     case "$cmd" in
-    url) grep --extended-regexp --only-matching "(https?://)?[[:alnum:]-]+(\.[[:alnum:]-]+)*\.(?:com|gg)/[^ '>]+" ;;
+    url) grep --extended-regexp --only-matching "(https?://)?[[:alnum:]-]+(\.[[:alnum:]-]+)*\.(?:com|geek|gg)/[^ '>]+" ;;
     # g -n "$@" # Segmentation fault: 11
     *) grep "$@" ;;
     esac
