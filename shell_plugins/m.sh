e91f44b1cc4a2a4ef4af4cfe182225cce777ced8
sharils <3063067+sharils@users.noreply.github.com>
2023-01-18 16:41:17 +0800

    Add m MAS_CMD

--- shell_plugins/m.sh
+++ shell_plugins/m.sh
@@ -9,6 +9,9 @@ m() {
   pj) m package-json ;;
   re) m re_format ;;
   sft) m strftime ;;
+
+  account | help | home | info | install | list | lucky | open | outdated | purchase | reset | search | signin | signout | uninstall | upgrade | vendor | version) mas "$@" ;;
+
   echo | false | kill | local | log | login | nice | nohup | printenv | printf | pwd | test | time | true | which | *)
     man "$(echo "$*" | sed 's# -.*##' | tr ' ' '-')" | l
     ;;
