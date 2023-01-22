cf977a43ebed2f8f978a4e5fda1b42bf948903eb
sharils <3063067+sharils@users.noreply.github.com>
2023-01-22 15:32:36 +0800

    Apply shfmt to x

--- shell_plugins/x.sh
+++ shell_plugins/x.sh
@@ -146,8 +146,8 @@ EOF
 
   rfc)
     case "$1" in
-      5424) x rfc "$*#page-11" ;;
-      *) l x "https://www.rfc-editor.org/rfc/rfc$*" ;;
+    5424) x rfc "$*#page-11" ;;
+    *) l x "https://www.rfc-editor.org/rfc/rfc$*" ;;
     esac
     ;;
 
