9d5a238ea474a924ce63bb551b2f9680aa08c2b9
sharils <3063067+sharils@users.noreply.github.com>
2023-01-23 16:21:24 +0800

    Change h help to pipe to l

--- shell_plugins/h.sh
+++ shell_plugins/h.sh
@@ -64,7 +64,7 @@ h() {
 
   t) todo.sh -vv help | l ;;
 
-  builtin | % | . | : | [ | { | alias | bg | bind | break | case | cd | command | complete | continue | dirs | echo | eval | exec | exit | export | false | fc | fg | for | getopts | hash | history | if | jobs | kill | local | log | logout | popd | printf | pushd | pwd | read | readonly | return | set | shift | source | suspend | test | time | times | trap | true | type | ulimit | umask | unalias | unset | until | wait | while) help "$cmd" "$@" ;;
+  builtin | % | . | : | [ | { | alias | bg | bind | break | case | cd | command | complete | continue | dirs | echo | eval | exec | exit | export | false | fc | fg | for | getopts | hash | history | if | jobs | kill | local | log | logout | popd | printf | pushd | pwd | read | readonly | return | set | shift | source | suspend | test | time | times | trap | true | type | ulimit | umask | unalias | unset | until | wait | while) help "$cmd" "$@" | l;;
 
   *) "$cmd" "$@" --help | l ;;
 
