4248d06c5cde1071defe05be73f23a0011173e1e
sharils <3063067+sharils@users.noreply.github.com>
2023-01-26 16:23:12 +0800

    Refactor x html t

--- shell_plugins/x.sh
+++ shell_plugins/x.sh
@@ -114,7 +114,7 @@ EOF
     shift
     case "$cmd" in
     t)
-      echo >&2 '==> index.html <=='
+      echo '==> index.html <==' >&2
       x html | tee index.html
       ;;
     *) echo '<!doctype html><html><head><meta charset="utf-8" /><meta http-equiv="Content-type" content="text/html; charset=utf-8" /><meta name="viewport" content="width=device-width, initial-scale=1" /><title>Hello HTML</title></head><body><h1>Hello HTML</h1></body></html>' ;;
