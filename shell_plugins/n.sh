d96414e0dd9d8e433679fa6abcab0e22958f38fa
sharils <3063067+sharils@users.noreply.github.com>
2023-01-26 18:40:12 +0800

    Add n mkcert

--- shell_plugins/n.sh
+++ shell_plugins/n.sh
@@ -65,6 +65,30 @@ n() {
 
   lv) NPM_CONFIG_LOGLEVEL='verbose' "$@" ;;
 
+  mkcert)
+    case "$1" in
+    ca)
+      [ ! -e ./ca.crt ] && [ ! -e ./ca.key ] &&
+        n y mkcert create-ca --organization '# Test CA' --validity 1
+      ;;
+    cert)
+      [ ! -e ./cert.crt ] && [ ! -e ./cert.key ] &&
+        n y mkcert create-cert --validity 1 --domains "${N_MKCERT_DOMAINS:-localhost,127.0.0.1}"
+      ;;
+    clean)
+      rm ca.crt ca.key cert.crt cert.key
+      ;;
+    refresh)
+      n mkcert clean
+      n mkcert
+      ;;
+    *)
+      n mkcert ca
+      n mkcert cert
+      ;;
+    esac
+    ;;
+
   na)
     cmd="$1"
     shift
