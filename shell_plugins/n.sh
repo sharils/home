7c34519b73ef93834a7e116518d26834936898fb
sharils <3063067+sharils@users.noreply.github.com>
2023-01-26 16:15:56 +0800

    Refactor n clear-npx-cache

--- shell_plugins/n.sh
+++ shell_plugins/n.sh
@@ -17,8 +17,7 @@ n() {
   clear-npx-cache)
     find "$(npm config get cache)/_npx" -depth 2 -name 'package.json' | while IFS= read -r package; do
       if [ "$(jq -r '.dependencies | keys | first' <"$package")" = "${1:?}" ]; then
-        echo >&2 + rm -fr "${package%/package.json}"
-        rm -fr "${package%/package.json}"
+        (set -x && rm -fr "${package%/package.json}")
         return
       fi
     done
