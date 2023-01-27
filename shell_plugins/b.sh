dca97323b3b2f1ec0260084bb0a4b37eaf66b2ac
sharils <3063067+sharils@users.noreply.github.com>
2023-01-27 19:19:32 +0800

    Refactor b shift

--- shell_plugins/b.sh
+++ shell_plugins/b.sh
@@ -18,28 +18,20 @@ b() {
     return
   fi
 
-  cmd="$1"
-  shift
-
-  case "$cmd" in
-  i) brew install "$@" ;;
-  ri) brew reinstall "$@" ;;
-  out) brew outdated "$@" ;;
-  v) brew abv "$@" ;;
-  f | l | n | o | t | u | x)
-    case "$cmd" in
-      c) cmd=/Applications/Chromium.app/Contents/MacOS/Chromium ;;
-      f) cmd=/Applications/Firefox.app/Contents/MacOS/firefox ;;
-      l) cmd=lynx ;;
-      n) cmd=none ;;
-      o) cmd=open ;;
-      u) cmd=qutebrowser ;;
-      t) cmd=/Applications/Tor\ Browser.app/Contents/MacOS/firefox ;;
-      x) cmd=/Applications/Firefox\ Developer\ Edition.app/Contents/MacOS/firefox ;;
-    esac
-    BROWSER="$cmd" "$@"
-    ;;
-  c | css | csv | eex | erl | ex | exs | gql | html | java | jq | js | json | jsx | lua | md | mdx | mw | pegjs | php | py | rs | scss | sh | sql | story | ts | tsx | txt | wat | xhtml | yaml | yml | zep) bat --language "$cmd" "$@" ;;
-  *) brew "$cmd" "$@" ;;
+  case "$1" in
+  cr) shift && BROWSER=/Applications/Chromium.app/Contents/MacOS/Chromium "$@" ;;
+  f) shift && BROWSER=/Applications/Firefox.app/Contents/MacOS/firefox "$@" ;;
+  i) shift && brew install "$@" ;;
+  l) shift && BROWSER=lynx "$@" ;;
+  n) shift && BROWSER=none "$@" ;;
+  o) shift && BROWSER=open "$@" ;;
+  out) shift && brew outdated "$@" ;;
+  ri) shift && brew reinstall "$@" ;;
+  t) shift && BROWSER=/Applications/Tor\ Browser.app/Contents/MacOS/firefox "$@" ;;
+  u) shift && BROWSER=qutebrowser "$@" ;;
+  v) shift && brew abv "$@" ;;
+  x) shift && BROWSER=/Applications/Firefox\ Developer\ Edition.app/Contents/MacOS/firefox "$@" ;;
+  c | css | csv | eex | erl | ex | exs | gql | html | java | jq | js | json | jsx | lua | md | mdx | mw | pegjs | php | py | rs | scss | sh | sql | story | ts | tsx | txt | wat | xhtml | yaml | yml | zep) bat --language "$@" ;;
+  *) shift && brew "$@" ;;
   esac
 }
