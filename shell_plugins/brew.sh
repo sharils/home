5a9211f8c3dd85b19147b12d9ef2e2090b0f6a52
sharils <3063067+sharils@users.noreply.github.com>
2023-01-30 19:55:37 +0800

    Fix brew shellenv

--- shell_plugins/brew.sh
+++ shell_plugins/brew.sh
@@ -2,3 +2,4 @@
 
 export HOMEBREW_NO_ANALYTICS=1
 [ -x "/opt/homebrew/bin/brew" ] && eval "$(/opt/homebrew/bin/brew shellenv)"
+[ -x "/usr/local/bin/brew" ] && eval "$(/usr/local/bin/brew shellenv)"
