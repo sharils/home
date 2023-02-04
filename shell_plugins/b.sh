0f40c5c216673b3194f105891c25ab566f17a2f4
sharils <3063067+sharils@users.noreply.github.com>
2023-02-05 00:05:28 +0800

    Fix b

--- shell_plugins/b.sh
+++ shell_plugins/b.sh
@@ -32,6 +32,6 @@ b() {
   v) shift && brew abv "$@" ;;
   x) shift && BROWSER=/Applications/Firefox\ Developer\ Edition.app/Contents/MacOS/firefox "$@" ;;
   c | css | csv | eex | erl | ex | exs | gql | html | java | jq | js | json | jsx | lua | md | mdx | mw | pegjs | php | py | rs | scss | sh | sql | story | ts | tsx | txt | wat | xhtml | yaml | yml | zep) bat --language "$@" ;;
-  *) shift && brew "$@" ;;
+  *) brew "$@" ;;
   esac
 }
