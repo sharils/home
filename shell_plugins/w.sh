6f45fbe7b32fbf2474be9ad5489b042dd32c252c
sharils <3063067+sharils@users.noreply.github.com>
2023-02-05 00:13:44 +0800

    Refactor w js

--- shell_plugins/w.sh
+++ shell_plugins/w.sh
@@ -9,8 +9,7 @@ w() {
   ex) w m --pattern '**/*.ex' '**/*.exs' \
     --run "${*:-mix test && mix format}" ;;
 
-  js) w m --pattern '**/*.js' '**/*.jsx' '**/*.ts' '**/*.tsx' \
-    --run "${*:-npm test}" ;;
+  js) w m --pattern '**/*.js' '**/*.jsx' '**/*.ts' '**/*.tsx' --run "${*:-npm test}" ;;
 
   m) watchman-make "$@" ;;
 
