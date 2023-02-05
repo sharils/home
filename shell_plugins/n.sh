8a284dc4d1566cade4c42814865f7b8dae495ea1
sharils <3063067+sharils@users.noreply.github.com>
2023-02-05 09:25:58 +0800

    Add n cnc

--- shell_plugins/n.sh
+++ shell_plugins/n.sh
@@ -14,7 +14,7 @@ n() {
 
   bs) n browser-sync "${@:-.}" --no-open ;;
 
-  clear-npx-cache)
+  cnc | clear-npx-cache)
     find "$(npm config get cache)/_npx" -depth 2 -name 'package.json' | while IFS= read -r package; do
       if [ "$(jq -r '.dependencies | keys | first' <"$package")" = "${1:?}" ]; then
         (set -x && rm -fr "${package%/package.json}")
