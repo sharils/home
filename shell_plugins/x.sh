eed442e8c017fe2f3c157ce9c3abf318d1856c29
sharils <3063067+sharils@users.noreply.github.com>
2023-02-22 09:55:12 +0800

    Add x ai fix-mypy-warnings-in-this-python-code

--- shell_plugins/x.sh
+++ shell_plugins/x.sh
@@ -27,6 +27,15 @@ x() {
       return
       ;;
 
+    fix-mypy-warnings-in-this-python-code)
+      shift && (
+        echo 'Fix mypy warnings in this Python code'
+        cat "$@"
+        mypy "$@"
+      ) | openai complete -
+      return
+      ;;
+
     # https://www.facebook.com/photo/?fbid=112335851759941&set=pcb.112341338426059
     crafting-business-canvas-models) shift && set -- "Generate a business canvas model for ${*:-an agriculture tech product that helps farmers increase their crop yield.}" ;;
 
