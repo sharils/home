91cd8ff3a9e158f9746a32508509f266fd0611ac
sharils <3063067+sharils@users.noreply.github.com>
2023-02-22 09:58:31 +0800

    Add x ai rewrite-these-tests-to-use-pytest-parametrized

--- shell_plugins/x.sh
+++ shell_plugins/x.sh
@@ -36,6 +36,14 @@ x() {
       return
       ;;
 
+    rewrite-these-tests-to-use-pytest-parametrized)
+      shift && (
+        echo 'Rewrite these tests to use pytest.parametrized'
+        cat "$@"
+      ) | openai complete -
+      return
+      ;;
+
     # https://www.facebook.com/photo/?fbid=112335851759941&set=pcb.112341338426059
     crafting-business-canvas-models) shift && set -- "Generate a business canvas model for ${*:-an agriculture tech product that helps farmers increase their crop yield.}" ;;
 
