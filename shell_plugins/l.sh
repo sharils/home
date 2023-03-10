82ef5fd7bf8e41dc94048c36c7f58e68a86ebc99
sharils <3063067+sharils@users.noreply.github.com>
2023-03-10 09:29:34 +0800

    Change l dg to l d

--- shell_plugins/l.sh
+++ shell_plugins/l.sh
@@ -23,7 +23,7 @@ l() {
   -*) lsd "$cmd" "$@" ;;
   F) less +F "$@" ;;
   b) l x "https://search.brave.com/search?q=$*" ;;
-  dg) l x "https://lite.duckduckgo.com/lite/?q=$*" ;;
+  d) l x "https://lite.duckduckgo.com/lite/?q=$*" ;;
   g) l x "https://www.google.com/search?client=firefox-b-d&gbv=1&q=$*" ;;
   pl) l x "https://lite.duckduckgo.com/lite/?q=site%3Avald-phoenix.github.io%20$*" ;;
   od) l x "https://www.oxfordlearnersdictionaries.com/definition/english/$*_1" ;;
