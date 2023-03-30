67379d40fa07a560bc16f51e1f58252540da9080
sharils <3063067+sharils@users.noreply.github.com>
2023-03-30 19:02:56 +0800

    Change g cbr

--- shell_plugins/g.sh
+++ shell_plugins/g.sh
@@ -46,6 +46,9 @@ g() {
     g cb |
       sed 's/^.* //' | grep -Ev '^(?:main|master)$' |
       xargs -I{} -n1 sh -c "git -c advice.skippedCherryPicks=false r $upstream {} || exit 255"
+    g cb |
+      sed 's/^.* //' | grep -E '^(?:main|master)$' |
+      xargs -I{} -n1 sh -c "git -c advice.skippedCherryPicks=false r origin/{} {} || exit 255"
     exit_code=$?
     g co "$current_branch"
     return $exit_code
