471c2d18df3f853a1609634546107a8cf31b6783
sharils <3063067+sharils@users.noreply.github.com>
2023-04-02 19:04:30 +0800

    Fix k g

--- shell_plugins/k.sh
+++ shell_plugins/k.sh
@@ -82,6 +82,7 @@ TEXT
     ;;
 
   g)
+    shift
     no_url_encode="!'()*-.0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz~"
     keepassxc-cli generate --custom "$no_url_encode" --length "${@:-40}" | tr -d \\n | pbcopy
     pbpaste | wc
