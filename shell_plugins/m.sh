298d290755407631e3089679a609ccb9e08a3fb7
sharils <3063067+sharils@users.noreply.github.com>
2023-04-03 19:32:42 +0800

    Refactor m

--- shell_plugins/m.sh
+++ shell_plugins/m.sh
@@ -1,21 +1,20 @@
 m() {
   case "$1" in
+
   aws) "$@" help | l ;;
+
   erl) erl -man erl | l ;;
-  i)
-    shift
-    m install "$@"
-    ;;
-  n | npm)
-    shift
-    npm help "$@" | l
-    ;;
+
+  i) shift && m install "$@" ;;
+
+  n | npm) shift && npm help "$@" | l ;;
+
   pj) m package-json ;;
+
   re) m re_format ;;
-  s)
-    shift
-    m search "$@"
-    ;;
+
+  s) shift && m search "$@" ;;
+
   sft) m strftime ;;
 
   account | help | home | info | install | list | lucky | open | outdated | purchase | reset | search | signin | signout | uninstall | upgrade | vendor | version) mas "$@" ;;
