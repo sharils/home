ba02bf797f65497abfc86018d67a991a322207f7
sharils <3063067+sharils@users.noreply.github.com>
2023-04-03 19:37:52 +0800

    Remove m help

--- shell_plugins/m.sh
+++ shell_plugins/m.sh
@@ -17,7 +17,7 @@ m() {
 
   sft) m strftime ;;
 
-  account | help | home | info | install | list | lucky | open | outdated | purchase | reset | search | signin | signout | uninstall | upgrade | vendor | version) mas "$@" ;;
+  account | home | info | install | list | lucky | open | outdated | purchase | reset | search | signin | signout | uninstall | upgrade | vendor | version) mas "$@" ;;
 
   echo | false | kill | local | log | login | nice | nohup | printenv | printf | pwd | test | time | true | which | *)
     man "$(echo "$*" | sed 's# -.*##' | tr ' ' '-')" | l
