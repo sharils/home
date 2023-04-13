ab781ece00dd614c4533f3877c247f8be9eb2051
sharils <3063067+sharils@users.noreply.github.com>
2023-04-13 19:57:26 +0800

    Fix t e

--- shell_plugins/t.sh
+++ shell_plugins/t.sh
@@ -40,14 +40,13 @@ t() {
     ;;
 
   e)
-    case "${1:-t}" in
+    case "$1" in
     q) shift && $EDITOR "$@" "$TODO_DIR/QuickNote.md" ;;
     t) shift && $EDITOR "$@" "$TODO_FILE" ;;
     *[!0-9]*) $EDITOR "$@" "$TODO_FILE" ;;
     *)
       cmd="$1" && shift
-      $EDITOR "+$cmd" "$@" "$TODO_FILE"
-      ;;
+      $EDITOR "+$cmd" "$@" "$TODO_FILE" ;;
     esac
     ;;
 
