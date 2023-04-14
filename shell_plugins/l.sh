a46940fbec61d8686ac6e912f82b0e32352bd125
sharils <3063067+sharils@users.noreply.github.com>
2023-04-14 22:44:32 +0800

    Refactor l cmd out

--- shell_plugins/l.sh
+++ shell_plugins/l.sh
@@ -16,22 +16,21 @@ l() {
     return
   fi
 
-  cmd="$1"
-  shift
-  case "$cmd" in
+  case "$1" in
   '') lsd ;;
-  -*) lsd "$cmd" "$@" ;;
-  F) less +F "$@" ;;
-  b) l x "https://search.brave.com/search?q=$*" ;;
-  cctld) l x 'https://en.wikipedia.org/wiki/Template:CcTLD#bodyContent' ;;
-  d) l x "https://lite.duckduckgo.com/lite/?q=$*" ;;
-  g) l x "https://www.google.com/search?client=firefox-b-d&gbv=1&q=$*" ;;
-  node) l x "https://nodejs.org/dist/latest-v18.x/docs/api/$*" ;;
-  od) l x "https://www.oxfordlearnersdictionaries.com/definition/english/$*_1" ;;
-  pep) l x "https://peps.python.org/pep-$(printf "%04d" "$*")/" ;;
-  pl) l x "https://lite.duckduckgo.com/lite/?q=site%3Avald-phoenix.github.io%20$*" ;;
-  port) l x 'https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers#Registered_ports' ;;
+  -*) shift && lsd "$1" "$@" ;;
+  F) shift && less +F "$@" ;;
+  b) shift && l x "https://search.brave.com/search?q=$*" ;;
+  cctld) shift && l x 'https://en.wikipedia.org/wiki/Template:CcTLD#bodyContent' ;;
+  d) shift && l x "https://lite.duckduckgo.com/lite/?q=$*" ;;
+  g) shift && l x "https://www.google.com/search?client=firefox-b-d&gbv=1&q=$*" ;;
+  node) shift && l x "https://nodejs.org/dist/latest-v18.x/docs/api/$*" ;;
+  od) shift && l x "https://www.oxfordlearnersdictionaries.com/definition/english/$*_1" ;;
+  pep) shift && l x "https://peps.python.org/pep-$(printf "%04d" "$*")/" ;;
+  pl) shift && l x "https://lite.duckduckgo.com/lite/?q=site%3Avald-phoenix.github.io%20$*" ;;
+  port) shift && l x 'https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers#Registered_ports' ;;
   rfc)
+    shift
     if [ $# -eq 0 ]; then
       cat <<'EOF'
 2119 RFC Key Words
@@ -51,17 +50,18 @@ EOF
     esac
     l x "https://www.rfc-editor.org/rfc/rfc$*"
     ;;
-  s) less "$@" ;;
-  t) l --tree "$@" ;;
-  wk) l x "https://en.wiktionary.org/wiki/$*#Esperanto" ;;
-  wp) l x "https://en.wikipedia.org/wiki/Special:Search?go=Go&ns0=1&search=$*" ;;
-  wz) l x "https://zh.wikipedia.org/wiki/$*" ;;
-  x) lynx "$@" ;;
+  s) shift && less "$@" ;;
+  t) shift && l --tree "$@" ;;
+  wk) shift && l x "https://en.wiktionary.org/wiki/$*#Esperanto" ;;
+  wp) shift && l x "https://en.wikipedia.org/wiki/Special:Search?go=Go&ns0=1&search=$*" ;;
+  wz) shift && l x "https://zh.wikipedia.org/wiki/$*" ;;
+  x) shift && lynx "$@" ;;
   *)
-    if [ -e "$cmd" ]; then
-      lsd "$cmd" "$@"
+    shift
+    if [ -e "$1" ]; then
+      lsd "$1" "$@"
     else
-      l -"$cmd" "$@"
+      l -"$1" "$@"
     fi
     ;;
   esac
