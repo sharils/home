443781d0fed3ecc3de26dc5d8f37ccb48a56fd26
sharils <3063067+sharils@users.noreply.github.com>
2023-04-15 09:17:07 +0800

    Refactor x rm

---
+++ shell_plugins/x/rm.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+rm() {
+  args=$(printf '%s\n' "$@")
+  printf '%s\n' "$args" | while read -r arg; do
+    osascript - "$(realpath "$arg")" <<APPLESCRIPT
+on run argv
+  set posix_files to {}
+
+  repeat with arg in argv
+    set end of posix_files to the POSIX file arg
+  end repeat
+
+  tell application "Finder" to move posix_files to trash
+end run
+APPLESCRIPT
+  done
+}
+
+rm "$@"
