8fa540fd538c97b37d0e3f4a27527e253ed49c23
sharils <3063067+sharils@users.noreply.github.com>
2023-04-15 09:06:43 +0800

    Refactor x tz

--- shell_plugins/x.sh
+++ shell_plugins/x.sh
@@ -251,34 +251,7 @@ EOF
 
   ts) n y ts-node "$@" ;;
 
-  tz)
-    cmd="$1"
-    shift
-    if [ "$cmd" = pdt ]; then
-      cmd=PST8PDT
-    elif [ "$cmd" = mdt ]; then
-      cmd=MST7MDT
-    elif [ "$cmd" = cdt ]; then
-      cmd=CST6CDT
-    elif [ "$cmd" = edt ]; then
-      cmd=EST5EDT
-    fi
-    if [ -n "$cmd" ] && realpath -q "/usr/share/zoneinfo/$cmd" >/dev/null; then
-      TZ="$cmd" "$@"
-      return
-    fi
-    tz="$(cd /usr/share/zoneinfo && find ./* -type f -name '*[[:upper:]]*' ! -name +VERSION | sed 's#^./##')"
-    if [ "$cmd" = 'date' ]; then
-      # shellcheck disable=SC2016 # in sh -c
-      tz="$(echo "$tz" | xargs -I{} sh -c 'printf "%s {}\n" "$(TZ={} date +%z)"')"
-      echo "$tz" | grep '^-' | sort -r
-      echo "$tz" | grep '^+' | sort
-    else
-      tz="$(echo "$tz" | sort)"
-      echo "$tz" | grep /
-      echo "$tz" | grep -v /
-    fi
-    ;;
+  tz) "$SHARILS_HOME/shell_plugins/x/tz.sh" "$@" ;;
 
   venv)
     if type deactivate >/dev/null 2>&1; then
