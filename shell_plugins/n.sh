e0e3dee4c2488a713e71ee06c96ef2d85b2256f6
sharils <3063067+sharils@users.noreply.github.com>
2023-04-15 15:04:11 +0800

    Refactor n p

--- shell_plugins/n.sh
+++ shell_plugins/n.sh
@@ -78,28 +78,7 @@ n() {
     n nx-workspace --name "$(basename "$PWD")" --preset next --appName "${cmd:-appName}" --style scss --nxCloud "$@"
     ;;
 
-  p)
-    case "$1" in
-    d) shift && npm pkg delete "$@" ;;
-    g) shift && npm pkg get "$@" ;;
-    s)
-      shift
-      case "$1" in
-      s)
-        shift
-        cmd="$1" && shift
-        p s scripts."$cmd"="$*"
-        ;;
-      *) npm pkg set "$@" ;;
-      esac
-      ;;
-    init) echo {} >.prettierrc.json ;;
-
-    awk | eex | elm | groovy | nginx | packagejson | pegjs | pgsql | sh | sql | ssh-config | tsconfig) n y --package prettier --package "prettier-plugin-$1" -- prettier --parser "$@" ;;
-
-    *) n y prettier --write "**/*.js" "**/*.jsx" "**/*.ts" "**/*.tsx" "$@" ;;
-    esac
-    ;;
+  p) "$SHARILS_HOME/shell_plugins/n/p.sh" "$@" ;;
 
   pkill)
     if [ $# -eq 0 ]; then
---
+++ shell_plugins/n/p.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+p() {
+  case "$1" in
+  d) shift && npm pkg delete "$@" ;;
+  g) shift && npm pkg get "$@" ;;
+  s)
+    shift
+    case "$1" in
+    s)
+      shift
+      cmd="$1" && shift
+      p s scripts."$cmd"="$*"
+      ;;
+    *) npm pkg set "$@" ;;
+    esac
+    ;;
+  init) echo {} >.prettierrc.json ;;
+
+  awk | eex | elm | groovy | nginx | packagejson | pegjs | pgsql | sh | sql | ssh-config | tsconfig) n y --package prettier --package "prettier-plugin-$1" -- prettier --parser "$@" ;;
+
+  *) n y prettier --write "**/*.js" "**/*.jsx" "**/*.ts" "**/*.tsx" "$@" ;;
+  esac
+}
+
+p "$@"
