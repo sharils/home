f5f575a17db7a00b8489f6bc29888ce4ece80f92
sharils <3063067+sharils@users.noreply.github.com>
2023-04-15 11:10:59 +0800

    Refactor x format

--- shell_plugins/x.sh
+++ shell_plugins/x.sh
@@ -1,128 +1,50 @@
 #!/usr/bin/env sh
 
 x() {
-  cmd="$1"
-  shift
-
-  case "$cmd" in
-
-  +) chmod -vv +x "$@" ;;
-
-  -) chmod -vv -x "$@" ;;
-
+  case "$1" in
+  +) shift && chmod -vv +x "$@" ;;
+  -) shift && chmod -vv -x "$@" ;;
   8601) x b 'https://en.wikipedia.org/wiki/ISO_8601' ;;
-
-  ai) "$SHARILS_HOME/shell_plugins/x/ai.sh" "$@" ;;
-
-  ap) "$SHARILS_HOME/shell_plugins/x/ap.sh" "$@" ;;
-
-  b) "$BROWSER" "$@" ;;
-
-  bc) "$SHARILS_HOME/shell_plugins/x/bc.sh" "$@" ;;
-
+  b) shift && "$BROWSER" "$@" ;;
   cal) cal -A4 -B4 ;;
-
-  chunk) fold -w "${@:-3}" | tr '\n' ' ' ;;
-
-  cr) /Applications/Chromium.app/Contents/MacOS/Chromium "$@" ;;
-
-  data) "$SHARILS_HOME/shell_plugins/x/data.sh" "$@" ;;
-
-  diff) diff --color --unified "$@" ;;
-
-  editorconfig) "$SHARILS_HOME/shell_plugins/x/editorconfig.sh" "$@" ;;
-
-  erl) "$SHARILS_HOME/shell_plugins/x/erl.sh" "$@" ;;
-
-  ex) "$SHARILS_HOME/shell_plugins/x/ex.sh" "$@" ;;
-
-  f) /Applications/Firefox.app/Contents/MacOS/firefox "$@" ;;
-
-  fde) /Applications/Firefox\ Developer\ Edition.app/Contents/MacOS/firefox "$@" ;;
-
-  focus) "$SHARILS_HOME/shell_plugins/x/focus.sh" "$@" ;;
-
-  fx) "$SHARILS_HOME/shell_plugins/x/fx.sh" "$@" ;;
-
-  fzf) fzf --select-1 --height=~14 --layout=reverse "$@" ;;
-
+  chunk) shift && fold -w "${@:-3}" | tr '\n' ' ' ;;
+  cr) shift && /Applications/Chromium.app/Contents/MacOS/Chromium "$@" ;;
+  diff) shift && diff --color --unified "$@" ;;
+  f) shift && /Applications/Firefox.app/Contents/MacOS/firefox "$@" ;;
+  fde) shift && /Applications/Firefox\ Developer\ Edition.app/Contents/MacOS/firefox "$@" ;;
+  fzf) shift && fzf --select-1 --height=~14 --layout=reverse "$@" ;;
   gpg) echo | gpg --clearsign >/dev/null ;;
-
-  gif) "$SHARILS_HOME/shell_plugins/x/gif.sh" "$@" ;;
-
   hostnameise) tr '[:upper:]' '[:lower:]' | sed -E 's/[^[:alnum:]]+/-/g' ;;
-
-  html) "$SHARILS_HOME/shell_plugins/x/html.sh" "$@" ;;
-
-  ico) "$SHARILS_HOME/shell_plugins/x/ico.sh" "$@" ;;
-
-  ip) ipconfig getifaddr "${@:-en0}" ;;
-
+  ip) shift && ipconfig getifaddr "${@:-en0}" ;;
   js) node --print "$*" ;;
-
-  log) log show --predicate 'eventMessage CONTAINS "unlockUIBecomesActive"' --last "${@:-1h}" ;;
-
-  mh) "$SHARILS_HOME/shell_plugins/x/mh.sh" "$@" ;;
-
-  mp) mkdir -pv "$@" && cd "$@" || return ;;
-
-  o) open "${@:-.}" ;;
-
-  pdf) "$SHARILS_HOME/shell_plugins/x/pdf.sh" "$@" ;;
-
-  php) php -r "$*" ;;
-
-  png) "$SHARILS_HOME/shell_plugins/x/png.sh" "$@" ;;
-
-  py) "$SHARILS_HOME/shell_plugins/x/py.sh" "$@" ;;
-
-  rm) "$SHARILS_HOME/shell_plugins/x/rm.sh" "$@" ;;
-
-  screen) screen -xRRe^Gg "$@" ;;
-
-  sh) "$SHARILS_HOME/shell_plugins/x/sh.sh" "$@" ;;
-
+  log) shift && log show --predicate 'eventMessage CONTAINS "unlockUIBecomesActive"' --last "${@:-1h}" ;;
+  mp) shift && mkdir -pv "$@" && cd "$@" || return ;;
+  o) shift && open "${@:-.}" ;;
+  php) shift && php -r "$*" ;;
+  screen) shift && screen -xRRe^Gg "$@" ;;
   shuf) tr ' ' '\n' | sort --random-sort | tr '\n' ' ' ;;
-
-  sn) "$SHARILS_HOME/shell_plugins/x/sn.sh" "$@" ;;
-
-  ssh) ssh -t "$@" screen -xRRe^Gg ;;
-
-  ssh-copy-id) ssh-copy-id -o PreferredAuthentications=password -i "$HOME/.ssh/id_ed25519.pub" "$@" ;;
-
-  ssh-keygen) "$SHARILS_HOME/shell_plugins/x/ssh-keygen.sh" "$@" ;;
-
-  tb) /Applications/Tor\ Browser.app/Contents/MacOS/firefox "$@" ;;
-
-  tel) open "tel:$*" ;;
-
-  touch) "$SHARILS_HOME/shell_plugins/x/touch.sh" "$@" ;;
-
-  tree) "$SHARILS_HOME/shell_plugins/x/tree.sh" "$@" ;;
-
-  ts) n y ts-node "$@" ;;
-
-  tz) "$SHARILS_HOME/shell_plugins/x/tz.sh" "$@" ;;
-
-  venv) "$SHARILS_HOME/shell_plugins/x/venv.sh" "$@" ;;
-
-  w8) "$SHARILS_HOME/shell_plugins/x/w8.sh" "$@" ;;
-
-  webp) "$SHARILS_HOME/shell_plugins/x/webp.sh" "$@" ;;
-
-  whois) "$SHARILS_HOME/shell_plugins/x/whois.sh" "$@" ;;
-
-  x) (set -x && "$@") ;;
-
+  ssh) shift && ssh -t "$@" screen -xRRe^Gg ;;
+  ssh-copy-id) shift && ssh-copy-id -o PreferredAuthentications=password -i "$HOME/.ssh/id_ed25519.pub" "$@" ;;
+  tb) shift && /Applications/Tor\ Browser.app/Contents/MacOS/firefox "$@" ;;
+  tel) shift && open "tel:$*" ;;
+  ts) shift && n y ts-node "$@" ;;
+  x) shift && (set -x && "$@") ;;
   zws) printf "\xe2\x80\x8b" | c ;;
 
   resetLaunchPad | showHidden) "$SHARILS_HOME/shell_plugins/x/resetLaunchPad.sh" "$@" ;;
 
-  aud | cad | chf | cnh | cny | eur | gbp | hkd | jpy | mop | nzd | sek | sgd | thb | usd | zar) x fx "$cmd" | x bc "$(awk '{ print $3 }')" "$@" ;;
+  aud | cad | chf | cnh | cny | eur | gbp | hkd | jpy | mop | nzd | sek | sgd | thb | usd | zar)
+    cmd="$1" && shift
+    x fx "$cmd" | x bc "$(awk '{ print $3 }')" "$@"
+    ;;
+
+  ai | ap | bc | data | editorconfig | erl | ex | focus | fx | gif | html | ico | mh | pdf | png | py | rm | sh | sn | ssh-keygen | touch | tree | tz | venv | w8 | webp | whois)
+    cmd="$1" && shift
+    "$SHARILS_HOME/shell_plugins/x/$cmd.sh" "$@"
+    ;;
 
   am | at | au | bg | br | by | cn | cz | de | dk | ee | et | fi | fr | gb | gr | hk | hr | hu | ie | il | is | it | jp | kr | kz | lt | nl | no | nz | pl | pt | ro | ru | se | si | sk | tr | tw | ua | us | yu | za | dech | frbe | frca | frch | itch | nlbe | ca | es | eu | en) "$SHARILS_HOME/shell_plugins/x/currency.sh" "$@" ;;
 
   *) echo not implemented >&2 ;;
-
   esac
 }
