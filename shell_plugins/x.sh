e7491de19f40dcb70622e7ccc592c8efa54a79f8
sharils <3063067+sharils@users.noreply.github.com>
2023-04-15 09:19:08 +0800

    Refactor x sn

--- shell_plugins/x.sh
+++ shell_plugins/x.sh
@@ -118,15 +118,7 @@ x() {
 
   shuf) tr ' ' '\n' | sort --random-sort | tr '\n' ' ' ;;
 
-  sn)
-    # shellcheck disable=2087
-    ssh no sh <<EOF
-set -o errexit -o nounset -o xtrace
-for arg in $@; do
-  curl --location --continue-at - --output sn/sn-\$arg.mp3 https://media.grc.com/sn/sn-\$arg.mp3
-done
-EOF
-    ;;
+  sn) "$SHARILS_HOME/shell_plugins/x/sn.sh" "$@" ;;
 
   ssh) ssh -t "$@" screen -xRRe^Gg ;;
 
---
+++ shell_plugins/x/sn.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+sn() {
+  # shellcheck disable=2087
+  ssh no sh <<EOF
+set -o errexit -o nounset -o xtrace
+for arg in $@; do
+  curl --location --continue-at - --output sn/sn-\$arg.mp3 https://media.grc.com/sn/sn-\$arg.mp3
+done
+EOF
+}
+
+sh "$@"
