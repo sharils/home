a72e41a617dd1d969d53320c0be22521f130a285
sharils <3063067+sharils@users.noreply.github.com>
2023-04-15 09:11:58 +0800

    Refactor x gif

--- shell_plugins/x.sh
+++ shell_plugins/x.sh
@@ -67,17 +67,7 @@ x() {
 
   gpg) echo | gpg --clearsign >/dev/null ;;
 
-  gif)
-    cmd="$1"
-    shift
-    case "$cmd" in
-    t)
-      echo '==> 43B.gif <==' >&2
-      x gif | tee 43B.gif
-      ;;
-    *) printf "\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x80\x00\x00\xff\xff\xff\x00\x00\x00\x21\xf9\x04\x01\x00\x00\x00\x00\x2c\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02\x44\x01\x00\x3b" ;;
-    esac
-    ;;
+  gif) "$SHARILS_HOME/shell_plugins/x/gif.sh" "$@" ;;
 
   hostnameise) tr '[:upper:]' '[:lower:]' | sed -E 's/[^[:alnum:]]+/-/g' ;;
 
