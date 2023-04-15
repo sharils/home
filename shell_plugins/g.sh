caf50b6365886d91e313d78c5e3ef25ef8ab4815
sharils <3063067+sharils@users.noreply.github.com>
2023-04-15 14:36:24 +0800

    Refactor g f

--- shell_plugins/g.sh
+++ shell_plugins/g.sh
@@ -74,43 +74,7 @@ g() {
 
   cs) git-crypt status "$@" ;;
 
-  f)
-    cmd="$1"
-    case "$cmd" in
-    i) cmd='init' ;;
-    f) cmd='feature' ;;
-    b) cmd='bugfix' ;;
-    r) cmd='release' ;;
-    h) cmd='hotfix' ;;
-    s) cmd='support' ;;
-    l) cmd='log' ;;
-    *)
-      git f "$@"
-      return
-      ;;
-    esac
-    # g f '' complains so shift after g f *
-    shift
-
-    cmd1="$1"
-    shift
-    case "$cmd1" in
-    '') cmd1='' ;;
-    h) cmd1='help' ;;
-    l) cmd1='list' ;;
-    s) cmd1='start' ;;
-    f) cmd1='finish' ;;
-    p) cmd1='publish' ;;
-    t) cmd1='track' ;;
-    d) cmd1='diff' ;;
-    r) cmd1='rebase' ;;
-    co) cmd1='checkout' ;;
-    P) cmd1='pull' ;;
-    rm) cmd1='delete' ;;
-    esac
-
-    git flow "$cmd" "$cmd1" "$@"
-    ;;
+  f) "$SHARILS_HOME/shell_plugins/g/f.sh" "$@" ;;
 
   i)
     git init "$@"
