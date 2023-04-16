6d2daee5e415bc01ecb1b3912d93518566be10f8
sharils <3063067+sharils@users.noreply.github.com>
2023-04-17 07:31:32 +0800

    Add c e

--- shell_plugins/c.sh
+++ shell_plugins/c.sh
@@ -8,6 +8,7 @@ c() {
 
   case "$1" in
   a) shift && set -- add "$@" ;;
+  e) [ -f Cargo.toml ] && $EDITOR Cargo.toml || return $? ;;
   n) shift && set -- new "${@:-rust-app}" ;;
   esac
   cargo "$@"
