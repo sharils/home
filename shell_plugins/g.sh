076edf276345316456be5e2a0ecc7e658c3f6ee0
sharils <3063067+sharils@users.noreply.github.com>
2023-04-16 12:16:37 +0800

    Refactor g cl

--- shell_plugins/g.sh
+++ shell_plugins/g.sh
@@ -30,7 +30,22 @@ g() {
 
   cbr) shift && "$SHARILS_HOME/shell_plugins/g/cbr.sh" "$@" ;;
 
-  cl) shift && "$SHARILS_HOME/shell_plugins/g/cl.sh" "$@" ;;
+  cl)
+    shift
+    case "$1" in
+    -e) git config --local "$@" ;;
+    t | *)
+      case "$1" in
+      ~) shift && x mp "$HOME$(dirname "$(echo "$*" | sed 's#https:/#git#;s#[@:]#/#g;s#^#/#')")" ;;
+      t) shift && z t ;;
+      esac
+      git clone "$@"
+      for last in "$@"; do :; done
+      last="$(basename "$last")"
+      cd "${last%.*}" || return
+      ;;
+    esac
+    ;;
 
   cmx) shift && g cm "Apply $*" ;;
 
