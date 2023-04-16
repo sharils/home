b1e6bb3d39f70857f1f04c9b5b8aee12b76a3f5f
sharils <3063067+sharils@users.noreply.github.com>
2023-04-16 15:57:04 +0800

    Refactor h e

---
+++ shell_plugins/h/e.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+e() {
+  case "${1:-install}" in
+  bp) vim "$SHARILS_HOME/bash_profile" ;;
+  install) vim "$SHARILS_HOME/install" ;;
+  karabiner) vim "$SHARILS_HOME/config/karabiner/karabiner.json" ;;
+  todo) vim "$SHARILS_HOME/todo.cfg" ;;
+  tig) vim "$SHARILS_HOME/tigrc" ;;
+  vim) vim "$SHARILS_HOME/vimrc" ;;
+  *)
+    if [ $# -le 1 ]; then
+      vim "$SHARILS_HOME/shell_plugins/$*.sh"
+    else
+      cmd="$1" && shift
+      grep --extended-regexp --line-number --with-filename "^\s+\b$*\)" "$SHARILS_HOME/shell_plugins/$cmd.sh" | v
+    fi
+    ;;
+  esac
+}
+
+e "$@"
