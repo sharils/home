a43fe3302e42ea52a4fbdbc90f11c239d70f7b84
sharils <3063067+sharils@users.noreply.github.com>
2023-04-17 21:10:06 +0800

    Add r d b

--- shell_plugins/r.sh
+++ shell_plugins/r.sh
@@ -2,7 +2,13 @@
 
 r() {
   case "$1" in
-  d) shift && rustup docs "$@" ;;
+  d)
+    shift
+    case "$1" in
+    b) shift && set -- --book "$@";;
+    esac
+    rustup docs "$@"
+    ;;
   e) shift && rustc --explain "$@" | l md ;;
   *) reset ;;
   esac
