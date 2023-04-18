b872c973dbdacc54ac444064022e66ca01d14a56
sharils <3063067+sharils@users.noreply.github.com>
2023-04-19 06:02:44 +0800

    Add h e $* for sub command

--- shell_plugins/h/e.sh
+++ shell_plugins/h/e.sh
@@ -11,14 +11,21 @@ e() {
   *)
     if [ $# -le 1 ]; then
       vim "$SHARILS_HOME/shell_plugins/$*.sh"
-    else
-      cmd="$1" && shift
-      tmp="$(mktemp)"
-      grep --extended-regexp --line-number --with-filename "(?:^|\|)\s+\b$*\)" "$SHARILS_HOME/shell_plugins/$cmd.sh" >"$tmp"
-      set -- -q "$tmp"
-      [ "$(wc -l < "$tmp" | tr -d '[:space:]')" -ge 2 ] && set -- +copen "$@"
-      vim "$@"
+      return $?
     fi
+
+    tmp="$SHARILS_HOME/shell_plugins/$(echo "$*" | tr ' ' '/').sh"
+    if [ -f "$tmp" ]; then
+      vim "$tmp"
+      return $?
+    fi
+
+    cmd="$1" && shift
+    tmp="$(mktemp)"
+    grep --extended-regexp --line-number --with-filename "(?:^|\|)\s+\b$*\)" "$SHARILS_HOME/shell_plugins/$cmd.sh" >"$tmp"
+    set -- -q "$tmp"
+    [ "$(wc -l <"$tmp" | tr -d '[:space:]')" -ge 2 ] && set -- +copen "$@"
+    vim "$@"
     ;;
   esac
 }
