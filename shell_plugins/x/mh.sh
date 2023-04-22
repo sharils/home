83febe230bfd1074d6405c61c3b1ee4746e87b64
sharils <3063067+sharils@users.noreply.github.com>
2023-04-22 14:27:51 +0800

    Fix x mh

--- shell_plugins/x/mh.sh
+++ shell_plugins/x/mh.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 mh() {
-  dir="$HOME/git/github.com/sharils/~miraheze/$(d F)"
-  x mp "$dir"
+  dir="$HOME/git/github.com/sharils/~miraheze/$(date +%F)"
+  mkdir -pv "$dir"
   mv -v ~/Downloads/smilingpuffinwiki_xml_*.xml.gz "$dir"
 }
 
