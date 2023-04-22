d94e558142c1a3b265f891bbee3eac154dd3e176
sharils <3063067+sharils@users.noreply.github.com>
2023-04-22 17:19:31 +0800

    Add x dig

--- shell_plugins/x.sh
+++ shell_plugins/x.sh
@@ -10,6 +10,7 @@ x() {
   chunk) shift && fold -w "${@:-3}" | tr '\n' ' ' ;;
   cr) shift && /Applications/Chromium.app/Contents/MacOS/Chromium "$@" ;;
   diff) shift && diff --color --unified "$@" ;;
+  dig) shift && dig "@$(dig +nssearch "$@")" "$@" ;;
   f) shift && /Applications/Firefox.app/Contents/MacOS/firefox "$@" ;;
   fde) shift && /Applications/Firefox\ Developer\ Edition.app/Contents/MacOS/firefox "$@" ;;
   fzf) shift && fzf --select-1 --height=~14 --layout=reverse "$@" ;;
