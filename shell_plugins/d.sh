a73700d0a857cd6d3376b2f7a5f132ee0af9840e
sharils <3063067+sharils@users.noreply.github.com>
2023-04-22 17:28:56 +0800

    Change d default.template to default.conf.template

--- shell_plugins/d.sh
+++ shell_plugins/d.sh
@@ -34,7 +34,7 @@ d() {
     )
     ;;
 
-  default.template) "$SHARILS_HOME/shell_plugins/d/default.template.sh" "$@" ;;
+  default.conf.template) "$SHARILS_HOME/shell_plugins/d/default.conf.template.sh" "$@" ;;
 
   e)
     case "${1:-e}" in
