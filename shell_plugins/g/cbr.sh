b24c57220afc5fd339c7f353ff3334a0d80cee92
sharils <3063067+sharils@users.noreply.github.com>
2023-04-22 09:43:40 +0800

    Fix g cbr

--- shell_plugins/g/cbr.sh
+++ shell_plugins/g/cbr.sh
@@ -3,15 +3,15 @@
 cbr() {
   upstream="${1:-origin/develop}"
   current_branch="$(git rev-parse --abbrev-ref HEAD)"
-  g f
-  g cb |
+  git f
+  git cb |
     sed 's/^.* //' | grep -Ev '^(?:main|master)$' |
     xargs -I{} -n1 sh -c "git -c advice.skippedCherryPicks=false r $upstream {} || exit 255"
-  g cb |
+  git cb |
     sed 's/^.* //' | grep -E '^(?:main|master)$' |
     xargs -I{} -n1 sh -c "git -c advice.skippedCherryPicks=false r origin/{} {} || exit 255"
   exit_code=$?
-  g co "$current_branch"
+  git co "$current_branch"
   return $exit_code
 }
 
