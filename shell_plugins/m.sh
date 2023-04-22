be818f32d125b27f71d6800b82675c7bea3a14ba
sharils <3063067+sharils@users.noreply.github.com>
2023-04-22 18:29:05 +0800

    Fix m aws

--- shell_plugins/m.sh
+++ shell_plugins/m.sh
@@ -1,6 +1,8 @@
 m() {
   case "$1" in
 
+  aws) shift && aws "$@" help | l ;;
+
   erl) erl -man erl | l ;;
 
   i)
@@ -10,6 +12,8 @@ m() {
     fi
     shift && m install "$@"
     ;;
+
+  npm) shift && npm help "$@" | l ;;
 
   p)
     cmd="phx" && shift
@@ -38,11 +42,6 @@ m() {
     ;;
 
   sft) m strftime ;;
-
-  aws | npm)
-    cmd="$1" && shift
-    "$cmd" help "$@" | l
-    ;;
 
   app.config | app.start | app.tree | archive | archive.build | archive.install | archive.uninstall | clean | cmd | compile | deps | deps.clean | deps.compile | deps.get | deps.tree | deps.unlock | deps.update | do | escript | escript.build | escript.install | escript.uninstall | eval | format | help | hex | hex.audit | hex.build | hex.config | hex.docs | hex.info | hex.organization | hex.outdated | hex.owner | hex.package | hex.publish | hex.registry | hex.repo | hex.retire | hex.search | hex.sponsor | hex.user | loadconfig | local | local.hex | local.phx | local.public_keys | local.rebar | new | phx | phx.digest | phx.digest.clean | phx.gen | phx.gen.auth | phx.gen.cert | phx.gen.channel | phx.gen.context | phx.gen.embedded | phx.gen.html | phx.gen.json | phx.gen.live | phx.gen.notifier | phx.gen.presence | phx.gen.release | phx.gen.schema | phx.gen.secret | phx.gen.socket | phx.new | phx.new.ecto | phx.new.web | phx.routes | phx.server | profile.cprof | profile.eprof | profile.fprof | release | release.init | run | test | test.coverage | xref) mix "$@" ;;
 
