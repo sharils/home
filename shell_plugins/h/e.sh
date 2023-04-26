e5e094d25cca945b21f3b74580f252f1c4a26e00
sharils <3063067+sharils@users.noreply.github.com>
2023-04-26 09:30:29 +0800

    Remove h e install

--- shell_plugins/h/e.sh
+++ shell_plugins/h/e.sh
@@ -1,29 +1,23 @@
 #!/usr/bin/env sh
 
 e() {
-  case "${1:-install}" in
-  install) vim "$SHARILS_HOME/install" ;;
+  if [ $# -le 1 ]; then
+    vim "$SHARILS_HOME/shell_plugins/$*.sh"
+    return $?
+  fi
 
-  *)
-    if [ $# -le 1 ]; then
-      vim "$SHARILS_HOME/shell_plugins/$*.sh"
-      return $?
-    fi
+  tmp="$SHARILS_HOME/shell_plugins/$(echo "$*" | tr ' ' '/').sh"
+  if [ -f "$tmp" ]; then
+    vim "$tmp"
+    return $?
+  fi
 
-    tmp="$SHARILS_HOME/shell_plugins/$(echo "$*" | tr ' ' '/').sh"
-    if [ -f "$tmp" ]; then
-      vim "$tmp"
-      return $?
-    fi
-
-    cmd="$1" && shift
-    tmp="$(mktemp)"
-    grep --extended-regexp --line-number --with-filename "(?:^|\|)\s+\b$*\)" "$SHARILS_HOME/shell_plugins/$cmd.sh" >"$tmp"
-    set -- -q "$tmp"
-    [ "$(wc -l <"$tmp" | tr -d '[:space:]')" -ge 2 ] && set -- +copen "$@"
-    vim "$@"
-    ;;
-  esac
+  cmd="$1" && shift
+  tmp="$(mktemp)"
+  grep --extended-regexp --line-number --with-filename "(?:^|\|)\s+\b$*\)" "$SHARILS_HOME/shell_plugins/$cmd.sh" >"$tmp"
+  set -- -q "$tmp"
+  [ "$(wc -l <"$tmp" | tr -d '[:space:]')" -ge 2 ] && set -- +copen "$@"
+  vim "$@"
 }
 
 e "$@"
