c8a71f1e214ddab87bdf2b13112b1ac44e244c7d
sharils <3063067+sharils@users.noreply.github.com>
2023-05-10 21:13:15 +0800

    Fix n i

--- shell_plugins/n/i.sh
+++ shell_plugins/n/i.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env sh
 
 i() {
+  set -x
   case "$1" in
   y) shift && npm init --yes "$@" ;;
   @capacitor/app | expo-app | next-app | node-cli | nx-workspace | react-app | react-native-app)
     app="$1" && shift
-    n i y "$app" -- "${@:-$app}"
+    npm init --yes "$app" -- "${@:-$app}"
     cd "${1:-$app}" || return
     ;;
   *)
