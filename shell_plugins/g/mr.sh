f5ea7ed1dcc17f345be5ce0fb323b1fdd8385ea4
sharils <3063067+sharils@users.noreply.github.com>
2023-05-11 19:31:37 +0800

    Fix g mr

--- shell_plugins/g/mr.sh
+++ shell_plugins/g/mr.sh
@@ -2,35 +2,35 @@
 
 mr() {
   case "${1:-ls}" in
-  a) shift && g mr approve "$@" ;;
-  c) shift && g mr create --assignee "${G_MR_ASSIGNEE:?}" --reviewer "${G_MR_REVIEWER:?}" --target-branch "${G_MR_TARGET_BRANCH:-develop}" "$@" ;;
-  d) shift && g mr diff "$@" ;;
+  a) shift && mr approve "$@" ;;
+  c) shift && mr create --assignee "${G_MR_ASSIGNEE:?}" --reviewer "${G_MR_REVIEWER:?}" --target-branch "${G_MR_TARGET_BRANCH:-develop}" "$@" ;;
+  d) shift && mr diff "$@" ;;
   m)
     shift
     if expr "$*" : '^[[:space:][:digit:]][[:space:][:digit:]]*$' >/dev/null; then
       for cmd in "$@"; do
-        g mr merge "$cmd" --rebase --remove-source-branch --yes
+        mr merge "$cmd" --rebase --remove-source-branch --yes
       done
     else
-      until g mr merge "$@" --rebase --remove-source-branch --yes; do sleep 1; done
+      until mr merge "$@" --rebase --remove-source-branch --yes; do sleep 1; done
     fi
-    g f
+    git fetch --prune
     ;;
-  n) shift && g mr note "$@" ;;
+  n) shift && mr note "$@" ;;
   u)
     shift
     if ! expr "$*" : '^[[:space:][:digit:]][[:space:][:digit:]]*$' >/dev/null; then
-      g mr update "${@:---ready}"
+      mr update "${@:---ready}"
       return
     fi
     for cmd in "$@"; do
-      g mr u "$cmd" --ready
+      mr u "$cmd" --ready
     done
     ;;
-  r) shift && g mr rebase "$@" ;;
-  v) shift && g mr view "$@" ;;
+  r) shift && mr rebase "$@" ;;
+  v) shift && mr view "$@" ;;
   *[!0-9]*) glab mr "$@" ;;
-  *) g mr v "$@" ;;
+  *) mr v "$@" ;;
   esac
 }
 
