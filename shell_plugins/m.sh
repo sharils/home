067d6e36b4fe00615dd46da9c4335f187949074d
sharils <3063067+sharils@users.noreply.github.com>
2023-05-16 09:49:24 +0800

    Add m 977

--- shell_plugins/m.sh
+++ shell_plugins/m.sh
@@ -1,6 +1,8 @@
 m() {
   case "$1" in
 
+  977) open -b com.apple.Music 'https://music.apple.com/tw/station/classical-fm-97-7/ra.1459554452?l=en' ;;
+
   aws) shift && aws "$@" help | l ;;
 
   erl) erl -man erl | l ;;
