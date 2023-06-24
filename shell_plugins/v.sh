08c5693ee47dc4c5fd3ac516c636a1274540bdc6
sharils <3063067+sharils@users.noreply.github.com>
2023-06-24 12:10:46 +0800

    Add v mmd

--- shell_plugins/v.sh
+++ shell_plugins/v.sh
@@ -39,7 +39,7 @@ v() {
   s) vim server.js ;;
   tor) vim ~/Library/Application\ Support/TorBrowser-Data/Tor/torrc ;;
 
-  applescript | c | css | csv | eex | erl | ex | exs | gql | html | java | jq | js | json | jsx | lua | md | mdx | mw | nginx | pegjs | php | py | rs | scss | sh | sql | story | t | ts | tsx | txt | wat | xhtml | yaml | yml | zep)
+  applescript | c | css | csv | eex | erl | ex | exs | gql | html | java | jq | js | json | jsx | lua | md | mdx | mmd | mw | nginx | pegjs | php | py | rs | scss | sh | sql | story | t | ts | tsx | txt | wat | xhtml | yaml | yml | zep)
     if [ -f "$1" ]; then
       vim "$1"
       return
