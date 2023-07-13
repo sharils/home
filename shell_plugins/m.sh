dfe6cc03f7742ad6681054e2dc0245c878a66709
sharils <3063067+sharils@users.noreply.github.com>
2023-07-13 19:05:09 +0800

    Add m 247

--- shell_plugins/m.sh
+++ shell_plugins/m.sh
@@ -1,6 +1,8 @@
 m() {
   case "$1" in
 
+  247) open -b com.apple.Music 'https://stream.zeno.fm/s2888pqwzqzuv' ;;
+
   977) open -b com.apple.Music 'https://music.apple.com/tw/station/classical-fm-97-7/ra.1459554452?l=en' ;;
 
   aws) shift && aws "$@" help | l ;;
