0b9db99ac577622783bfeeff7659e212d5a07935
sharils <3063067+sharils@users.noreply.github.com>
2023-07-23 17:57:17 +0800

    Refactor r e

--- shell_plugins/r.sh
+++ shell_plugins/r.sh
@@ -15,8 +15,11 @@ r() {
     ;;
   e)
     shift
+    if [ $# -eq 0 ]; then
+      $EDITOR "$(find src/ -name main.rs -o -name lib.rs)"
+      return
+    fi
     case "$1" in
-    '') $EDITOR "$(find src/ -name main.rs -o -name lib.rs)" ;;
     *) rustc --explain "$@" | l md ;;
     esac
     ;;
