aeecfd88fb78799399970fced32f439a6a88b36d
sharils <3063067+sharils@users.noreply.github.com>
2023-08-26 14:43:52 +0800

    Change u e

--- shell_plugins/u.sh
+++ shell_plugins/u.sh
@@ -5,7 +5,7 @@ u() {
 
   clean) shift && rm "${@:--fir}" "$HOME/Library/Application Support/qutebrowser/webengine" ;;
 
-  e) $EDITOR "$HOME/Library/Preferences/qutebrowser/autoconfig.yml" "$HOME/.qutebrowser/bookmarks/urls" "$HOME/.qutebrowser/quickmarks" "$HOME/.qutebrowser/greasemonkey" "$HOME/.qutebrowser/config.py" ;;
+  e) vim -p "$HOME/Library/Preferences/qutebrowser/autoconfig.yml" "$HOME/.qutebrowser/bookmarks/urls" "$HOME/.qutebrowser/quickmarks" "$HOME/.qutebrowser/greasemonkey" "$HOME/.qutebrowser/config.py" ;;
 
   l) shift && l "$@" "$HOME/Library/Application Support/qutebrowser/webengine" ;;
 
