5ee03ae1764f2ac00e293c0734915b8681d38e36
sharils <3063067+sharils@users.noreply.github.com>
2023-09-02 16:14:57 +0800

    Refactor qutebrowser config.py

--- .qutebrowser/config.py
+++ .qutebrowser/config.py
@@ -4,7 +4,7 @@ config.load_autoconfig()
 
 c.colors.webpage.darkmode.enabled = (
     subprocess.run(
-        'defaults read -g AppleInterfaceStyle 2>&1', capture_output=True, shell=True
+        "defaults read -g AppleInterfaceStyle 2>&1", capture_output=True, shell=True
     ).stdout
-    == b'Dark\n'
+    == b"Dark\n"
 )
