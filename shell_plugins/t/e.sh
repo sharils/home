04b70e56bdc18893faf84c178e118569438319ce
sharils <3063067+sharils@users.noreply.github.com>
2023-09-03 13:38:55 +0800

    Fix t e [^[:digit:]]+

--- shell_plugins/t/e.sh
+++ shell_plugins/t/e.sh
@@ -5,7 +5,7 @@ e() {
   cfg) shift && $EDITOR "$@" ~/.todo.cfg ;;
   q) shift && $EDITOR "$@" "$TODO_DIR/QuickNote.md" ;;
   t) shift && $EDITOR "$@" "$TODO_FILE" ;;
-  *[!0-9]*) $EDITOR "$@" "$TODO_FILE" ;;
+  *[!0-9]*) $EDITOR "$TODO_DIR/$*.txt" ;;
   *)
     cmd="$1" && shift
     $EDITOR "+$cmd" "$@" "$TODO_FILE"
