84a9800a94278481fef1d84824b9e85517fbd1ff
sharils <3063067+sharils@users.noreply.github.com>
2023-09-09 07:57:26 +0800

    Change x w8

--- shell_plugins/x/w8.sh
+++ shell_plugins/x/w8.sh
@@ -4,7 +4,9 @@ w8() {
   dir="$HOME/Sync/Libera PRO"
   file="$dir/${2:?}.txt"
   curl --compressed --location --silent --output "$file" "https://dl1.wenku8.com/down.php?type=big5&id=${1:?}"
-  echo "$file"
+  echo "mv '$file' '$dir/next' # or"
+  echo "vim '$file'" | pbcopy
+  echo "$(pbpaste) # copied to clipboard"
 }
 
 w8 "$@"
