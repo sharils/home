0d9c4fc9a8cb5f5b5ab628675ff971e666c819af
sharils <3063067+sharils@users.noreply.github.com>
2023-09-09 07:43:02 +0800

    Change g e

--- shell_plugins/g/e.sh
+++ shell_plugins/g/e.sh
@@ -4,10 +4,10 @@ e() {
   $EDITOR "${@:--p}" \
     "$SHARILS_HOME/alias.gitconfig" \
     "$SHARILS_HOME/shell_plugins/g.sh" \
-    "$SHARILS_HOME/.vim/plugged/after/plugin/vim-fugitive.vim" \
     "$SHARILS_HOME/.tig/bind-gitconfig.tigrc" \
     "$SHARILS_HOME/.tig/bind-fugitive.tigrc" \
     "$SHARILS_HOME/.tig/bind-deprecated.tigrc" \
+    "$SHARILS_HOME/.vim/plugged/after/plugin/vim-fugitive.vim" \
     "$SHARILS_HOME/.vim/plugged/vim-fugitive/autoload/fugitive.vim" \
     "$SHARILS_HOME/.gitignore" \
     "$SHARILS_HOME/.gitconfig"
