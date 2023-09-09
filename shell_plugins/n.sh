0985232b0d65d12dfec9d2b629a5b1854354efd6
sharils <3063067+sharils@users.noreply.github.com>
2023-09-09 17:40:24 +0800

    Refactor n e

--- shell_plugins/n.sh
+++ shell_plugins/n.sh
@@ -26,22 +26,7 @@ n() {
     n degit "$@"
     ;;
 
-  e)
-    shift
-    if [ $# -eq 0 ]; then
-      $EDITOR ./package.json
-      return $?
-    fi
-    case "$1" in
-    m) vim "$(npm pkg get main | jq -r)" ;;
-    s) vim server.js ;;
-    *)
-      tmp="$(mktemp)"
-      grep --line-number --with-filename "$1" package.json >"$tmp"
-      vim -q "$tmp"
-      ;;
-    esac
-    ;;
+  e) shift && "$SHARILS_HOME/shell_plugins/n/e.sh" "$@" ;;
 
   eas) shift && n eas-cli "$@" ;;
 
---
+++ shell_plugins/n/e.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+e() {
+  if [ $# -eq 0 ]; then
+    $EDITOR ./package.json
+    return $?
+  fi
+  case "$1" in
+  m) vim "$(npm pkg get main | jq -r)" ;;
+  s) vim server.js ;;
+  *)
+    tmp="$(mktemp)"
+    grep --line-number --with-filename "$1" package.json >"$tmp"
+    vim -q "$tmp"
+    ;;
+  esac
+}
+
+e "$@"
