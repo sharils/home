39fcbc2a50acb01dee8d200abb6131eb4533a476
sharils <3063067+sharils@users.noreply.github.com>
2023-09-09 17:36:08 +0800

    Change m 247 to m o 247

--- shell_plugins/m/o.sh
+++ shell_plugins/m/o.sh
@@ -3,6 +3,7 @@
 o() {
   case "$1" in
   91) open -b com.apple.Music 'https://jazzfm91.streamb.live/SB00009' ;;
+  247) open -b com.apple.Music 'https://stream.zeno.fm/s2888pqwzqzuv' ;;
   esac
 }
 
