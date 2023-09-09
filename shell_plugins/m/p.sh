c2c4a6a70070adccc0c95dc3eea8e9e660023ae7
sharils <3063067+sharils@users.noreply.github.com>
2023-09-10 00:41:22 +0800

    Refactor m p n

--- shell_plugins/m/p.sh
+++ shell_plugins/m/p.sh
@@ -18,46 +18,7 @@ p() {
     esac
     ;;
   i) shift && iex -S mix phx.server "$@" ;;
-  n)
-    shift
-    path="${1:-hello_elixir}" && shift
-    mix phx.new "$path" "$@" --install
-    cd "$path" || return $?
-
-    cat <<'YAML' >docker-compose.yml
-# https://docs.docker.com/compose/compose-file/compose-file-v3/
-version: "3.8"
-services:
-  db:
-    # https://hub.docker.com/_/postgres
-    image: postgres:15.2-alpine3.17
-    restart: always
-    environment:
-      POSTGRES_USER:
-      POSTGRES_PASSWORD:
-      POSTGRES_DB:
-    ports:
-      - 5432:5432
-    profiles:
-      - psql
-YAML
-
-    cat <<YAML >.envrc
-#!/usr/bin/env sh
-
-export POSTGRES_USER='postgres'
-export POSTGRES_PASSWORD='postgres'
-export POSTGRES_DB='${path}_dev'
-YAML
-
-    cat <<SH
-cd $path
-direnv allow
-docker compose --profile db start
-mix ecto.create
-docker compose --profile db stop
-SH
-    ;;
+  n) shift && "$SHARILS_HOME/shell_plugins/m/p/n.sh" "$@" ;;
   s) shift && mix phx.server "$@" ;;
   v) shift && mix phx.new --version "$@" ;;
   esac
