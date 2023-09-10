0ef2ab30b385a250a2c3da09d914394d4bb77f15
sharils <3063067+sharils@users.noreply.github.com>
2023-09-10 17:46:19 +0800

    Add t oot alias

--- shell_plugins/t/oot.sh
+++ shell_plugins/t/oot.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env sh
 
 oot() {
+  case "$1" in
+  l)
+    shift
+    case "$1" in
+    l) shift && set -- --local --reverse --count 1 "${@:---public}" ;;
+    p) shift && set -- --public --reverse --count 1 "$@" ;;
+    t) shift && set -- --reverse --count 1 --tag "$@" ;;
+    esac
+    set -- timeline "$@"
+    ;;
+  n) shift && set -- notifications "${@:---reverse}" ;;
+  p) shift && set -- post "${@:---reverse}" ;;
+  t) shift && set -- thread "$@" ;;
+  ui) shift && set -- tui "$@" ;;
+  esac
+
   [ -n "$TOOT_USING" ] && set -- "$@" --using "$TOOT_USING"
+
   toot "$@"
 }
 
