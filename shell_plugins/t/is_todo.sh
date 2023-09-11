8349058cb6bbee666ef4f7ec859f2aa2531dd2c2
sharils <3063067+sharils@users.noreply.github.com>
2023-09-11 19:18:27 +0800

    Fix t is_todo

--- shell_plugins/t/is_todo.sh
+++ shell_plugins/t/is_todo.sh
@@ -2,7 +2,7 @@
 
 is_todo() {
   case "$1" in
-  add | a | addm | addto | append | app | archive | b | command | d | deduplicate | del | e | m | r | rm | depri | dp | done | do | help | list | ls | listall | lsa | listaddons | listcon | lsc | listfile | lf | listpri | lsp | listproj | lsprj | move | mv | prepend | prep | pri | replace | report | shorthelp | z) return 0 ;;
+  add | a | addm | addto | append | app | archive | command | deduplicate | del | rm | depri | dp | done | do | help | list | ls | listall | lsa | listaddons | listcon | lsc | listfile | lf | listpri | lsp | listproj | lsprj | move | mv | prepend | prep | pri | replace | report | shorthelp) return 0 ;;
   *) return 1 ;;
   esac
 }
