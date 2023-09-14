e635cf62945734829f77bd3f500b66b980908146
sharils <3063067+sharils@users.noreply.github.com>
2023-09-14 20:06:39 +0800

    Add p rs

--- shell_plugins/p.sh
+++ shell_plugins/p.sh
@@ -25,6 +25,11 @@ p() {
 
   rm) shift && poetry remove "$@" ;;
 
+  rs)
+    proto_files="$2" && shift 2 && out_dir="${1:-.}" && shift
+    protoc --rust_out="experimental-codegen=enabled,kernel=cpp:$out_dir" "$@" "$proto_files"
+    ;;
+
   venv) x venv "$(poetry env info --path)" ;;
 
   *) echo not implemented >&2 ;;
