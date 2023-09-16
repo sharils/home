d1b911dd42c7745b0444ac599fdc3b8e3805af61
sharils <3063067+sharils@users.noreply.github.com>
2023-09-16 10:59:28 +0800

    Refactor c

--- shell_plugins/c.sh
+++ shell_plugins/c.sh
@@ -7,25 +7,34 @@ c() {
   fi
 
   case "$1" in
+
   a) shift && set -- add "$@" ;;
+
   b)
     shift
     case "$1" in
     r) shift && set -- --release "$@" ;;
+
     esac
     set -- build "$@"
     ;;
+
   d)
     case "$2" in
     o) shift 2 && set -- d --open "$@" ;;
+
     esac
     ;;
+
   e) shift && $EDITOR Cargo.toml && true && return $? ;;
+
   p) pwd | pbcopy && return $? ;;
+
   n)
     shift
     case "$1" in
     l) shift && set -- --lib "${@:-rust-app}" ;;
+
     esac
     cargo new "${@:-rust-app}"
     for last in "$@"; do :; done
@@ -33,7 +42,9 @@ c() {
     G_I_M="Execute cargo new ${*:-rust-app}" g i
     return $?
     ;;
+
   u) set -- update "$@" ;;
+
   esac
   cargo "$@"
 }
