cf08058ef5919d81bacdc077de01b1a33dbdc0d9
sharils <3063067+sharils@users.noreply.github.com>
2023-09-16 18:29:22 +0800

    Add n i puck-app

--- shell_plugins/n/i.sh
+++ shell_plugins/n/i.sh
@@ -4,7 +4,7 @@ i() {
   set -x
   case "$1" in
   y) shift && npm init --yes "$@" ;;
-  @capacitor/app | expo-app | next-app | node-cli | nx-workspace | react-app | react-native-app)
+  @capacitor/app | expo-app | next-app | node-cli | nx-workspace | puck-app | react-app | react-native-app)
     app="$1" && shift
     npm init --yes "$app" -- "${@:-$app}"
     cd "${1:-$app}" || return
