59726375b196f2f7ee230ac18509713066595e40
sharils <3063067+sharils@users.noreply.github.com>
2023-09-16 17:59:56 +0800

    Add k credit-card

---
+++ shell_plugins/k/credit-card.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+credit_card() {
+  cat <<'EOF'
+VISA: 4111 1111 1111 1111
+MasterCard: 5555 5555 5555 4444
+Discover: 6011 6011 6011 6611
+AmEx: 3782 8224 6310 005
+EOF
+}
+
+credit_card "$@"
