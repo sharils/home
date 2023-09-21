a02a5a8d670a4b12a23019afc9fa7e95b00cd2f5
sharils <3063067+sharils@users.noreply.github.com>
2023-09-21 22:47:03 +0800

    Fix t su e

--- shell_plugins/t/su.sh
+++ shell_plugins/t/su.sh
@@ -12,9 +12,14 @@ t_su() {
     return
   fi
 
-  auth="$(t_su | grep "$1" | fzf --select-1 --height=~14 --layout=reverse)" && shift
-  echo "TOOT_USING=$auth" >&2
-  TOOT_USING="${auth:?}" "$SHARILS_HOME/shell_plugins/t/oot.sh" "$@"
+  case "$1" in
+  e) shift && TODO_FILE="$TODO_SU" "$SHARILS_HOME/shell_plugins/t/e.sh" "$@" ;;
+  *)
+    auth="$(t_su | grep "$1" | fzf --select-1 --height=~14 --layout=reverse)" && shift
+    echo "TOOT_USING=$auth" >&2
+    TOOT_USING="${auth:?}" "$SHARILS_HOME/shell_plugins/t/oot.sh" "$@"
+    ;;
+  esac
 }
 
 t_su "$@"
