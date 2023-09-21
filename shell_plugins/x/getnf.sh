4f813ba9eb9cf77083ecdc708c076d48c2e4e5b2
sharils <3063067+sharils@users.noreply.github.com>
2023-09-21 22:13:17 +0800

    Add x getnf

--- shell_plugins/x/getnf.sh
+++ shell_plugins/x/getnf.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
 getnf() {
-  :
+  [ -f '/tmp/getnf' ] || curl -s 'https://raw.githubusercontent.com/ronniedroid/getnf/master/getnf' >'/tmp/getnf'
+  bash '/tmp/getnf'
 }
 
 getnf "$@"
