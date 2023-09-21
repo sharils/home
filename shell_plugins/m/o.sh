ad97b9ce223394ae5e5cf5105f231bd7c22cc50d
sharils <3063067+sharils@users.noreply.github.com>
2023-09-21 22:16:49 +0800

    Add m o a1

--- shell_plugins/m/o.sh
+++ shell_plugins/m/o.sh
@@ -5,6 +5,7 @@ o() {
   91) open -b com.apple.Music 'https://jazzfm91.streamb.live/SB00009' ;;
   247) open -b com.apple.Music 'https://stream.zeno.fm/s2888pqwzqzuv' ;;
   977) open -b com.apple.Music 'https://music.apple.com/tw/station/classical-fm-97-7/ra.1459554452?l=en' ;;
+  a1) open -b com.apple.Music 'https://music.apple.com/tw/station/apple-music-1/ra.978194965?l=en-GB' ;;
   bbc) open -b com.apple.Music 'https://music.apple.com/tw/station/bbc-radio-3/ra.1461288674?l=en' ;;
   cap) open -b com.apple.Music 'https://playerservices.streamtheworld.com/pls/ClassicalStreamAAC.pls' ;;
   kamu) open -b com.apple.Music 'https://kamu.streamguys1.com/hd2-64' ;;
