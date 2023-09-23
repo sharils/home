95d9f88f543e152a2432dc9fa3363401c165df78
sharils <3063067+sharils@users.noreply.github.com>
2023-09-23 19:16:14 +0800

    Change x mh

--- shell_plugins/x/mh.sh
+++ shell_plugins/x/mh.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 mh() {
-  dir="$HOME/git/github.com/sharils/~miraheze/$(date +%F)"
+  dir="$HOME/git/github.com/sharils/~backup/miraheze/$(date +%F)"
   mkdir -pv "$dir"
   mv -v ~/Downloads/smilingpuffinwiki_xml_*.xml.gz "$dir"
 }
