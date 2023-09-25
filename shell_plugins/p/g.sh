3c772c300aa0c44e88aed832ce03a52714bd2d3c
sharils <3063067+sharils@users.noreply.github.com>
2023-09-25 21:18:05 +0800

    Add p g f r

--- shell_plugins/p/g.sh
+++ shell_plugins/p/g.sh
@@ -4,12 +4,18 @@
 
 g() {
   case "$1" in
-  f) shift && set -- freeze "$@";;
-  i) shift && set -- install "$@";;
-  ls) shift && set -- list "$@";;
-  s) shift && set -- search "$@";;
-  rm) shift && set -- uninstall "$@";;
-  v) shift && set -- show "$@";;
+  f)
+    shift
+    case "$1" in
+    r) shift && PIP_USER=1 pip freeze > requirements.txt ;;
+    esac
+    set -- freeze "$@"
+    ;;
+  i) shift && set -- install "$@" ;;
+  ls) shift && set -- list "$@" ;;
+  s) shift && set -- search "$@" ;;
+  rm) shift && set -- uninstall "$@" ;;
+  v) shift && set -- show "$@" ;;
   esac
   PIP_USER=1 pip "$@"
 }
