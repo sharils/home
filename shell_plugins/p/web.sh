7e4ae60d71a0182cd211045ec333736ad2b061d8
sharils <3063067+sharils@users.noreply.github.com>
2023-09-28 23:09:05 +0800

    Fix p web

--- shell_plugins/p/web.sh
+++ shell_plugins/p/web.sh
@@ -8,7 +8,7 @@ web() {
   ts) import_style='typescript' ;;
   web) import_style='commonjs' ;;
   esac
-  proto_files="$2" && shift 2 && out_dir="${1:-.}" && shift
+  proto_files="${2:?}" && shift 2 && out_dir="${1:-.}" && shift
   protoc --grpc-web_out="import_style=$import_style,mode=grpcwebtext:$out_dir" "$proto_files"
 }
 
