f7a01f5e41e317f1a11efcc6a7b698659b30970c
sharils <3063067+sharils@users.noreply.github.com>
2023-09-28 23:07:58 +0800

    Fix p js

--- shell_plugins/p/js.sh
+++ shell_plugins/p/js.sh
@@ -3,7 +3,7 @@
 [ -n "$SET_X" ] && set -x
 
 js() {
-  proto_files="$2" && shift 2 && out_dir="${1:-.}" && shift
+  proto_files="${1:?}" && shift && out_dir="${1:-.}" && shift
   "$HOMEBREW_PREFIX/opt/protobuf@3/bin/protoc" --js_out="import_style=commonjs,binary:$out_dir" "$@" "$proto_files"
 }
 
