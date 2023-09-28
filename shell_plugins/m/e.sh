bf7e1bba2d3fa11408b9b2e8fb8b77aa2c13577d
sharils <3063067+sharils@users.noreply.github.com>
2023-09-28 23:25:17 +0800

    Add m e m

--- shell_plugins/m/e.sh
+++ shell_plugins/m/e.sh
@@ -9,6 +9,14 @@ e() {
   fi
   case "$1" in
   c) shift && mix ecto.create "$@" ;;
+  m)
+    shift
+    case "$1" in
+    g) mix ecto.gen.migration "$@" ;;
+    ls) mix ecto.migrations "$@" ;;
+    *) mix ecto.migrate "$@" ;;
+    esac
+    ;;
   *)
     tmp="$(mktemp)"
     grep --line-number --with-filename "$1" mix.exs >"$tmp"
