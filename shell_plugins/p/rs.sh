8c8dcb562f7528016431207840be171e5d75ee79
sharils <3063067+sharils@users.noreply.github.com>
2023-09-28 23:09:01 +0800

    Fix p rs

--- shell_plugins/p/rs.sh
+++ shell_plugins/p/rs.sh
@@ -3,7 +3,7 @@
 [ -n "$SET_X" ] && set -x
 
 rs() {
-  proto_files="$2" && shift 2 && out_dir="${1:-.}" && shift
+  proto_files="${1:?}" && shift && out_dir="${1:-.}" && shift
   protoc --rust_out="experimental-codegen=enabled,kernel=cpp:$out_dir" "$@" "$proto_files"
 }
 
