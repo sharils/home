155cd564d145024747d95270ccaf8f21c609c671
sharils <3063067+sharils@users.noreply.github.com>
2023-09-30 15:40:15 +0800

    Change x hbt

--- shell_plugins/x/hbt.sh
+++ shell_plugins/x/hbt.sh
@@ -5,7 +5,7 @@
 hbt() {
   dir="$HOME/git/github.com/sharils/~backup/habitica/$(date +%F)"
   mkdir -pv "$dir"
-  mv -v ~/Downloads/habitica-user-data.json "$dir"
+  mv -v ~/Downloads/habitica-user-data.json ~/Downloads/habitica-tasks-history.csv "$dir"
 }
 
 hbt "$@"
