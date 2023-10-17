1251f0a3c7d825f46fc83c94abe52d7a50822590
sharils <3063067+sharils@users.noreply.github.com>
2023-10-18 00:10:57 +0800

    Add m pieces

--- shell_plugins/m.sh
+++ shell_plugins/m.sh
@@ -37,6 +37,12 @@ EOF
 
   p) shift && "$SHARILS_HOME/shell_plugins/m/p.sh" "$@" ;;
 
+  pieces)
+    cat <<'EOF' | sed 's/, /\n/g' | sort -R | head -n1
+K265, BWV881, BWV1007-1012, Butterfly Lover, Carmina Burana, Goldberg Variation, Also sprach Zarathustra, Turandot, Má vlast, Tannhäuser, Der Fliegende Holländer, L'Apprenti sorcier, Ночь на лысой горе, Guillaume Tell, Tristan und Isolde, Peer Gynt Suites, Night on Bald Mountain, Night on Bald Mountain, Aida, Requiem (Verdi), Peter and the Wolf, Carnival of the Animals, Csárdás
+EOF
+    ;;
+
   pj) m package-json ;;
 
   r) shift && "$SHARILS_HOME/shell_plugins/m/r.sh" "$@" ;;
