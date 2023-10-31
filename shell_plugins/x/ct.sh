f2d116d0a9c5ac819670e2b47e0f64cdb41d0bf2
sharils <3063067+sharils@users.noreply.github.com>
2023-10-31 20:21:26 +0800

    Refactor x ct

--- shell_plugins/x/ct.sh
+++ shell_plugins/x/ct.sh
@@ -3,6 +3,7 @@
 [ -n "$SET_X" ] && set -x
 
 # Default to 南投縣 for geographic center
+# https://data.gov.tw/dataset/75380
 ct() {
   [ -f /tmp/A-B0062-001.json ] || curl --location 'https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/A-B0062-001?Authorization=rdec-key-123-45678-011121314&format=JSON' >/tmp/A-B0062-001.json
 
