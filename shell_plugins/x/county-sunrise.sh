3fd98f6b270c6e5e62adea6ac895d4059b869634
sharils <3063067+sharils@users.noreply.github.com>
2023-10-31 20:26:49 +0800

    Change x county-sunrise

--- shell_plugins/x/county-sunrise.sh
+++ shell_plugins/x/county-sunrise.sh
@@ -4,7 +4,7 @@
 
 # Default to 南投縣 for geographic center
 # https://data.gov.tw/dataset/75380
-ct() {
+county_sunrise() {
   [ -f /tmp/A-B0062-001.json ] || curl --location 'https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/A-B0062-001?Authorization=rdec-key-123-45678-011121314&format=JSON' >/tmp/A-B0062-001.json
 
   # shellcheck disable=SC2016
@@ -26,4 +26,4 @@ JQ
     bat --language json --style=plain
 }
 
-ct "$@"
+county_sunrise "$@"
