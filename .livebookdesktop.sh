79b0bf5a42c4d970b01af4139fa367133364477d
sharils <3063067+sharils@users.noreply.github.com>
2023-11-04 13:36:15 +0800

    Add .livebookdesktop.sh

---
+++ .livebookdesktop.sh
@@ -0,0 +1,10 @@
+# This file is used to configure Livebook before booting.
+# If you change this file, you must restart Livebook for your changes to take place.
+# See https://hexdocs.pm/livebook/readme.html#environment-variables for all available environment variables.
+
+# # Allow Livebook to connect to remote machines over IPv6
+# export LIVEBOOK_DISTRIBUTION=name
+# export ERL_AFLAGS="-proto_dist inet6_tcp"
+
+# # Add Homebrew to PATH
+# export PATH=/opt/homebrew/bin:$PATH
