17bf7d24ac0a75592542360122ef0c3d6e5368da
sharils <3063067+sharils@users.noreply.github.com>
2023-11-04 09:23:29 +0800

    Change x su

--- shell_plugins/x/su.sh
+++ shell_plugins/x/su.sh
@@ -3,8 +3,21 @@
 [ -n "$SET_X" ] && set -x
 
 _su() {
-  [ $# -ge 1 ] && set -- -c "$*"
-  sudo su - "${X_SU:?}" "$@"
+  if [ $# -eq 0 ]; then
+    sudo su - "${X_SU:?}" "$@"
+    return $?
+  fi
+
+  case "$1" in
+  b)
+    shift
+    case "$1" in
+    i) shift && set -- install "$@" ;;
+    esac
+    set -- brew "$@"
+    ;;
+  esac
+  sudo su "${X_SU:?}" -c "$*"
 }
 
 _su "$@"
