647c082c60629ad63ad49ffc58c10a5015b2b5b5
sharils <3063067+sharils@users.noreply.github.com>
2023-11-06 19:41:31 +0800

    Refactor x st

--- shell_plugins/x/st.sh
+++ shell_plugins/x/st.sh
@@ -4,15 +4,21 @@
 
 st() {
   case "$1" in
+
   l) shift && st ls | jq length ;;
+
   ls) shift && st cli show connections | jq '.connections | to_entries | map(select(.value.connected)) | from_entries' ;;
+
   o) shift && open -b com.github.xor-gate.syncthing-macosx "$@" ;;
+
   ps)
     shift
     # shellcheck disable=SC2009
     ps aux | grep syncthing
     ;;
+
   q) shift && osascript -e 'quit app "Syncthing"' ;;
+
   su)
     shift
     case "$1" in
@@ -20,6 +26,7 @@ st() {
     *) echo not implemented >&2 ;;
     esac
     ;;
+
   t)
     shift
     case "$(osascript -e 'tell application "System Events" to (name of processes) contains "Syncthing"')" in
@@ -27,7 +34,9 @@ st() {
     true) osascript -e 'quit app "Syncthing"' ;;
     esac
     ;;
+
   *) /Applications/Syncthing.app/Contents/Resources/syncthing/syncthing "${@:---help}" ;;
+
   esac
 }
 
