9003cfa2691d5d45cac8c3a9b05a3257e6de90db
sharils <3063067+sharils@users.noreply.github.com>
2023-11-23 17:33:35 +0800

    Add a ls a js

--- shell_plugins/a/ls.sh
+++ shell_plugins/a/ls.sh
@@ -4,6 +4,13 @@
 
 a_ls() {
   case "$1" in
+  a)
+    shift
+    case "$1" in
+    js) shift && asdf list all nodejs "$2" ;;
+    *) echo 'not implemented' >&2 ;;
+    esac
+    ;;
   js) shift && asdf list nodejs "$2" ;;
   *) aws lightsail "$@" ;;
   esac
