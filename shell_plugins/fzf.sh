3fb5d2b9b5c5bd53019f1ccd1698f08ad103432f
sharils <3063067+sharils@users.noreply.github.com>
2023-11-28 19:42:40 +0800

    Change fzf

--- shell_plugins/fzf.sh
+++ shell_plugins/fzf.sh
@@ -5,11 +5,11 @@
 [ -f ~/.fzf.bash ] && . ~/.fzf.bash
 
 export FZF_COMPLETION_TRIGGER='*'
-export FZF_DEFAULT_COMMAND="fd --hidden --unrestricted --color always --ignore-file $HOME/.fdignore"
+export FZF_DEFAULT_COMMAND="fd --hidden --unrestricted --color always --ignore-file $HOME/.fdignore --ignore-file $PWD/gitignore.fdignore"
 export FZF_DEFAULT_OPTS='--ansi --cycle --multi'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_OPTS='--preview "bat --color=always --line-range :500 {}"'
-export FZF_ALT_C_COMMAND="fd --hidden --unrestricted --color always --ignore-file $HOME/.fdignore --type d"
+export FZF_ALT_C_COMMAND="fd --hidden --unrestricted --color always --ignore-file $HOME/.fdignore --ignore-file $PWD/gitignore.fdignore --type d"
 
 # https://github.com/junegunn/fzf/blob/master/README.md#supported-commands
 _fzf_setup_completion path b g l v w x
