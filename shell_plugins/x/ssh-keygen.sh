06353b6063db5dc9b13993f16589be6c7784521f
sharils <3063067+sharils@users.noreply.github.com>
2023-12-09 17:07:00 +0800

    Refactor x ssh-keygen

--- shell_plugins/x/ssh-keygen.sh
+++ shell_plugins/x/ssh-keygen.sh
@@ -2,11 +2,11 @@
 
 [ -n "$SET_X" ] && set -x
 
-ssh_keygen() {
-  /usr/bin/env ssh-keygen -f ~/.ssh/id_ed25519 -t ed25519 -C "${@:?}"
+x_ssh_keygen() {
+  ssh-keygen -f ~/.ssh/id_ed25519 -t ed25519 -C "${@:?}"
   pbcopy <~/.ssh/id_ed25519.pub
   pbpaste
   cp ~/.ssh/id_ed25519.pub "$HOME/.ssh/$*.pub"
 }
 
-ssh_keygen "$@"
+x_ssh_keygen "$@"
