7ff30c16777b804413509ce4c5e4cef6c1474f07
sharils <3063067+sharils@users.noreply.github.com>
2023-12-09 17:07:19 +0800

    Change x ssh-keygen

--- shell_plugins/x/ssh-keygen.sh
+++ shell_plugins/x/ssh-keygen.sh
@@ -3,7 +3,7 @@
 [ -n "$SET_X" ] && set -x
 
 x_ssh_keygen() {
-  ssh-keygen -f ~/.ssh/id_ed25519 -t ed25519 -C "${@:?}"
+  ssh-keygen -f ~/.ssh/id_ed25519 -t ed25519 -C "${@:?email?}"
   pbcopy <~/.ssh/id_ed25519.pub
   pbpaste
   cp ~/.ssh/id_ed25519.pub "$HOME/.ssh/$*.pub"
