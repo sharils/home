cb07c9af3ea58aedad9247c2486ba881a2cdaff3
sharils <3063067+sharils@users.noreply.github.com>
2023-12-09 11:30:38 +0800

    Add x w8/0

--- shell_plugins/x/w8.sh
+++ shell_plugins/x/w8.sh
@@ -4,6 +4,10 @@
 
 w8() {
   dir="$HOME/Sync/Libera PRO"
+  if [ $# -eq 0 ]; then
+    ls "$dir"
+    return $?
+  fi
   file="$dir/${2:?}.txt"
   curl --compressed --location --silent --output "$file" "https://dl1.wenku8.com/down.php?type=big5&id=${1:?}"
   echo "mv '$file' '$dir/next' # or"
