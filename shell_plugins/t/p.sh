d08fd27be38689c7f67c0cf7c70270a8bdb6fad8
sharils <3063067+sharils@users.noreply.github.com>
2023-12-15 21:11:56 +0800

    Fix t p

--- shell_plugins/t/p.sh
+++ shell_plugins/t/p.sh
@@ -5,7 +5,7 @@
 p() {
   case "$1" in
   e) shift && p --editor "$EDITOR" "$@" ;;
-  '' | *[!0-9]*) "$SHARILS_HOME/shell_plugins/t/oot.sh" post "$@" ;;
+  '' | *[!0-9]*) "$SHARILS_HOME/shell_plugins/t/oot.sh" post "${@:---editor}" ;;
   *) todo.sh pri "$@" ;;
   esac
 }
