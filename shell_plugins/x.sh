b1ba4b621dba7b9826078478a52c1e373790506d
sharils <3063067+sharils@users.noreply.github.com>
2023-12-26 09:02:53 +0800

    Add x iconv

--- shell_plugins/x.sh
+++ shell_plugins/x.sh
@@ -39,7 +39,7 @@ x() {
     x fx "$cmd" | x bc "$(awk '{ print $3 }')" '*' "${@:-1}"
     ;;
 
-  ai | ap | aq | bc | cal | cat-weight | cert | cm | county-sunrise | data | editorconfig | eq | erl | ex | fer | focus | fong | fx | getnf | gif | hbt | html | ico | jq | kibble | law | members | mh | paisa | pdf | png | py | rm | salary | sh | shutdown | smolsite | sn | sqlite | ssh-keygen | ss | su | touch | tree | tz | uri | venv | webp | whois | zip)
+  ai | ap | aq | bc | cal | cat-weight | cert | cm | county-sunrise | data | editorconfig | eq | erl | ex | fer | focus | fong | fx | getnf | gif | hbt | html | ico | iconv | jq | kibble | law | members | mh | paisa | pdf | png | py | rm | salary | sh | shutdown | smolsite | sn | sqlite | ssh-keygen | ss | su | touch | tree | tz | uri | venv | webp | whois | zip)
     cmd="$1" && shift
     "$SHARILS_HOME/shell_plugins/x/$cmd.sh" "$@"
     ;;
