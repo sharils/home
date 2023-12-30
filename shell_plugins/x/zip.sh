f8b1823fc70e97017c3163989fe12c16d5cbfbfd
sharils <3063067+sharils@users.noreply.github.com>
2023-12-30 20:43:50 +0800

    Add x zip code

--- shell_plugins/x/zip.sh
+++ shell_plugins/x/zip.sh
@@ -3,7 +3,10 @@
 [ -n "$SET_X" ] && set -x
 
 x_zip() {
-  zip --encrypt --recurse-paths ~"/Downloads/$(date -uIseconds | sed 's#+00:00$#Z#;s#[^[:digit:]T]##g').zip" "$@"
+  case "$1" in
+  code) shift && "$SHARILS_HOME/shell_plugins/x/zip/code.sh" "$@" ;;
+  *) zip --encrypt --recurse-paths ~"/Downloads/$(date -uIseconds | sed 's#+00:00$#Z#;s#[^[:digit:]T]##g').zip" "$@" ;;
+  esac
 }
 
 x_zip "$@"
