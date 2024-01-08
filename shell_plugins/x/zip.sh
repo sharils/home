6e005472f39221673eefe22e5ff9cefbc8d5b531
sharils <3063067+sharils@users.noreply.github.com>
2024-01-08 19:40:07 +0800

    Change x zip

--- shell_plugins/x/zip.sh
+++ shell_plugins/x/zip.sh
@@ -5,7 +5,13 @@
 x_zip() {
   case "$1" in
   code) shift && "$SHARILS_HOME/shell_plugins/x/zip/code.sh" "$@" ;;
-  *) zip --encrypt --recurse-paths "$HOME/Downloads/$(date -uIseconds | sed 's#+00:00$#Z#;s#[^[:digit:]T]##g').zip" "$@" ;;
+  *)
+    if [ ! -t 0 ]; then
+      set -- "$(mktemp)" "$@"
+      cat >"$1"
+    fi
+    zip --encrypt --recurse-paths "$HOME/Downloads/$(date -uIseconds | sed 's#+00:00$#Z#;s#[^[:digit:]T]##g').zip" "$@"
+    ;;
   esac
 }
 
