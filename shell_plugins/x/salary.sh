6f919ccb16db2040685c099c239a450311b6993e
sharils <3063067+sharils@users.noreply.github.com>
2024-01-09 21:59:52 +0800

    Change x salary

--- shell_plugins/x/salary.sh
+++ shell_plugins/x/salary.sh
@@ -8,12 +8,16 @@ salary() {
 
   [ -f "$dest" ] || curl "$src" >"$dest"
 
+  printf "%s年%s月%s日\n" "$(($(date +%Y) - 1911))" "$(date +%m)" "$(date +%d)"
+
   jq <"$dest" \
     --raw-output \
+    --arg LAST_YEAR "$(($(date +%Y) - 1911 - 1))年1月1日" \
     --arg THIS_YEAR "$(($(date +%Y) - 1911))年1月1日" \
     --arg NEXT_YEAR "$(($(date +%Y) - 1911 + 1))年1月1日" "$(
       cat <<'EOF'
       map(select(
+        .["實施日期（民國）"] == $ARGS.named.LAST_YEAR or
         .["實施日期（民國）"] == $ARGS.named.THIS_YEAR or
         .["實施日期（民國）"] == $ARGS.named.NEXT_YEAR
       )) |
