f07763cd372e1e38404207c010da392df722006d
sharils <3063067+sharils@users.noreply.github.com>
2024-01-20 13:09:04 +0800

    Change x english-words

--- shell_plugins/x/english-words.sh
+++ shell_plugins/x/english-words.sh
@@ -3,7 +3,7 @@
 [ -n "$SET_X" ] && set -x
 
 english_words() {
-  grep "$@" "$SHARILS_HOME/shell_plugins/x/english-words/words_alpha.txt"
+  rg --pcre2 "${@:-^}" "$SHARILS_HOME/shell_plugins/x/english-words/words_alpha.txt"
 }
 
 english_words "$@"
