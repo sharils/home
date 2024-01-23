5d659ce5d296ab2a482e2ca6f86149f0ebe7a985
sharils <3063067+sharils@users.noreply.github.com>
2024-01-23 20:45:45 +0800

    Add x til n

--- shell_plugins/x/til.sh
+++ shell_plugins/x/til.sh
@@ -5,6 +5,7 @@
 til() {
   case "${1:-c}" in
   c) set -- "$("$SHARILS_HOME/shell_plugins/x/ss.sh" | sed -n '/civil_twilight_begin/p' | sed 's/  */\t/' | cut -f2)" "$@" ;;
+  n) set -- "$("$SHARILS_HOME/shell_plugins/x/ss.sh" | sed -n '/nautical_twilight_begin/p' | sed 's/  */\t/' | cut -f2)" "$@" ;;
   esac
   python -c 'import sys;from datetime import *;print(datetime.strptime(sys.argv[1], "%Y-%m-%dT%H:%M:%S") - datetime.now())' "$@"
 }
