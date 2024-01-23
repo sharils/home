a83345effcb6f1b272721d5473c38bdbffce615f
sharils <3063067+sharils@users.noreply.github.com>
2024-01-24 07:01:42 +0800

    Add x til 1930

--- shell_plugins/x/til.sh
+++ shell_plugins/x/til.sh
@@ -4,6 +4,7 @@
 
 til() {
   case "${1:-c}" in
+  1930) set -- "$(date +%FT19:30:00)" "$@" ;;
   c) set -- "$("$SHARILS_HOME/shell_plugins/x/ss.sh" | sed -n '/civil_twilight_begin/p' | sed 's/  */\t/' | cut -f2)" "$@" ;;
   n) set -- "$("$SHARILS_HOME/shell_plugins/x/ss.sh" | sed -n '/nautical_twilight_begin/p' | sed 's/  */\t/' | cut -f2)" "$@" ;;
   esac
