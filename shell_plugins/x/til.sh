d42a03ea2d6d429ca6d3c41b53da264ac3a32a42
sharils <3063067+sharils@users.noreply.github.com>
2024-01-24 09:47:17 +0800

    Refactor x til

--- shell_plugins/x/til.sh
+++ shell_plugins/x/til.sh
@@ -3,10 +3,10 @@
 [ -n "$SET_X" ] && set -x
 
 til() {
-  case "${1:-c}" in
+  [ $# -eq 0 ] && set -- cb "$@"
+  case "$1" in
   1930) set -- "$(date +%FT19:30:00)" "$@" ;;
-  c) set -- "$("$SHARILS_HOME/shell_plugins/x/ss.sh" | sed -n '/civil_twilight_begin/p' | sed 's/  */\t/' | cut -f2)" "$@" ;;
-  n) set -- "$("$SHARILS_HOME/shell_plugins/x/ss.sh" | sed -n '/nautical_twilight_begin/p' | sed 's/  */\t/' | cut -f2)" "$@" ;;
+  *) key="$1" && shift && set -- "$("$SHARILS_HOME/shell_plugins/x/ss.sh" "$key")" "$@" ;;
   esac
   python -c 'import sys;from datetime import *;print(datetime.strptime(sys.argv[1], "%Y-%m-%dT%H:%M:%S") - datetime.now())' "$@"
 }
