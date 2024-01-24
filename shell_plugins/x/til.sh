f436f4efafdbc5b6e0940d4919b6247ef7d74af2
sharils <3063067+sharils@users.noreply.github.com>
2024-01-24 09:47:52 +0800

    Add x til c-8

--- shell_plugins/x/til.sh
+++ shell_plugins/x/til.sh
@@ -7,6 +7,7 @@ til() {
   case "$1" in
   1930) set -- "$(date +%FT19:30:00)" "$@" ;;
   n-8) set -- "$(date -ujf%FT%T -v-8H "$("$SHARILS_HOME/shell_plugins/x/ss.sh" nb)" +%FT%T)" "$@" ;;
+  c-8) set -- "$(date -ujf%FT%T -v-8H "$("$SHARILS_HOME/shell_plugins/x/ss.sh" cb)" +%FT%T)" "$@" ;;
   *) key="$1" && shift && set -- "$("$SHARILS_HOME/shell_plugins/x/ss.sh" "$key")" "$@" ;;
   esac
   python -c 'import sys;from datetime import *;print(datetime.strptime(sys.argv[1], "%Y-%m-%dT%H:%M:%S") - datetime.now())' "$@"
