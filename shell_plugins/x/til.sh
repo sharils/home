fec8e2f57d16d488712d5af1942e37825bbd45da
sharils <3063067+sharils@users.noreply.github.com>
2024-01-25 20:42:18 +0800

    Add x til c

--- shell_plugins/x/til.sh
+++ shell_plugins/x/til.sh
@@ -3,8 +3,8 @@
 [ -n "$SET_X" ] && set -x
 
 til() {
-  [ $# -eq 0 ] && set -- cb "$@"
-  case "$1" in
+  [ $# -eq 0 ] && set -- c "$@"
+  case "${1:-c}" in
   0715) set -- "$(date +%FT07:15:00)" "$@" ;;
   0730) set -- "$(date +%FT07:30:00)" "$@" ;;
   1700) set -- "$(date +%FT17:00:00)" "$@" ;;
@@ -12,7 +12,7 @@ til() {
   c-a) set -- "$(date -ujf%FT%T -v-10H "$("$SHARILS_HOME/shell_plugins/x/ss.sh" cb)" +%FT%T)" "$@" ;;
   n-8) set -- "$(date -ujf%FT%T -v-8H "$("$SHARILS_HOME/shell_plugins/x/ss.sh" nb)" +%FT%T)" "$@" ;;
   c-8) set -- "$(date -ujf%FT%T -v-8H "$("$SHARILS_HOME/shell_plugins/x/ss.sh" cb)" +%FT%T)" "$@" ;;
-  *) key="$1" && shift && set -- "$("$SHARILS_HOME/shell_plugins/x/ss.sh" "$key")" "$@" ;;
+  c) set -- "$("$SHARILS_HOME/shell_plugins/x/ss.sh" cb)" "$@" ;;
   esac
   python -c 'import sys;from datetime import *;print(datetime.strptime(sys.argv[1], "%Y-%m-%dT%H:%M:%S") - datetime.now())' "$@"
 }
