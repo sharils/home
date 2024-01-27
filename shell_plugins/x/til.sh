5b6cb29cb3e98736d3ffc15bb3bb385a7de27165
sharils <3063067+sharils@users.noreply.github.com>
2024-01-27 13:59:05 +0800

    Refactor x til

--- shell_plugins/x/til.sh
+++ shell_plugins/x/til.sh
@@ -5,14 +5,21 @@
 til() {
   [ $# -eq 0 ] && set -- c "$@"
   case "${1:-c}" in
-  0715) set -- "$(date +%FT07:15:00)" "$@" ;;
-  0730) set -- "$(date +%FT07:30:00)" "$@" ;;
-  1700) set -- "$(date +%FT17:00:00)" "$@" ;;
-  1930) set -- "$(date +%FT19:30:00)" "$@" ;;
+
   c-a) set -- "$(date -ujf%FT%T -v-10H "$("$SHARILS_HOME/shell_plugins/x/ss.sh" cb)" +%FT%T)" "$@" ;;
+
   n-8) set -- "$(date -ujf%FT%T -v-8H "$("$SHARILS_HOME/shell_plugins/x/ss.sh" nb)" +%FT%T)" "$@" ;;
+
   c-8) set -- "$(date -ujf%FT%T -v-8H "$("$SHARILS_HOME/shell_plugins/x/ss.sh" cb)" +%FT%T)" "$@" ;;
+
   c) set -- "$("$SHARILS_HOME/shell_plugins/x/ss.sh" cb)" "$@" ;;
+
+  # https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#Pattern-Matching
+  [[:digit:]][[:digit:]][[:digit:]][[:digit:]])
+    t="$(echo "$1" | sed 's/^../&:/; s/$/:00/')" && shift
+    set -- "$(date +%FT"$t")" "$@"
+    ;;
+
   esac
   python -c 'import sys;from datetime import *;print(datetime.strptime(sys.argv[1], "%Y-%m-%dT%H:%M:%S") - datetime.now())' "$@"
 }
