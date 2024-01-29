52c53c168b452901d2d45c094c99dd0cabb0c414
sharils <3063067+sharils@users.noreply.github.com>
2024-01-29 21:09:32 +0800

    Change k % week

--- shell_plugins/k/%/week.sh
+++ shell_plugins/k/%/week.sh
@@ -5,9 +5,9 @@
 week() {
   cat <<SH
 week:
-  %U: 00-53, $(date +%U), Sun-Sat
-  %V: 01-53, $(date +%V), ISO
-  %W: 00-53, $(date +%W), Mon-Sun
+  %U: 00-53, $(date +%U) # Sun-Sat
+  %V: 01-53, $(date +%V) # ISO
+  %W: 00-53, $(date +%W) # Mon-Sun
   %OU: POSIX, $(date +%OU)
   %OV: POSIX, $(date +%OV)
   %OW: POSIX, $(date +%OW)
