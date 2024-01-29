c8f9547b3552ee3e48c5a092308200ca3bf6a970
sharils <3063067+sharils@users.noreply.github.com>
2024-01-29 21:08:07 +0800

    Fix x cal st

--- shell_plugins/x/cal.sh
+++ shell_plugins/x/cal.sh
@@ -19,12 +19,19 @@ x_cal() {
     # https://data.gov.tw/dataset/157677
     [ -f "${st:=/tmp/x-cal-st-$(date +%Y).csv}" ] ||
       curl --location 'https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/A-A0087-003?Authorization=rdec-key-123-45678-011121314&format=CSV' >"$st"
-    yq @json "$st" | jq --arg TODAY "$(date +%F)" "$(cat <<'JQ'
+    yq @json "$st" | jq --raw-output --arg TODAY "$(date +%F)" "$(
+      cat <<'JQ'
       map(select(.["節氣"] | type == "string")) |
       map(select($ARGS.named.TODAY <= .["格里曆日期"])) |
-      .[0:4]
+      map([.["格里曆日期"], .["節氣時分"], .["節氣"]])[] |
+      @tsv
 JQ
-)"
+    )" |
+      while IFS= read -r line; do
+        d="$(echo "$line" | cut -f1)"
+        echo "$line $(date -jf%F "$d" +%a)"
+      done |
+      column -t
     return $?
     ;;
 
