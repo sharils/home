ade5cd07cf966f3db0c4c3727b6a97c6d7009b21
sharils <3063067+sharils@users.noreply.github.com>
2024-01-29 21:10:19 +0800

    Change k % year

--- shell_plugins/k/%/year.sh
+++ shell_plugins/k/%/year.sh
@@ -6,9 +6,9 @@ year() {
   cat <<SH
 year:
   %C: 19-99, $(date +%C)
-  %G: 1900-9999, $(date +%G), ISO
+  %G: 1900-9999, $(date +%G) # ISO
   %Y: 1900-9999, $(date +%Y)
-  %g: 00-99, $(date +%g), ISO
+  %g: 00-99, $(date +%g) # ISO
   %y: 00-99, $(date +%y)
   %EC: POSIX, $(date +%EC)
   %EY: POSIX, $(date +%EY)
