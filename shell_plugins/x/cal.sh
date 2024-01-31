93132604bcfe290eaa067be939edfd2409c404c9
sharils <3063067+sharils@users.noreply.github.com>
2024-01-31 20:56:00 +0800

    Add x cal zh lantern

--- shell_plugins/x/cal.sh
+++ shell_plugins/x/cal.sh
@@ -50,6 +50,9 @@ JQ
     # https://data.gov.tw/dataset/157677
     [ -f "${zh:=/tmp/x-cal-zh-$(date +%Y).csv}" ] ||
       curl --location 'https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/A-A0087-001?Authorization=rdec-key-123-45678-011121314&format=CSV' >"$zh"
+    case "$1" in
+    lantern) shift && set -- 'map(select(.["農曆月"] == 1 and .["農曆日"] == 15)) | map(.["格里曆日期"])' "$@" ;;
+    esac
     yq @json "$zh" | jq --arg TODAY "$(date +%F)" "${@:-map(select(.[\"格里曆日期\"] == \$ARGS.named.TODAY))}"
     return $?
     ;;
