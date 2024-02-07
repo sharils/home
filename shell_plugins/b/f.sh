6342f75daee6a231cc67cc377c155b2fc4f6a072
sharils <3063067+sharils@users.noreply.github.com>
2024-02-07 18:06:13 +0800

    Add b f sc

--- shell_plugins/b/f.sh
+++ shell_plugins/b/f.sh
@@ -5,6 +5,12 @@
 f() {
   BROWSER=/Applications/Firefox.app/Contents/MacOS/firefox
   case "$1" in
+  sc) $BROWSER "about:certificate?cert=$(openssl s_client -connect example.com:443 2>/dev/null </dev/null |
+    sed -n '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' |
+    sed '/^-/d' |
+    tr -d \\n |
+    jq --raw-input --raw-output @uri
+  )" ;;
   *) BROWSER=$BROWSER "$@" ;;
   esac
 }
