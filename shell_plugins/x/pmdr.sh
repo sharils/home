c735747ffcf8b76f1d3fb947eeab4c6054d6e153
sharils <3063067+sharils@users.noreply.github.com>
2024-02-14 19:31:31 +0800

    Add x pmdr

---
+++ shell_plugins/x/pmdr.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+[ -n "$SET_X" ] && set -x
+
+pmdr() {
+  LONG="${X_PMDR:-4}"
+  LONG_MAX=30
+  LONG_MIN=15
+  POMODORO=25
+  SHORT_MAX=5
+  SHORT_MIN=3
+
+  next=$(((POMODORO - $1 % POMODORO) % POMODORO))
+  pomodoro=$(($1 / POMODORO))
+  long=$((pomodoro / LONG))
+  short=$((pomodoro % LONG + long * (LONG - 1)))
+  min=$((short * SHORT_MIN + long * LONG_MIN))
+  max=$((short * SHORT_MAX + long * LONG_MAX))
+
+  cat <<EOF
+til next  $next
+pomodoro  $pomodoro
+short     $short
+long      $long
+min       $min
+max       $max
+EOF
+}
+
+pmdr "$@"
