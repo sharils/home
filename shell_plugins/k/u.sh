cd24fd966233f7c2d19021eb0a58d6e668023b00
sharils <3063067+sharils@users.noreply.github.com>
2024-02-14 10:42:04 +0800

    Change k u

--- shell_plugins/k/u.sh
+++ shell_plugins/k/u.sh
@@ -3,7 +3,12 @@
 [ -n "$SET_X" ] && set -x
 
 u() {
-  sed -n '/^bindings.default:$/,/^bindings.commands:$/p' /Applications/qutebrowser.app/Contents/Resources/qutebrowser/config/configdata.yml | grep "$*"
+  if [ $# -eq 0 ]; then
+    set -- ^ "$@"
+  else
+    set -- "$*\|:$"
+  fi
+  sed -n '/^bindings.default:$/,/^bindings.commands:$/p' /Applications/qutebrowser.app/Contents/Resources/qutebrowser/config/configdata.yml | grep "$@"
 }
 
 u "$@"
