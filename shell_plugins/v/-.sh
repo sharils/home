b87f622df5deb22590b58d3ff9d0800db2b031fd
sharils <3063067+sharils@users.noreply.github.com>
2024-02-22 20:18:08 +0800

    Add v - ex

--- shell_plugins/v/-.sh
+++ shell_plugins/v/-.sh
@@ -5,6 +5,7 @@
 v_v() {
   case "$1" in
   erl) erl -eval '{ok, Version} = file:read_file(filename:join([code:root_dir(), "releases", erlang:system_info(otp_release), "OTP_VERSION"])), io:fwrite(Version), halt().' ;;
+  ex) elixir --version ;;
   *) "$@" --version ;;
   esac
 }
