916d104ca264a9476f480e100cb586aacf0344ba
sharils <3063067+sharils@users.noreply.github.com>
2024-02-22 20:16:23 +0800

    Change m p g

--- shell_plugins/m/p.sh
+++ shell_plugins/m/p.sh
@@ -6,18 +6,21 @@ p() {
   case "$1" in
   g)
     shift
-    case "$1" in
-    a) shift && mix phx.gen.auth "$@" ;;
-    e) shift && mix phx.gen.embedded "$@" ;;
-    h) shift && mix phx.gen.html "$@" ;;
-    j) shift && mix phx.gen.json "$@" ;;
-    l) shift && mix phx.gen.live "$@" ;;
-    n) shift && mix phx.gen.notifier "$@" ;;
-    p) shift && mix phx.gen.presence "$@" ;;
-    r) shift && mix phx.gen.release "$@" ;;
-    schema) shift && mix phx.gen.schema "$@" ;;
-    *) mix phx.gen "$@" ;;
-    esac
+    (
+      cd apps/*_web || return $?
+      case "$1" in
+      a) shift && mix phx.gen.auth "$@" ;;
+      e) shift && mix phx.gen.embedded "$@" ;;
+      h) shift && mix phx.gen.html "$@" ;;
+      j) shift && mix phx.gen.json "$@" ;;
+      l) shift && mix phx.gen.live "$@" ;;
+      n) shift && mix phx.gen.notifier "$@" ;;
+      p) shift && mix phx.gen.presence "$@" ;;
+      r) shift && mix phx.gen.release "$@" ;;
+      schema) shift && mix phx.gen.schema "$@" ;;
+      *) mix phx.gen "$@" ;;
+      esac
+    )
     ;;
   i) shift && iex -S mix phx.server "$@" ;;
   n) shift && "$SHARILS_HOME/shell_plugins/m/p/n.sh" "$@" ;;
