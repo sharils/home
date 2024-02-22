ceb228117874aa3795a3fa3433766193e83c602d
sharils <3063067+sharils@users.noreply.github.com>
2024-02-22 23:14:05 +0800

    Add v - v|flyctl

--- shell_plugins/v/-.sh
+++ shell_plugins/v/-.sh
@@ -6,6 +6,7 @@ v_v() {
   case "$1" in
   erl) erl -eval '{ok, Version} = file:read_file(filename:join([code:root_dir(), "releases", erlang:system_info(otp_release), "OTP_VERSION"])), io:fwrite(Version), halt().' ;;
   ex) elixir --version ;;
+  f | flyctl) flyctl version ;;
   *) "$@" --version ;;
   esac
 }
