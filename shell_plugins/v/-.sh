203548b396a9892939a4a2424397e47cd6030b75
sharils <3063067+sharils@users.noreply.github.com>
2024-02-22 20:17:59 +0800

    Fix v - erl

--- shell_plugins/v/-.sh
+++ shell_plugins/v/-.sh
@@ -4,7 +4,7 @@
 
 v_v() {
   case "$1" in
-  erl) "$1" -version ;;
+  erl) erl -eval '{ok, Version} = file:read_file(filename:join([code:root_dir(), "releases", erlang:system_info(otp_release), "OTP_VERSION"])), io:fwrite(Version), halt().' ;;
   *) "$@" --version ;;
   esac
 }
