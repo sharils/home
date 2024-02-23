5ee484cec9f8ec8d17159a889795261646bda3ac
sharils <3063067+sharils@users.noreply.github.com>
2024-02-23 21:13:00 +0800

    Change m phx.gen.*x

--- shell_plugins/m.sh
+++ shell_plugins/m.sh
@@ -62,7 +62,10 @@ EOF
   sft) m strftime ;;
 
   phx.gen | phx.gen.auth | phx.gen.cert | phx.gen.channel | phx.gen.context | phx.gen.embedded | phx.gen.html | phx.gen.json | phx.gen.live | phx.gen.notifier | phx.gen.presence | phx.gen.release | phx.gen.schema | phx.gen.secret | phx.gen.socket)
-    mix "$@"
+    (
+      pwd | grep --quiet '_umbrella$' && cd apps/*_web || return $?
+      mix "$@"
+    )
     ;;
 
   app.config | app.start | app.tree | archive | archive.build | archive.install | archive.uninstall | clean | cmd | compile | deps | deps.clean | deps.compile | deps.get | deps.tree | deps.unlock | deps.update | do | escript | escript.build | escript.install | escript.uninstall | eval | format | help | hex | hex.audit | hex.build | hex.config | hex.docs | hex.info | hex.organization | hex.outdated | hex.owner | hex.package | hex.publish | hex.registry | hex.repo | hex.retire | hex.search | hex.sponsor | hex.user | loadconfig | local | local.hex | local.phx | local.public_keys | local.rebar | new | phx | phx.digest | phx.digest.clean | phx.new | phx.new.ecto | phx.new.web | phx.routes | phx.server | profile.cprof | profile.eprof | profile.fprof | release | release.init | run | test | test.coverage | xref) mix "$@" ;;
