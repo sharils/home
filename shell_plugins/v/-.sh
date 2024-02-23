45249e8208e7908bb8e16eba41236784609f83ad
sharils <3063067+sharils@users.noreply.github.com>
2024-02-23 17:27:30 +0800

    Add v - phx

--- shell_plugins/v/-.sh
+++ shell_plugins/v/-.sh
@@ -7,6 +7,7 @@ v_v() {
   erl) erl -eval '{ok, Version} = file:read_file(filename:join([code:root_dir(), "releases", erlang:system_info(otp_release), "OTP_VERSION"])), io:fwrite(Version), halt().' ;;
   ex) elixir --version ;;
   f | flyctl) flyctl version ;;
+  phx) mix phx.new --version ;;
   *) "$@" --version ;;
   esac
 }
