72774286214ed8748ed00b2b4cd786692b47ea4a
sharils <3063067+sharils@users.noreply.github.com>
2024-02-28 16:09:56 +0800

    Add k med 2

--- shell_plugins/k/med.sh
+++ shell_plugins/k/med.sh
@@ -28,16 +28,16 @@ med() {
   fi
 
   cat <<EOF | grep -E "^(?: ?$1.*)?"
-$(date +%k%M                   )  #    P  M    K      # Note
-$(printf %4s $(( 530 + offset)))  1            W      # Water
-$(printf %4s $(( 730 + offset)))  2 1  P  T O  WT CM  # Pigs Try Out White Tail Could Meow
-$(printf %4s $(( 930 + offset)))  3 2      U   W H    # Unicorns Will Help
-$(printf %4s $((1130 + offset)))  4 3     T    WT  M  # Ten White Tiger Meow
-$(printf %4s $((1330 + offset)))  5 4       O  W  CM  # Old Wolves Chase Moon
-$(printf %4s $((1530 + offset)))  6 5     TU   WT  M  # To Understand We Teach Math
-$(printf %4s $((1730 + offset)))  7 6          W   M  # Working Monkey
-$(printf %4s $((1930 + offset)))  8 7  P  T O  WTH    # Pigs Trick Octopus With Tasty Honey
-$(printf %4s $((2130 + offset)))  9 8      U      C   # Unicorn Charlie
+$(date +%k%M                   )  #    P  M    K      2  # Notes
+$(printf %4s $(( 530 + offset)))  1            W      F  # Water
+$(printf %4s $(( 730 + offset)))  2 1  P  T O  WT CM  T  # Pigs Try Out White Tail Could Meow
+$(printf %4s $(( 930 + offset)))  3 2      U   W H    F  # Unicorns Will Help
+$(printf %4s $((1130 + offset)))  4 3     T    WT  M  F  # Ten White Tiger Meow
+$(printf %4s $((1330 + offset)))  5 4       O  W  CM  F  # Old Wolves Chase Moon
+$(printf %4s $((1530 + offset)))  6 5     TU   WT  M  T  # To Understand We Teach Math
+$(printf %4s $((1730 + offset)))  7 6          W   M  F  # Working Monkey
+$(printf %4s $((1930 + offset)))  8 7  P  T O  WTH    T  # Pigs Trick Octopus With Tasty Honey
+$(printf %4s $((2130 + offset)))  9 8      U      C   F  # Unicorn Charlie
 EOF
 }
 
