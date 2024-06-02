f4bc379611317c9fa0cf0655f9195579f0fed59c
sharils <3063067+sharils@users.noreply.github.com>
2024-06-02 13:04:10 +0800

    Fix x aq w

--- shell_plugins/x/aq.sh
+++ shell_plugins/x/aq.sh
@@ -38,7 +38,7 @@ JQ
   w)
     shift
     op="${1:-lt}"
-    printf 'https://v5.airmap.g0v.tw/#/map'
+    printf 'https://v5.airmap.g0v.tw/#/map\n'
     printf 'https://pm25.lass-net.org/grafana/d/airbox_dashboard_v3/airdata?orgId=2&var-source=AirBox&var-device_id=%s&refresh=1m\n' "$X_AQ" >&2
 
     while :; do
