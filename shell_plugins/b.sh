0db7763e734ab8ab63b5802f25f5e957bad78619
sharils <3063067+sharils@users.noreply.github.com>
2024-06-02 16:32:47 +0800

    Change b u to brew upgrade

--- shell_plugins/b.sh
+++ shell_plugins/b.sh
@@ -47,7 +47,7 @@ b() {
   ri) shift && brew reinstall "$@" ;;
   s) shift && brew -S "$@" ;;
   tb) shift && BROWSER=/Applications/Tor\ Browser.app/Contents/MacOS/firefox "$@" ;;
-  u) shift && BROWSER=qutebrowser "$@" ;;
+  u) shift && brew upgrade "$@" ;;
   v) shift && brew abv "$@" ;;
   x) shift && BROWSER=/Applications/Firefox\ Developer\ Edition.app/Contents/MacOS/firefox "$@" ;;
   c | css | csv | eex | erl | ex | exs | gql | html | java | jq | js | json | jsx | lua | md | mdx | mw | pegjs | php | py | rs | scss | sh | sql | story | ts | tsx | txt | wat | xhtml | yaml | yml | zep) bat --language "$@" ;;
