adf0df5409abf66d57b60c406624411146768c7d
sharils <3063067+sharils@users.noreply.github.com>
2024-07-12 08:35:21 +0800

    Change x bmi

--- shell_plugins/x/bmi.sh
+++ shell_plugins/x/bmi.sh
@@ -3,7 +3,20 @@
 [ -n "$SET_X" ] && set -x
 
 bmi() {
-  bc --mathlib --expression="scale=2; $2 / ($1 / 100)^2"
+  cat <<'EOF'
+Underweight (Severe thinness)    < 16.0
+Underweight (Moderate thinness)  16.0 – 16.9
+Underweight (Mild thinness)      17.0 – 18.4
+Normal range                     18.5 – 24.9
+Overweight (Pre-obese)           25.0 – 29.9
+Obese (Class I)                  30.0 – 34.9
+Obese (Class II)                 35.0 – 39.9
+Obese (Class III)                ≥ 40.0
+EOF
+
+  echo
+
+  printf 'H: %s, W: %s, BMI: %s' "$1" "$2" "$(bc --mathlib --expression="scale=2; $2 / ($1 / 100)^2")"
 }
 
 bmi "$@"
