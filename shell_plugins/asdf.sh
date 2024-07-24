35e5a87815cf4ab1ba13cf7d40c2206a6e6216f7
sharils <3063067+sharils@users.noreply.github.com>
2024-07-24 11:45:32 +0800

    Change asdf-direnv

--- shell_plugins/asdf.sh
+++ shell_plugins/asdf.sh
@@ -3,4 +3,4 @@
 # shellcheck source=/dev/null
 . "$HOMEBREW_PREFIX/opt/asdf/libexec/asdf.sh"
 
-. "${XDG_CONFIG_HOME:-$HOME/.config}/asdf-direnv/bashrc"
+#. "${XDG_CONFIG_HOME:-$HOME/.config}/asdf-direnv/bashrc"
