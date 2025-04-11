ad5cc69452713c7ce2d64bb49c229eb15da9cf2d
sharils <3063067+sharils@users.noreply.github.com>
2025-04-11 12:08:51 +0800

    Change k distortion

--- shell_plugins/k/distortion.sh
+++ shell_plugins/k/distortion.sh
@@ -4,24 +4,24 @@
 
 distortion() {
   cat <<'EOF'
-All-or-nothing thinking
-Jumping to conclusions
-  Mind reading
-  Fortune-telling
-  Labeling
-Emotional reasoning
-Should/shouldn't and must/mustn't statements
-Gratitude traps
-Personalization and blaming
-  Always being right
-  Fallacy of change
-Minimizing-mislabeling
-  Magnification and minimization
-  Labeling and mislabeling
+All-or-nothing thinking (全有或全無的想法)
+Jumping to conclusions (妄下結論)
+  Mind reading (讀心術)
+  Fortune-telling (算命)
+  Labeling (貼標籤和貼錯標籤)
+Emotional reasoning (情緒化推理)
+Should/shouldn't and must/mustn't statements (做出「必須」或「應該」的陳述)
+Gratitude traps (感恩陷阱)
+Personalization and blaming (個人化和歸責)
+  Always being right (永遠正確)
+  Fallacy of change (變化謬誤)
+Minimizing-mislabeling (誇大和貼標籤)
+  Magnification and minimization (誇大和最小化)
+  Labeling and mislabeling (貼標籤和貼錯標籤)
 Assuming the worst
-  Overgeneralizing
-  Disqualifying the positive
-  Mental filtering
+  Overgeneralizing (過度概括)
+  Disqualifying the positive (取消積極的資格)
+  Mental filtering (過濾)
 
 source https://en.wikipedia.org/wiki/Cognitive_distortion
 EOF
