77af4f9c9a8e8123f63c3fc7c3d3846b05125ac8
sharils <3063067+sharils@users.noreply.github.com>
2025-04-17 22:04:03 +0800

    Add m o 60

--- shell_plugins/m/o.sh
+++ shell_plugins/m/o.sh
@@ -8,6 +8,7 @@ o() {
     return $?
   fi
   case "$1" in
+  60) $BROWSER 'https://www.youtube.com/watch?v=dadisjcjqtk' ;;
   91) open -b com.apple.Music 'https://jazzfm91.streamb.live/SB00009' ;;
   247) open -b com.apple.Music 'https://stream.zeno.fm/s2888pqwzqzuv' ;;
   977) open -b com.apple.Music 'https://music.apple.com/tw/station/classical-fm-97-7/ra.1459554452?l=en' ;;
