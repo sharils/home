ae9e9ad4cf72e8eb05d607fcc4b710d551cf7145
sharils <3063067+sharils@users.noreply.github.com>
2025-06-26 10:45:52 +0800

    Add t e b

--- shell_plugins/t/e.sh
+++ shell_plugins/t/e.sh
@@ -5,6 +5,7 @@
 e() {
   case "$1" in
   cfg) shift && $EDITOR "$@" ~/.todo.cfg ;;
+  b) shift && $EDITOR "$@" "$TODO_DIR/backlog.md" ;;
   q) shift && $EDITOR "$@" "$TODO_DIR/QuickNote.md" ;;
   t) shift && $EDITOR "$@" "$TODO_FILE" ;;
   *[!0-9]*) $EDITOR "$TODO_DIR/$*.txt" ;;
