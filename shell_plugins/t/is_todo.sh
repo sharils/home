6492d9f9edc6fab60f3b9b99cd37b8d4bea96f5a
sharils <3063067+sharils@users.noreply.github.com>
2025-06-28 11:12:29 +0800

    Add t s

--- shell_plugins/t/is_todo.sh
+++ shell_plugins/t/is_todo.sh
@@ -11,6 +11,8 @@ is_todo() {
 
   r) echo replace && return 0 ;;
 
+  s) echo stale && return 0 ;;
+
   z) echo archive && return 0 ;;
 
   add | a | addm | addto | append | app | archive | command | deduplicate | del | rm | depri | dp | done | do | help | list | ls | listall | lsa | listaddons | listcon | lsc | listfile | lf | listpri | lsp | listproj | lsprj | move | mv | prepend | prep | pri | replace | report | shorthelp | count | note | outline | projectview | stale | today)
