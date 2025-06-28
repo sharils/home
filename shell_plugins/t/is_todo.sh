9d596546d7dc5e8b07a89971b76668cf5c89a1b4
sharils <3063067+sharils@users.noreply.github.com>
2025-06-28 11:10:26 +0800

    Add t <addon>

--- shell_plugins/t/is_todo.sh
+++ shell_plugins/t/is_todo.sh
@@ -11,7 +11,7 @@ is_todo() {
 
   z) echo archive && return 0 ;;
 
-  add | a | addm | addto | append | app | archive | command | deduplicate | del | rm | depri | dp | done | do | help | list | ls | listall | lsa | listaddons | listcon | lsc | listfile | lf | listpri | lsp | listproj | lsprj | move | mv | prepend | prep | pri | replace | report | shorthelp)
+  add | a | addm | addto | append | app | archive | command | deduplicate | del | rm | depri | dp | done | do | help | list | ls | listall | lsa | listaddons | listcon | lsc | listfile | lf | listpri | lsp | listproj | lsprj | move | mv | prepend | prep | pri | replace | report | shorthelp | count | note | outline | projectview | stale | today)
     echo "$1" && return 0
     ;;
 
