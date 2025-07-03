04a68096bb4b9ab130d7d88b98fb60bdc554e7a0
sharils <3063067+sharils@users.noreply.github.com>
2025-07-03 09:35:32 +0800

    Refactor x cycle

--- shell_plugins/x/cycle.sh
+++ shell_plugins/x/cycle.sh
@@ -5,8 +5,8 @@
 cycle() {
   day="${1:-$(date +%F)}"
   printf "D1\t%s\n" "$day"
-  printf "More\t%s\n" "$(date -jf%F -v+"$((X_CYCLE_BEGIN))"d "$day" +%F)"
-  printf "Less\t%s\n" "$(date -jf%F -v+"$((X_CYCLE_PERIOD - X_CYCLE_END - 1))"d "$day" +%F)"
+  printf "More\t%s\n" "$(date -jf%F -v+"$((X_CYCLE_MORE))"d "$day" +%F)"
+  printf "Less\t%s\n" "$(date -jf%F -v+"$((X_CYCLE_PERIOD - X_CYCLE_LESS - 1))"d "$day" +%F)"
   printf "Period\t%s\n" "$(date -jf%F -v+"$((X_CYCLE_PERIOD - 1))"d "$day" +%F)"
   printf "Cycle\t%s" "$(date -jf%F -v+"$X_CYCLE_CYCLE"d "$day" +%F)"
 }
