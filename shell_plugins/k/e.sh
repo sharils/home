98c18a9f8327a034e718f0f42b5d53188f627541
sharils <3063067+sharils@users.noreply.github.com>
2025-08-17 03:41:49 +0800

    Change k e emoji

--- shell_plugins/k/e.sh
+++ shell_plugins/k/e.sh
@@ -4,10 +4,7 @@
 
 e() {
   case "$1" in
-  animal | nature | food | drink | activity) open "$SHARILS_HOME/emoji/AnimalNatureFoodDrinkActivity.png" ;;
-  smiles | people) open "$SHARILS_HOME/emoji/SmilesPeople.png" ;;
-  symbols | flags) open "$SHARILS_HOME/emoji/SymbolsFlags.png" ;;
-  travel | places | objects) open "$SHARILS_HOME/emoji/TravelPlacesObjects.png" ;;
+  smileys\&people | animals\&nature | food\&drink | activity | travel\&places | objects | symbols | flags) open "$SHARILS_HOME/emoji/$1.png" ;;
   *) $EDITOR "$@" ~/.config/karabiner/karabiner.json ;;
   esac
 }
