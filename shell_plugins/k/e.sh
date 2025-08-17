54df366672bb9fb009d1a10612c537a06fa90d79
sharils <3063067+sharils@users.noreply.github.com>
2025-08-18 01:41:34 +0800

    Add k e ls

--- shell_plugins/k/e.sh
+++ shell_plugins/k/e.sh
@@ -4,7 +4,19 @@
 
 e() {
   case "$1" in
-  smileys\&people | animals\&nature | food\&drink | activity | travel\&places | objects | symbols | flags) open "$SHARILS_HOME/emoji/$1.png" ;;
+  sp) shift && set -- smileys-people "$@" ;;
+  an) shift && set -- animals-nature "$@" ;;
+  fd) shift && set -- food-drink "$@" ;;
+  a) shift && set -- activity "$@" ;;
+  tp) shift && set -- travel-places "$@" ;;
+  o) shift && set -- objects "$@" ;;
+  s) shift && set -- symbols "$@" ;;
+  f) shift && set -- flags "$@" ;;
+  ls) shift && set -- "$(ls "$SHARILS_HOME/emoji/"  | sed 's/.png//g' | fzf)" "$@" ;;
+  esac
+
+  case "$1" in
+  smileys-people | animals-nature | food-drink | activity | travel-places | objects | symbols | flags) open "$SHARILS_HOME/emoji/$1.png" ;;
   *) $EDITOR "$@" ~/.config/karabiner/karabiner.json ;;
   esac
 }
