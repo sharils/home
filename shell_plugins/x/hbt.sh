3d102da21be3f17fae12686c84c8fec19d1c2472
sharils <3063067+sharils@users.noreply.github.com>
2025-08-24 22:41:14 +0800

    Fix _backup to ~backup

--- shell_plugins/x/hbt.sh
+++ shell_plugins/x/hbt.sh
@@ -3,7 +3,7 @@
 [ -n "$SET_X" ] && set -x
 
 hbt() {
-  dir="$HOME/git/github.com/sharils/_backup/habitica/$(date +%F)"
+  dir="$HOME/git/github.com/sharils/~backup/habitica/$(date +%F)"
   mkdir -pv "$dir"
   mv -v ~/Downloads/habitica-user-data.json ~/Downloads/habitica-tasks-history.csv "$dir"
 }
