bbc195e4b79116ed48810211bea0f0c9290aefd7
sharils <3063067+sharils@users.noreply.github.com>
2025-10-25 21:43:59 +0800

    Change espanso clipy

--- shell_plugins/k/size.sh
+++ shell_plugins/k/size.sh
@@ -5,9 +5,10 @@
 size() {
   cat <<'EOF' | sed 's/\t/  /g'
 ISO/IEC 7810    	 85.60mm	  53.98mm
-HTC Desire 12s  	154.20cm	  72.70cm	   8.30cm	  5.7in	150.000g	 8.1.0
-MacBook Air 2024	 30.41cm	  21.50cm	   1.13cm	 13.6in	  1.24kg	15.6.1
-iPhone 6S       	138.30cm	  67.10cm	   7.10cm	  4.7in	143.000g	15.8.5
+HTC Desire 12s  	154.20cm	  72.70cm	   8.30cm	150.000g	  5.7in	 8.1.0
+MacBook Air 2024	 30.41cm	  21.50cm	   1.13cm	  1.24kg	 13.6in	15.6.1
+iPhone 6S       	138.30cm	  67.10cm	   7.10cm	143.000g	  4.7in	15.8.5
+Apple Watch 7   	 41.00mm	  35.00mm	  10.70mm	 32.000g	  430px	 352px	Aluminum
 Cool Summer     	(50, 95)	(-5, +15)	(-20, +5)	(5, 35)
 EOF
 }
