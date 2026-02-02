18e36bad9148cc559d34bd4ca63c16f98a4ab1f5
sharils <3063067+sharils@users.noreply.github.com>
2026-02-02 17:28:41 +0800

    Change k size

--- shell_plugins/k/size.sh
+++ shell_plugins/k/size.sh
@@ -8,6 +8,7 @@ Apple Watch 7   	  41.00mm	   35.00mm	   10.70mm	 32.000g	 430px	352px 	Aluminum
 ISO/IEC 7810    	  85.60mm	   53.98mm
 Pocket Tissue S 	  12.50cm	    8.00cm
 Pocket Tissue L 	  12.50cm	   10.50cm
+A4              	  29.70cm	   21.00cm
 iPhone 6S       	 138.30cm	   67.10cm	    7.10cm	143.000g	 4.7in	15.8.5
 HTC Desire 12s  	 154.20cm	   72.70cm	    8.30cm	150.000g	 5.7in	8.1.0
 MacBook Air 2024	  30.41cm	   21.50cm	    1.13cm	  1.24kg	13.6in	15.6.1
