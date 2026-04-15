a08af6e78a6e1392b3f2961e5a3f7af2399354c9
sharils <3063067+sharils@users.noreply.github.com>
2026-04-15 12:17:44 +0800

    Change k db

--- shell_plugins/k/db.sh
+++ shell_plugins/k/db.sh
@@ -5,21 +5,21 @@
 db() {
   cat <<'EOF'
 Sound	  Time	Example
- 10dB	     ∞	Normal breathing
- 30dB	     ∞	Soft whispering
- 40dB	     ∞	Library
- 60dB	     ∞	Normal conversation
- 80dB	   40h	Doorbell
- 85dB	12h30m	Heavy traffic (inside car)
- 90dB	    4h	Shouted conversation
- 95dB	 1h15m	Motorcycle
-100dB	   20m	Hair dryer
-105dB	    8m	Car horn at 5 meters
-110dB	  2.5m	Shouting in the ear
-120dB	   12s	Standing near a siren
-130dB	   <1s	Jackhammer
-140dB	    0s	Airplane taking off
 150dB	    0s	Firecrackers
+140dB	    0s	Airplane taking off
+130dB	   <1s	Jackhammer
+120dB	   12s	Standing near a siren
+110dB	  2.5m	Shouting in the ear
+105dB	    8m	Car horn at 5 meters
+100dB	   20m	Hair dryer
+ 95dB	 1h15m	Motorcycle
+ 90dB	    4h	Shouted conversation
+ 85dB	12h30m	Heavy traffic (inside car)
+ 80dB	   40h	Doorbell
+ 60dB	     ∞	Normal conversation
+ 40dB	     ∞	Library
+ 30dB	     ∞	Soft whispering
+ 10dB	     ∞	Normal breathing
 
 https://www.who.int/news-room/questions-and-answers/item/deafness-and-hearing-loss-safe-listening
 EOF
