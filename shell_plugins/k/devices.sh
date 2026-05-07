d5dcf969c2740db85a76c1b3cf39990eb5e9152a
sharils <3063067+sharils@users.noreply.github.com>
2026-05-07 10:47:15 +0800

    Add k devices iRobot

--- shell_plugins/k/devices.sh
+++ shell_plugins/k/devices.sh
@@ -4,8 +4,9 @@
 
 devices() {
   cat <<'EOF'
-LG         WD-90MGA   DD直驅變頻滾筒洗衣機 白 / 9公斤洗衣容量
-Panasonic  NI-GHK045  2in1 蒸氣電熨斗
+LG         WD-90MGA    DD直驅變頻滾筒洗衣機 白 / 9公斤洗衣容量
+Panasonic  NI-GHK045   2in1 蒸氣電熨斗
+iRobot     Roomba 890  掃地機器人
 EOF
 }
 
