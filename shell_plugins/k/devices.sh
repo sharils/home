9eaba58d0094ad36d4c74ce6f86475883dea95ad
sharils <3063067+sharils@users.noreply.github.com>
2026-05-19 21:25:03 +0800

    Change k devices

--- shell_plugins/k/devices.sh
+++ shell_plugins/k/devices.sh
@@ -5,6 +5,7 @@
 devices() {
   cat <<'EOF'
 LG         WD-90MGA    DD直驅變頻滾筒洗衣機 白 / 9公斤洗衣容量
+LG         AS551DWG0   PuriCare™ 360°空氣清淨機 - HEPA 13版/適用17坪 (單層)
 Panasonic  NI-GHK045   2in1 蒸氣電熨斗
 iRobot     Roomba 890  掃地機器人
 EOF
