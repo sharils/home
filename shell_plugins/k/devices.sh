2036899faa8fb47d687c3792ca7796cbbbb5ad06
sharils <3063067+sharils@users.noreply.github.com>
2026-06-12 21:50:53 +0800

    Change k devices

--- shell_plugins/k/devices.sh
+++ shell_plugins/k/devices.sh
@@ -4,10 +4,11 @@
 
 devices() {
   cat <<'EOF'
-LG         WD-90MGA    DD直驅變頻滾筒洗衣機 白 / 9公斤洗衣容量
-LG         AS551DWG0   PuriCare™ 360°空氣清淨機 - HEPA 13版/適用17坪 (單層)
-Panasonic  NI-GHK045   2in1 蒸氣電熨斗
-iRobot     Roomba 890  掃地機器人
+LG         WD-90MGA         DD直驅變頻滾筒洗衣機 白 / 9公斤洗衣容量
+LG         AS551DWG0        PuriCare™ 360°空氣清淨機 - HEPA 13版/適用17坪 (單層)
+Panasonic  NI-GHK045        2in1 蒸氣電熨斗
+iRobot     Roomba 890       掃地機器人
+3M         3M 90565-4DC-PS  耳罩
 EOF
 }
 
