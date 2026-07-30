7a1ecd3e694f603fc22721e190b7fb0e9638bd19
sharils <3063067+sharils@users.noreply.github.com>
2026-07-30 20:18:25 +0800

    Remove alias pnpm

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,4 @@
--- require("config.lazy")
+vim.pack.add({ 'https://github.com/f-person/auto-dark-mode.nvim' })
+
+vim.pack.add({ 'https://github.com/nvim-mini/mini.pairs' })
+require('mini.pairs').setup()
--- shell_plugins/alias.sh
+++ shell_plugins/alias.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env sh
 
-pnpm() {
-  npx --yes pnpm "$@"
-}
-
 yarn() {
   npx --yes yarn "$@"
 }
