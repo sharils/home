b291d8ae67c6fe24753c5c1303bdd58ddd3a40fd
sharils <3063067+sharils@users.noreply.github.com>
2026-08-08 11:37:57 +0800

    Change nvim init

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,15 @@
-vim.pack.add({ 'https://github.com/f-person/auto-dark-mode.nvim' })
+-- require("config.lazy")
 
-vim.pack.add({ 'https://github.com/nvim-mini/mini.pairs' })
-require('mini.pairs').setup()
+vim.opt.autowriteall = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.pack.add({ 'https://github.com/zenbones-theme/zenbones.nvim' })
+vim.pack.add({ 'https://github.com/f-person/auto-dark-mode.nvim' })
+vim.pack.add({ 'https://github.com/nvim-mini/mini.pairs' }); require('mini.pairs').setup()
